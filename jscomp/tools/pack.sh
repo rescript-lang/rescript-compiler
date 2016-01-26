1998f8faef046c5fbec296b30adc234ed12c80e8
Hongbo Zhang <hzhang295@bloomberg.net>
2016-01-26 16:28:17 -0500

    1. add a timestamp 2.snapshot the new release 3. add a release script

--- jscomp/tools/pack.sh
+++ jscomp/tools/pack.sh
@@ -3,6 +3,6 @@ set -e
 
 # Not needed only for bootstrapping
 # ocaml_pack ../js-lambda/tools/depend.ml ../js-lambda/tools/depend.mli ocaml_extract.ml ocaml_extract.mli ocaml_pack_main.ml > ocaml_pack.ml
-ocamlopt.opt ocamlcommon.cmxa -I +compiler-libs ocaml_pack.ml -o ocaml_pack
+ocamlopt.opt unix.cmxa ocamlcommon.cmxa -I +compiler-libs -I +unix ocaml_pack.ml -o ocaml_pack
 
 
