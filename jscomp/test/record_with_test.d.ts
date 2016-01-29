1c7dec709fec91e95c634e5a22340047d31290d5
Hongbo Zhang <hzhang295@bloomberg.net>
2016-01-29 13:48:40 -0500

    add a test case

---
+++ jscomp/test/record_with_test.d.ts
@@ -0,0 +1,5 @@
+export var v: any ;
+export var uv: any ;
+export var u_v: any ;
+export var f: (g : any, h : any) => any ;
+
---
+++ jscomp/test/record_with_test.js
@@ -0,0 +1,43 @@
+// Generated CODE, PLEASE EDIT WITH CARE
+"use strict";
+
+
+var v = [
+  /* record */0,
+  /* None */0,
+  0,
+  0,
+  0,
+  0,
+  0,
+  0
+];
+
+var newrecord = /* unknown */"duprecord regular 7";
+
+newrecord[2] = 0;
+
+function f(_, _$1) {
+  var newrecord = /* unknown */"duprecord regular 7";
+  newrecord[2] = 0;
+  return newrecord;
+}
+
+var uv = [
+  /* record */0,
+  /* None */0,
+  1,
+  0,
+  0,
+  0,
+  0,
+  0
+];
+
+var u_v = newrecord;
+
+exports.v   = v;
+exports.uv  = uv;
+exports.u_v = u_v;
+exports.f   = f;
+/*  Not a pure module */
