d51b8278593ba5d7b6303f2988c387c315e3c087
Hongbo Zhang <bobzhang1988@gmail.com>
2016-02-17 22:26:26 -0500

    add a test case for extensible variant

---
+++ jscomp/test/extensible_variant_test.d.ts
@@ -0,0 +1,6 @@
+export var Str: any ;
+export var N: any ;
+export var Int: any ;
+export var to_int: (x : any) => any ;
+export var suites: any ;
+
---
+++ jscomp/test/extensible_variant_test.ml
@@ -0,0 +1,24 @@
+type attr = ..
+
+type attr += Str of string
+
+module N = struct
+  type attr += Int of int * int
+end
+
+type attr += Int of int * int 
+
+let to_int (x : attr) = 
+  match x with
+  | Str  _ -> - 1
+  | N.Int (a,_) -> a
+  | Int (_, b) -> b   
+  | _ -> assert false
+
+let suites = Mt.[
+  "test_int", (fun _ -> Eq(3, to_int (N.Int(3,0))));
+  "test_int2", (fun _ -> Eq(0, to_int (Int(3,0))));
+  "test_string", (fun _ -> Eq(-1, to_int (Str "x")))
+]
+
+;; Mt.from_pair_suites __FILE__ suites
