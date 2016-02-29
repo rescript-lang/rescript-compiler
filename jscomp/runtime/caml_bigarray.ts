ccefc9f2eb2f3fb88c07da4de40cc7216058ca42
Hongbo Zhang <hzhang295@bloomberg.net>
2016-02-29 15:02:26 -0500

    add some comments

--- jscomp/runtime/caml_bigarray.ts
+++ jscomp/runtime/caml_bigarray.ts
@@ -1,5 +1,29 @@
+// Js_of_ocaml runtime support
+// http://www.ocsigen.org/js_of_ocaml/
+// Copyright (C) 2014 Jérôme Vouillon, Hugo Heuzard, Andy Ray
+// Laboratoire PPS - CNRS Université Paris Diderot
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, with linking exception;
+// either version 2.1 of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+//  Copyright (c) 2015-2016 Bloomberg LP. All rights reserved. 
+// Hongbo Zhang (hzhang295@bloomberg.net)              
+
 'use strict';
 // Bigarray.
+// Note this is  temporary and does not get tested, 
+// we will replace this part with ocaml itself instead
 //
 // - all bigarray types including Int64 and Complex.
 // - fortran + c layouts
