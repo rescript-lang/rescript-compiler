37216b1033b2d719ce6654aa59595d8a07e1946d
Hongbo Zhang <bobzhang1988@gmail.com>
2016-03-21 10:18:34 +0800

    more tests

---
+++ jscomp/test/random_test.ml
@@ -0,0 +1,7 @@
+(** TODO: need [caml_md5_string] support *)
+let f () =
+  Random.self_init ();
+  let x = Random.int 10000 in
+  Random.self_init ();
+  let y = Random.int 1000 in
+  if x = y then print_endline "FAILED" else print_endline "PASSED"
