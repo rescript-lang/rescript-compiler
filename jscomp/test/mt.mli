991fa0e4a6e27cc7cdf130cb67ad53c64e85aa0f
Hongbo Zhang <bobzhang1988@gmail.com>
2016-04-06 06:15:02 -0400

    add stress testing of fib, fac and fix mock_mt variance problem

--- jscomp/test/mt.mli
+++ jscomp/test/mt.mli
@@ -3,7 +3,7 @@ type + _ eq =
   | Neq : 'a * 'a -> 'c eq
   | Approx : float * float -> 'd eq 
   | ThrowAny : (unit -> unit) -> _ eq
-type 'a pair_suites = (string * (unit -> 'a eq)) list
+type + 'a pair_suites = (string * (unit -> 'a eq)) list
 
 val from_suites : string -> (string * (unit -> unit)) list -> unit
 val from_pair_suites : string -> 'a pair_suites -> unit
