a807ff90a55d55c8632ebf5e42a2a3a4604539bd
Hongbo Zhang <hzhang295@bloomberg.net>
2016-04-14 14:17:15 -0400

    finish class tests, everything looks good!

---
+++ jscomp/test/class8_test.ml
@@ -0,0 +1,44 @@
+let suites :  Mt.pair_suites ref  = ref []
+let test_id = ref 0
+let eq loc x y = 
+  incr test_id ; 
+  suites := 
+    (loc ^" id " ^ (string_of_int !test_id), (fun _ -> Mt.Eq(x,y))) :: !suites
+
+
+
+class virtual comparable =
+    object (_ : 'a)
+      method virtual leq : 'a -> bool
+    end;;
+
+class money (x : float) =
+    object
+      inherit comparable
+      val repr = x
+      method value = repr
+      method leq p = repr <= p#value
+    end;;
+
+
+class money2 x =
+    object
+      inherit money x
+      method times k = {< repr = k *. repr >}
+    end;;
+
+let min (x : #comparable) y =
+    if x#leq y then x else y;;
+
+let () = 
+  eq __LOC__ 
+    1.
+    (min (new money  1.0) (new money 3.0))#value;;
+
+let () = 
+  eq __LOC__
+    3.
+  (* Js.log  *)
+    (min (new money2 5.0) (new money2 3.))#value;;
+
+let () = Mt.from_pair_suites __FILE__ !suites
---
+++ jscomp/test/class_repr.js
@@ -0,0 +1,26 @@
+// Generated CODE, PLEASE EDIT WITH CARE
+'use strict';
+
+var CamlinternalOO = require("../stdlib/camlinternalOO");
+var Caml_curry     = require("../runtime/caml_curry");
+var Oo             = require("../stdlib/oo");
+
+function x_init($$class) {
+  var x = CamlinternalOO.new_variable($$class, "x");
+  return function (_, self, v) {
+    var self$1 = CamlinternalOO.create_object_opt(self, $$class);
+    self$1[x] = v;
+    return self$1;
+  };
+}
+
+var x = CamlinternalOO.make_class(0, x_init);
+
+var v = Caml_curry.app2(x[0], 0, 3);
+
+var u = Oo.copy(v);
+
+exports.x = x;
+exports.v = v;
+exports.u = u;
+/* x Not a pure module */
