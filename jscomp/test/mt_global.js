cd51c9597682e5c3efad9c7938399b3ccd91d963
Hongbo Zhang <bobzhang1988@gmail.com>
2016-04-15 16:43:55 -0400

    more float tests (#257)

---
+++ jscomp/test/mt_global.js
@@ -0,0 +1,45 @@
+// Generated CODE, PLEASE EDIT WITH CARE
+'use strict';
+
+
+function collect_eq(test_id, suites, loc, x, y) {
+  test_id[0] = test_id[0] + 1 | 0;
+  suites[0] = /* :: */[
+    /* tuple */[
+      loc + (" id " + test_id[0]),
+      function () {
+        return /* Eq */{
+                0: x,
+                1: y,
+                length: 2,
+                tag: 0
+              };
+      }
+    ],
+    suites[0]
+  ];
+  return /* () */0;
+}
+
+function collect_approx(test_id, suites, loc, x, y) {
+  test_id[0] = test_id[0] + 1 | 0;
+  suites[0] = /* :: */[
+    /* tuple */[
+      loc + (" id " + test_id[0]),
+      function () {
+        return /* Approx */{
+                0: x,
+                1: y,
+                length: 2,
+                tag: 2
+              };
+      }
+    ],
+    suites[0]
+  ];
+  return /* () */0;
+}
+
+exports.collect_eq     = collect_eq;
+exports.collect_approx = collect_approx;
+/* No side effect */
---
+++ jscomp/test/mt_global.mli
@@ -0,0 +1,9 @@
+
+val collect_eq : 
+  int ref -> Mt.pair_suites ref
+  -> string -> 'b -> 'b -> unit
+
+val collect_approx : 
+  int ref -> Mt.pair_suites ref
+  -> string -> float  -> float -> unit
+ 
