ff9cf2fbdcae7bedbf686df2656d646183a4cdbd
Hongbo Zhang <bobzhang1988@gmail.com>
2016-04-23 20:07:27 -0400

    [fix] add more tests of float and hash from compiler test suites

---
+++ jscomp/test/float_record.mli
@@ -0,0 +1,18 @@
+(***********************************************************************)
+(*                                                                     *)
+(*                                OCaml                                *)
+(*                                                                     *)
+(*             Pierre Weis, projet Cristal, INRIA Rocquencourt         *)
+(*                                                                     *)
+(*  Copyright 2007 Institut National de Recherche en Informatique et   *)
+(*  en Automatique.  All rights reserved.  This file is distributed    *)
+(*  under the terms of the Q Public License version 1.0.               *)
+(*                                                                     *)
+(***********************************************************************)
+
+type t = private float;;
+
+val make : float -> t;;
+val from : t -> float;;
+
+type s = {f : t};;
