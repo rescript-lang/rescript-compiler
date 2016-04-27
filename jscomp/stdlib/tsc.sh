c20f8c8cde883a9ea90bc8048195d4dcbab0f958
Hongbo Zhang <hzhang295@bloomberg.net>
2016-04-27 14:16:02 -0400

    npm seems to work

--- jscomp/stdlib/tsc.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-set -e 
-d node ../../node_modules/typescript/bin/tsc 
---
+++ jscomp/test_npm/test_list.js
@@ -0,0 +1,20 @@
+// GENERATED CODE BY BUCKLESCRIPT VERSION 0.3 , PLEASE EDIT WITH CARE
+'use strict';
+
+var List = require("buckle/list.js");
+
+var len = List.length(/* :: */[
+      1,
+      /* :: */[
+        2,
+        /* :: */[
+          3,
+          /* [] */0
+        ]
+      ]
+    ]);
+
+console.log(len);
+
+exports.len = len;
+/* len Not a pure module */
