c3f03880c012c55a0ec3776e8d0e27f4649f171c
Hongbo Zhang <bobzhang1988@gmail.com>
2016-04-27 14:19:01 -0400

    npm seems to work (#317)

--- jscomp/dist.sh
+++ jscomp/dist.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
 
-cd dist && rm -rf * && cd ..
+cd dist && rm -rf *.ml *.mli *.cm* *.js *.annot  && cd ..
 cd runtime && cp *.ml *.mli *.cmt  *.cmj *.cmi *.js ../dist && cd ..
 cd stdlib && cp $(git ls-files) ../dist  && cd ..
-cd dist && make all && rm *.ml *.mli  && cd ..
-cd dist && rm *.ml *.mli && cd ..
+cd dist && make all && rm -f *.ml *.mli  && cd ..
+
 # currently remove *.ml and *.mli for LICENSE issues
