841ebab470071051dd5ceb276da9f030b57d686c
Hongbo Zhang <bobzhang1988@gmail.com>
2016-04-28 22:18:59 -0400

    [fix] file descriptor , move date into test dir

--- jscomp/dist.sh
+++ jscomp/dist.sh
@@ -6,5 +6,7 @@ cp bin/compiler.ml bin/compiler.mli dist/bin
 cd runtime && cp *.ml *.mli *.cmt  *.cmj *.cmi *.js ../dist && cd ..
 cd stdlib && cp $(git ls-files) ../dist  && cd ..
 cd dist && make all && rm -f *.ml *.mli  && cd ..
-cp test/*.ml dist/test && cd dist && bsc -I . -I test -c `ocamldep -sort -one-line -modules test/*.mli test/*.ml` && cd ..
+cp test/*.ml test/*.mli dist/test 
+cd dist && bsc -I . -I test -c `ocamldep -sort -one-line -modules test/*.mli test/*.ml` && cd ..
+cd dist && npm run test
 # currently remove *.ml and *.mli for LICENSE issues
