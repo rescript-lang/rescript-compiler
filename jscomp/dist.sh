81c784d022c2b0164e539f6927e2b955eedcdc4b
Hongbo Zhang <bobzhang1988@gmail.com>
2016-04-28 21:47:03 -0400

    tweak dist

--- jscomp/dist.sh
+++ jscomp/dist.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 set -e
 
-cd dist && rm -rf *.ml *.mli *.cm* *.js *.annot  && cd ..
+cd dist && git clean -dfx  && cd ..
 cp bin/compiler.ml bin/compiler.mli dist/bin
 cd runtime && cp *.ml *.mli *.cmt  *.cmj *.cmi *.js ../dist && cd ..
 cd stdlib && cp $(git ls-files) ../dist  && cd ..
 cd dist && make all && rm -f *.ml *.mli  && cd ..
-
+cp test/*.ml dist/test && cd dist && bsc -I . -I test -c `ocamldep -sort -one-line -modules test/*.mli test/*.ml` && cd ..
 # currently remove *.ml and *.mli for LICENSE issues
