05d361cc11380fe025dcc8e573280b3b668bfec5
Hongbo Zhang <hzhang295@bloomberg.net>
2016-05-02 13:26:25 -0400

    [refact] clean up bundler script

--- jscomp/tools/boot.sh
+++ jscomp/tools/boot.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-./ocaml_pack depend.ml ocaml_extract.ml ocaml_extract.mli ocaml_pack_main.ml > ocaml_pack.ml
+
+## build bundler first
+ocamlopt.opt unix.cmxa ocamlcommon.cmxa -I +compiler-libs -I +unix ocaml_pack.ml -o ocaml_pack
+
+## bootstrap, if successful, there should be no diff with regard to `ocaml_pack.ml
+./ocaml_pack tools.mllib > ocaml_pack.ml
