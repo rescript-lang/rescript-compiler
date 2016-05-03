228255072de734c744c63570dbd1f1cb85b4e757
Hongbo Zhang <bobzhang1988@gmail.com>
2016-05-03 14:02:20 -0400

    [snapshot] (#334)

--- jscomp/tools/boot.sh
+++ jscomp/tools/boot.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-## build bundler first
-./pack.sh
-## bootstrap, if successful, there should be no diff with regard to `ocaml_pack.ml
-./repack.sh
+
+../bin/ocaml_pack tools.mllib > ./ocaml_pack0.ml
+ocamlopt.opt -w -40 -I +compiler-libs  ./ocaml_pack0.ml -o ./ocaml_pack
+./ocaml_pack tools.mllib > ./ocaml_pack1.ml
+
+if ! diff -q ./ocaml_pack0.ml ./ocaml_pack1.ml > /dev/null  2>&1; then
+    echo "they differ"
+else 
+    echo "fixpoint"
+fi
