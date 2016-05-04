c4b065dc9bcc69387060eaa5c3ae2845da45d3fa
Hongbo Zhang <bobzhang1988@gmail.com>
2016-05-03 21:14:03 -0400

    clean up

--- jscomp/bin/config.ml
+++ jscomp/bin/config.ml
@@ -23,7 +23,7 @@
 (* The main OCaml version string has moved to ../VERSION *)
 let version = Sys.ocaml_version
 
-let standard_library_default = "/Users/hongbozhang/git/ocamlscript/ocaml/lib/ocaml"
+let standard_library_default = ""
 
 let standard_library =
   try
@@ -34,19 +34,19 @@ let standard_library =
   with Not_found ->
     standard_library_default
 
-let standard_runtime = "/Users/hongbozhang/git/ocamlscript/ocaml/bin/ocamlrun"
-let ccomp_type = "cc"
-let bytecomp_c_compiler = "gcc -O  -Wall -D_FILE_OFFSET_BITS=64 -D_REENTRANT -O "
-let bytecomp_c_libraries = "-lcurses -lpthread"
-let native_c_compiler = "gcc -O  -D_FILE_OFFSET_BITS=64 -D_REENTRANT"
+let standard_runtime = ""
+let ccomp_type = ""
+let bytecomp_c_compiler = ""
+let bytecomp_c_libraries = ""
+let native_c_compiler = ""
 let native_c_libraries = ""
-let native_pack_linker = "ld -r -arch x86_64  -o "
+let native_pack_linker = ""
 let ranlib = "ranlib"
 let ar = "ar"
-let cc_profile = "-pg"
-let mkdll = "gcc -bundle -flat_namespace -undefined suppress -Wl,-no_compact_unwind"
-let mkexe = "gcc -Wl,-no_compact_unwind"
-let mkmaindll = "gcc -bundle -flat_namespace -undefined suppress -Wl,-no_compact_unwind"
+let cc_profile = ""
+let mkdll = ""
+let mkexe = ""
+let mkmaindll = ""
 
 let exec_magic_number = "Caml1999X011"
 and cmi_magic_number = "Caml1999I017"
@@ -74,9 +74,9 @@ let stack_threshold = 256 (* see byterun/config.h *)
 
 let architecture = "amd64"
 let model = "default"
-let system = "macosx"
+let system = ""
 
-let asm = "clang -arch x86_64 -c"
+let asm = ""
 let asm_cfi_supported = true
 let with_frame_pointers = false
 
@@ -85,8 +85,8 @@ let ext_asm = ".s"
 let ext_lib = ".a"
 let ext_dll = ".so"
 
-let host = "x86_64-apple-darwin15.0.0"
-let target = "x86_64-apple-darwin15.0.0"
+let host = ""
+let target = ""
 
 let default_executable_name =
   match Sys.os_type with
