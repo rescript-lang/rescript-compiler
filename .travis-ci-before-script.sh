54e8ca8fcc4d33edea17908a6cf2e2aa68b07f42
Hongbo Zhang <hzhang295@bloomberg.net>
2016-05-09 14:12:09 -0400

    [feature] prepare formal npm release

--- .travis-ci-before-script.sh
+++ .travis-ci-before-script.sh
@@ -2,8 +2,8 @@ set -e
 
 git submodule update --init --recursive
 
-cd ocaml &&  ./configure -prefix $(dirname $(pwd))  -no-ocamldoc -no-ocamlbuild && make -j9 world.opt && make install && git clean -dfx && git checkout master && cd ..
+cd ocaml &&  ./configure -prefix $(dirname $(pwd))  -no-ocamldoc -no-ocamlbuild && make -j9 world.opt && make install  && cd ..
 
 export PATH=$(pwd)/bin:$PATH
 
-cd jscomp && make travis-world-test
+cd jscomp && make travis-world-test && make install
