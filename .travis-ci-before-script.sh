4c96d104a38eb82607d4d3ea3d81257b33c0a201
Hongbo Zhang <bobzhang1988@gmail.com>
2016-05-09 15:37:11 -0400

    [feature] prepare formal npm release (#355)
    * [feature] prepare formal npm release
    
    * [fix] install issues
    
    * tweak makefile
    
    * [snapshot] and add env variables for CI
    
    * ready for publish
    
    * [fix] ignore files from travis ci, no bin for bsc since it requires its existence before post-install
    
    * [done]
    
    * [tweak] use specialized env, replace the shell with js file later
    
    * [tweak]
    
    * [tweak]

--- .travis-ci-before-script.sh
+++
@@ -1,9 +0,0 @@
-set -e
-
-git submodule update --init --recursive
-
-cd ocaml &&  ./configure -prefix $(dirname $(pwd))  -no-ocamldoc -no-ocamlbuild && make -j9 world.opt && make install && git clean -dfx && git checkout master && cd ..
-
-export PATH=$(pwd)/bin:$PATH
-
-cd jscomp && make travis-world-test
---
+++ .travis-ci.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -e
+
+if [ $BS_TRAVIS_CI ]
+then 
+    git submodule update --init --recursive
+fi 
+
+export OCAMLPARAM='_,bin-annot=1' 
+export OCAMLRUNPARAM=b
+
+# we encoruage people to install from opam 
+# for our own local installation 
+# it can be minimal
+cd ocaml &&  ./configure -prefix $(dirname $(pwd))  -no-ocamldoc -no-ocamlbuild -no-shared-libs -no-curses -no-graph -no-pthread -no-debugger  && make -j9 world.opt && make install  && cd ..
+
+# we should not rely on git
+# it will break `npm postinstall`
+# if [ ! $BS_TRAVIS_CI ]
+# then 
+#     cd ocaml && git clean -dfx && cd ..
+# fi
+
+export PATH=$(pwd)/bin:$PATH
+
+cd jscomp && make travis-world-test && make install
