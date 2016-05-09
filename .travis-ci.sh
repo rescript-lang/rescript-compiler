9a1f5bdefeaf7cb229a8a96c3dbefb93890b5733
Hongbo Zhang <hzhang295@bloomberg.net>
2016-05-09 15:15:22 -0400

    [done]

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -14,6 +14,11 @@ export OCAMLRUNPARAM=b
 # it can be minimal
 cd ocaml &&  ./configure -prefix $(dirname $(pwd))  -no-ocamldoc -no-ocamlbuild -no-shared-libs -no-curses -no-graph -no-pthread -no-debugger  && make -j9 world.opt && make install  && cd ..
 
+if [ ! $TRAVIS_CI ]
+then 
+    cd ocaml && git clean -dfx && cd ..
+fi
+
 export PATH=$(pwd)/bin:$PATH
 
 cd jscomp && make travis-world-test && make install
