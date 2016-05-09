495df7332b6f0de89a5c0ea201daab9a2168a1d4
Hongbo Zhang <hzhang295@bloomberg.net>
2016-05-09 18:11:04 -0400

    debug prepublish later

--- scripts/prepublish.sh
+++ scripts/prepublish.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 set -e
 
-cd jscomp && watchman watch-del . || true &&  cd ..
-cd jscomp && git clean -dfx && cd ..
-cd ocaml && git clean -dfx && git archive HEAD -o ../ocaml.tar.gz
+# cd jscomp && watchman watch-del . || true &&  cd ..
+# cd jscomp && git clean -dfx && cd ..
+
+# have no idea why the tar.gz is not correct
+cd ocaml && git clean -dfx && git archive HEAD -o ../ocaml.tar.gz && cd ..
 
 
