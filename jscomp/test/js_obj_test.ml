ef1111e67ce80fb5206cd4166b7f5ad291174106
Hongbo Zhang <hzhang295@bloomberg.net>
2016-05-18 14:05:44 -0400

    [fix] fix unit test

--- jscomp/test/js_obj_test.ml
+++ jscomp/test/js_obj_test.ml
@@ -10,14 +10,16 @@ class type  x =
 
 let f  (u : x ) = u # say 32
 
+let f_js u = u##say 32
+
 let suites = Mt.[
   "caml_obj", (fun _ -> Eq (33, f (object method say x = 1 + x end)));
   "js_obj", (fun _ -> let module N = 
     struct
-      external mk : say:'a -> <say:'a> = ""[@@bs.obj] 
+      external mk : say:'a -> <say:'a> Js.t = ""[@@bs.obj] 
     end 
-  in Eq(34, f (N.mk ~say:(fun x -> x + 2)
-            )))
+  in 
+  Eq(34, f_js (N.mk ~say:(fun %uncurry  x -> x + 2))))
 ]
 
 ;; Mt.from_pair_suites __FILE__ suites
