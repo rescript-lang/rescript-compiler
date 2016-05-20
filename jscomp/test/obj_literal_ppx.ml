1a26292ac99ecd6f6106a37d4810d7809400673c
Hongbo Zhang <hzhang295@bloomberg.net>
2016-05-20 14:55:41 -0400

    [feature] extensible record

---
+++ jscomp/test/obj_literal_ppx.ml
@@ -0,0 +1,17 @@
+
+
+let a = { x = 3 ; y = [| 1|]} [@bs.obj]
+
+let b = { x = 3 ; y = [| 1 |]; z = 3; u = fun %uncurry (x,y) -> x + y } [@bs.obj]
+
+let f obj = 
+  obj ## x + Array.length (obj ## y)
+
+let h obj = obj##u (1,2)
+
+let u = f  a
+
+let v = f b 
+
+let vv = h b 
+
