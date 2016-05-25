90ca7b76646f63a035f0f5578d8b87ceee9b371b
Hongbo Zhang <hzhang295@bloomberg.net>
2016-05-25 16:30:20 -0400

    add more examples

---
+++ jscomp/test/nested_obj_literal.ml
@@ -0,0 +1,13 @@
+
+
+let structural_obj = { x = { y = { z = 3 }}} [@bs.obj]
+(* compiler inferred type :
+  val structural_obj : < x : < y : < z : int >  >  > [@bs.obj] *)
+
+type 'a x = {x : 'a }
+type 'a y = {y : 'a}
+type 'a z = { z : 'a}  
+let f_record   =  { x = { y = { z = 3 }}} 
+(* compiler inferred type :
+   val f_record : int z y x *)
+
