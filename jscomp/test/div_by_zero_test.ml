83c6e2a95d7173dc999b9041e9aa3033d1f559a2
Hongbo Zhang <hzhang295@bloomberg.net>
2016-06-24 10:08:58 -0400

    fix optimization bug and add more tests

--- jscomp/test/div_by_zero_test.ml
+++ jscomp/test/div_by_zero_test.ml
@@ -11,7 +11,12 @@ let add suite =
   suites := suite :: !suites
 
 let () = 
-  add (__LOC__, (fun _ -> ThrowAny (fun _ -> ignore (3 / 0))))
+  add (__LOC__, (fun _ -> ThrowAny (fun _ -> ignore (3 / 0))));
+  add (__LOC__, (fun _ -> ThrowAny (fun _ -> ignore (3 mod 0))));
+  add (__LOC__, (fun _ -> ThrowAny (fun _ -> ignore (Int32.div 3l  0l))));
+  add (__LOC__, (fun _ -> ThrowAny (fun _ -> ignore (Int32.rem 3l  0l))));
+  add (__LOC__, (fun _ -> ThrowAny (fun _ -> ignore (Int64.div 3L  0L))));
+  add (__LOC__, (fun _ -> ThrowAny (fun _ -> ignore (Int64.rem 3L  0L))))
 
 let div x y = x / y + 3 
 
