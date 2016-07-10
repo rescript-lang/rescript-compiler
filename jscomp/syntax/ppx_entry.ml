da4bc5d754672afd676489834f0200a1fd5cc400
Hongbo Zhang <hzhang295@bloomberg.net>
2016-07-10 15:07:44 -0400

    add non_exn support (better perf)

--- jscomp/syntax/ppx_entry.ml
+++ jscomp/syntax/ppx_entry.ml
@@ -172,7 +172,8 @@ let handle_typ
   match ty with
   | {ptyp_desc = Ptyp_extension({txt = "bs.obj"}, PTyp ty)}
     -> 
-    Ext_ref.protect obj_type_as_js_obj_type true (fun _ -> self.typ self ty )
+    Ext_ref.non_exn_protect obj_type_as_js_obj_type true 
+      (fun _ -> self.typ self ty )
   | {ptyp_attributes ;
      ptyp_desc = Ptyp_arrow ("", args, body);
      ptyp_loc = loc
@@ -196,7 +197,8 @@ let handle_typ
 
     let check_auto_uncurry core_type = 
       if  !obj_type_auto_uncurry then
-        Ext_ref.protect uncurry_type true (fun _ -> self.typ self core_type  )          
+        Ext_ref.non_exn_protect uncurry_type true 
+          (fun _ -> self.typ self core_type  )          
       else self.typ self core_type in 
     let methods, ptyp_attributes  =
       begin match Ext_list.exclude_with_fact
@@ -224,7 +226,7 @@ let handle_typ
                     Ast_attributes.bs_this :: core_type.ptyp_attributes}
           ) methods , ptyp_attributes
       |  Some _ ,  ptyp_attributes -> 
-        Ext_ref.protect uncurry_type true begin fun _ -> 
+        Ext_ref.non_exn_protect uncurry_type true begin fun _ -> 
           List.map (fun (label, ptyp_attrs, core_type ) -> 
               match Ast_attributes.process_attributes_rev ptyp_attrs with 
               | `Nothing,  _ -> label, ptyp_attrs , self.typ self core_type
@@ -272,9 +274,12 @@ let rec unsafe_mapper : Ast_mapper.mapper =
             begin match payload with 
             | PStr [{pstr_desc = Pstr_eval (e,_)}]
               -> 
-              Ext_ref.protect2 record_as_js_object  obj_type_as_js_obj_type
-                true true
-                (fun ()-> self.expr self e ) 
+              Ext_ref.non_exn_protect2 
+                record_as_js_object
+                obj_type_as_js_obj_type
+                true
+                true
+                (fun () -> self.expr self e ) 
             | _ -> Location.raise_errorf ~loc "Expect an expression here"
             end
         (** End rewriting *)
