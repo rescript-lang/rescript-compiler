a2d0a64062c9b81c83eac2cd091127fc7f451141
Hongbo Zhang <bobzhang1988@gmail.com>
2016-08-11 21:49:59 -0400

    seems to work with js

--- jscomp/js.sh
+++ jscomp/js.sh
@@ -3,17 +3,16 @@ set -e
 ## building js 
 # ocamlbuild -use-ocamlfind -no-hygiene  -syntax camlp4o -pkgs js_of_ocaml,js_of_ocaml.syntax,js_of_ocaml.toplevel exports.byte
 
-ocamlbuild -lflags -no-check-prims -use-ocamlfind -no-hygiene   -pkgs compiler-libs.toplevel -no-links jsoo_exports.byte
-js_of_ocaml -I +compiler-libs --toplevel +dynlink.js +toplevel.js +weak.js  _build/jsoo_exports.byte -o _build/exports.js 
+ocamlbuild -lflags -no-check-prims -use-ocamlfind -no-hygiene   -pkgs compiler-libs.bytecomp -no-links jsoo_exports.byte
+js_of_ocaml -I +compiler-libs --toplevel +dynlink.js +toplevel.js +weak.js  _build/jsoo_exports.byte -I ./runtime/ --file js.cmi:/cmis   -o _build/exports.js 
 
+rm -rf $BUCKLESCRIPT_DOC/js-demo/exports.js && cp _build/exports.js  $BUCKLESCRIPT_DOC/js-demo/
 
-rm -rf $BUCKLESCRIPT_DOC/js-demo/exports.js && cp _build/exports.js $BUCKLESCRIPT_DOC/js-demo/exports.js
-
-ocamlbuild -use-ocamlfind -no-hygiene -no-links js_generate_require.byte --
-rm -rf  $BUCKLESCRIPT_DOC/js-demo/pre_load.js
-cp ./pre_load.js $BUCKLESCRIPT_DOC/js-demo/
-# TODO: build with amd first 
-cp ../lib/amdjs/*.js $BUCKLESCRIPT_DOC/js-demo/stdlib
+# ocamlbuild -use-ocamlfind -no-hygiene -no-links js_generate_require.byte --
+# rm -rf  $BUCKLESCRIPT_DOC/js-demo/pre_load.js
+# cp ./pre_load.js $BUCKLESCRIPT_DOC/js-demo/
+# # TODO: build with amd first 
+# cp ../lib/amdjs/*.js $BUCKLESCRIPT_DOC/js-demo/stdlib
 
 
 
