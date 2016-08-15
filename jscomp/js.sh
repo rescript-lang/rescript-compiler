46a43cbf6dc46ea64f6bd3ee0e2e2991b599d3fa
Hongbo Zhang <bobzhang1988@gmail.com>
2016-08-15 18:06:47 -0400

    experimental support for reason

--- jscomp/js.sh
+++ jscomp/js.sh
@@ -2,11 +2,16 @@
 set -e 
 ## building js 
 # ocamlbuild -use-ocamlfind -no-hygiene  -syntax camlp4o -pkgs js_of_ocaml,js_of_ocaml.syntax,js_of_ocaml.toplevel exports.byte
+FILE=bin/reason.byte
 
-ocamlbuild -lflags -no-check-prims -use-ocamlfind -no-hygiene   -pkgs compiler-libs.bytecomp -no-links jsoo_main.byte
+# only need make a new snapshot when we run
+# git submodule --update
+# bspack bin/reason.mllib > bin/reason.ml
+
+ocamlbuild -lflags -no-check-prims -use-ocamlfind -I bin -no-hygiene   -pkgs compiler-libs.common -no-links $FILE
 
 # jsoo_mkcmis stdlib
-js_of_ocaml -I +compiler-libs   --toplevel +weak.js +toplevel.js  ./polyfill.js _build/jsoo_main.byte -I ./runtime/ --file js.cmi:/cmis/js.cmi --file js_unsafe.cmi:/cmis/js_unsafe.cmi --file js_re.cmi:/cmis/js_re.cmi   -o _build/exports.js 
+js_of_ocaml -I +compiler-libs   --toplevel +weak.js +toplevel.js  ./polyfill.js _build/$FILE -I ./runtime/ --file js.cmi:/cmis/js.cmi --file js_unsafe.cmi:/cmis/js_unsafe.cmi --file js_re.cmi:/cmis/js_re.cmi   -o _build/exports.js 
 
 rm -rf $BUCKLESCRIPT_DOC/js-demo/exports.js && cp _build/exports.js  $BUCKLESCRIPT_DOC/js-demo/
 
