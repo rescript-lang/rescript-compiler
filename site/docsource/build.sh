ed45209d2b4e7b17482cf7b334a9ffe01337d112
Hongbo Zhang <bobzhang1988@gmail.com>
2016-08-26 10:22:40 -0400

    add landing page

--- site/docsource/build.sh
+++ site/docsource/build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e 
 echo "Building" >build.compile
-asciidoctor -a reproducible -D ../../docs/ ./index.adoc 2>> build.compile
+asciidoctor -a reproducible -D ../../docs/ ./Manual.adoc  2>> build.compile
+asciidoctor -a linkcss -a stylesheet! -a reproducible -D ../../docs/ ./index.adoc 2>> build.compile
 echo "Finished" >> build.compile
 reload
