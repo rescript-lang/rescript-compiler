98b4c159fd72b12bf20270af2442b37f5a248f9a
Pavel Volokitin <pavelvolokitin@gmail.com>
2016-10-11 19:04:42 +0500

    Flow: Cleaner examples.

--- jscomp/test_flow/main.ml
+++ jscomp/test_flow/main.ml
@@ -1,45 +1,36 @@
-module M = struct
+module Mod = struct
   type t = int
 end
-module M1 = M
+module Aliased = Mod
 
-let do_if b = if b then true else false
+let tuple = ("string", 1, 2.0, true, false)
 
-let mk_tuple (int1: M1.t) int2 () = (int1 + 1, int2 + 2, "3", '4')
+let aliased: Aliased.t = 1
 
+let js_obj = [%bs.obj { name = "nam"; id = 2 }]
+  
 type rec_t = { name: string; id: int; }
 
-let mk_rec ~name () = { name; id = 1; }
+let record = { name = "rec"; id = 1; }
 
 class type obj_t = object
   method name : string
   method id : int
 end [@bs]
 
-let mk_rec (o : obj_t) =
-  {
-    name = o#name;
-    id = o#id;
-  }
+let obj_arg (o : obj_t) = ()
 
-let mk_obj r = [%bs.obj { name = "nam"; id = 2 }]
-  
-let mk_arr () = [|1; 2; 3|]
+let arr = [|1; 2; 3|]
 
 let from_util (a: Util.t) = ()
-
-let float_ = 1.0
 
 type tt = int
 let tt: tt = 0
 
-let iter items1 items2 fn1 fn2 = Array.iter fn1 items1; Array.iter fn2 items2
+let iter items fn = Array.iter fn items
 
 type 'a pair = 'a * 'a
 
-let pair (a : 'a pair) = (a, a)
+let pair (a : 'a pair) = ()
 
-let fn_in_obj = (
-  (fun a -> pair a),
-  1
-)
+let tuple_with_fn = (fun a -> pair a), 1
