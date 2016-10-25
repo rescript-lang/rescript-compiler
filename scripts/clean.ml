76e9ef125bbd1e87d0b987ddba037a4c12e8b3a4
Hongbo Zhang <bobzhang1988@gmail.com>
2016-10-25 17:08:43 -0400

    add scripts to uninstall ocaml bins after installation

---
+++ scripts/clean.ml
@@ -0,0 +1,28 @@
+
+[@@@bs.config{no_export}]
+module Fs = Node.Fs
+
+
+let _ = 
+  Js.Undefined.bind [%node __dirname] (fun [@bs] dir -> 
+      let bin_dir = Node.Path.join [|dir ; ".."; "bin"|] in 
+      let files = Fs.readdirSync bin_dir in 
+      Js.log ("cleaning now", files);
+      files |> Js.Array.forEach (fun [@bs] f -> 
+          if not @@ Js.to_bool @@ Js.String.startsWith "bs" f 
+             && f <> ".gitignore"
+          then 
+            let p = Node.Path.join [| bin_dir; f|] in
+            try 
+              Js.log ("removing", p, "now");
+              Fs.unlinkSync p 
+            with _ -> Js.log ("removing", p, "failure")
+        );
+      Js.undefined
+    )
+
+
+
+(* local variables: *)
+(* compile-command: "bscc -c clean.ml" *)
+(* end: *)
--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -30,7 +30,12 @@ export PATH=$(pwd)/bin:$PATH
 
 if [ $BS_TRAVIS_CI ]
 then 
-    cd jscomp && make travis-world-test && make install
+    cd jscomp && make travis-world-test && make install && cd ..
 else 
-    cd jscomp && make world && make install
+    cd jscomp && make world && make install && cd ..
 fi
+
+# uninstall ocaml
+
+node scripts/clean.js
+
