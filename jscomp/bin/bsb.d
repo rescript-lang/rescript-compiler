qs9816/bucklescript

switch to makefile instead of ocamlbuild

jscomp/bin/bsb.d
@@ -1,35 +1,35 @@
-bsb.ml : ext/ext_pervasives.ml
-bsb.ml : ext/ext_pervasives.mli
-bsb.ml : ext/ext_bytes.ml
-bsb.ml : ext/ext_bytes.mli
-bsb.ml : ext/ext_string.ml
-bsb.ml : ext/ext_string.mli
-bsb.ml : ext/literals.ml
-bsb.ml : ext/literals.mli
-bsb.ml : ext/ext_filename.ml
-bsb.ml : ext/ext_filename.mli
-bsb.ml : ext/string_map.ml
-bsb.ml : ext/string_map.mli
-bsb.ml : depends/binary_cache.ml
-bsb.ml : depends/binary_cache.mli
-bsb.ml : ./bs_build_schemas.ml
-bsb.ml : ./bs_dir.ml
-bsb.ml : ./bs_dir.mli
-bsb.ml : ext/ext_array.ml
-bsb.ml : ext/ext_array.mli
-bsb.ml : ./bs_json.ml
-bsb.ml : ./bs_json.mli
-bsb.ml : ext/ext_file_pp.ml
-bsb.ml : ext/ext_file_pp.mli
-bsb.ml : ./bs_build_ui.ml
-bsb.ml : ./bs_build_ui.mli
-bsb.ml : ./bs_dep_infos.ml
-bsb.ml : ./bs_dep_infos.mli
-bsb.ml : ext/string_set.ml
-bsb.ml : ext/string_set.mli
-bsb.ml : ./bs_ninja.ml
-bsb.ml : ./bs_ninja.mli
-bsb.ml : ext/ext_list.ml
-bsb.ml : ext/ext_list.mli
-bsb.ml : ./bsb_main.ml
-bsb.ml : ./bsb_main.mli
+bin/bsb.ml : ext/ext_pervasives.ml
+bin/bsb.ml : ext/ext_pervasives.mli
+bin/bsb.ml : ext/ext_bytes.ml
+bin/bsb.ml : ext/ext_bytes.mli
+bin/bsb.ml : ext/ext_string.ml
+bin/bsb.ml : ext/ext_string.mli
+bin/bsb.ml : ext/literals.ml
+bin/bsb.ml : ext/literals.mli
+bin/bsb.ml : ext/ext_filename.ml
+bin/bsb.ml : ext/ext_filename.mli
+bin/bsb.ml : ext/string_map.ml
+bin/bsb.ml : ext/string_map.mli
+bin/bsb.ml : depends/binary_cache.ml
+bin/bsb.ml : depends/binary_cache.mli
+bin/bsb.ml : core/bs_build_schemas.ml
+bin/bsb.ml : core/bs_dir.ml
+bin/bsb.ml : core/bs_dir.mli
+bin/bsb.ml : ext/ext_array.ml
+bin/bsb.ml : ext/ext_array.mli
+bin/bsb.ml : core/bs_json.ml
+bin/bsb.ml : core/bs_json.mli
+bin/bsb.ml : ext/ext_file_pp.ml
+bin/bsb.ml : ext/ext_file_pp.mli
+bin/bsb.ml : core/bs_build_ui.ml
+bin/bsb.ml : core/bs_build_ui.mli
+bin/bsb.ml : core/bs_dep_infos.ml
+bin/bsb.ml : core/bs_dep_infos.mli
+bin/bsb.ml : ext/string_set.ml
+bin/bsb.ml : ext/string_set.mli
+bin/bsb.ml : core/bs_ninja.ml
+bin/bsb.ml : core/bs_ninja.mli
+bin/bsb.ml : ext/ext_list.ml
+bin/bsb.ml : ext/ext_list.mli
+bin/bsb.ml : core/bsb_main.ml
+bin/bsb.ml : core/bsb_main.mli

jscomp/bin/bspack.d
@@ -1,58 +1,58 @@
-bspack.ml : ../ocaml/utils/terminfo.ml
-bspack.ml : ../ocaml/utils/terminfo.mli
-bspack.ml : ../ocaml/utils/warnings.ml
-bspack.ml : ../ocaml/utils/warnings.mli
-bspack.ml : ../ocaml/parsing/location.ml
-bspack.ml : ../ocaml/parsing/location.mli
-bspack.ml : ../ocaml/parsing/asttypes.mli
-bspack.ml : ../ocaml/utils/misc.ml
-bspack.ml : ../ocaml/utils/misc.mli
-bspack.ml : ../ocaml/parsing/longident.ml
-bspack.ml : ../ocaml/parsing/longident.mli
-bspack.ml : ../ocaml/parsing/parsetree.mli
-bspack.ml : depends/binary_ast.ml
-bspack.ml : depends/binary_ast.mli
-bspack.ml : ext/ext_pervasives.ml
-bspack.ml : ext/ext_pervasives.mli
-bspack.ml : ext/ext_bytes.ml
-bspack.ml : ext/ext_bytes.mli
-bspack.ml : ext/ext_string.ml
-bspack.ml : ext/ext_string.mli
-bspack.ml : ext/literals.ml
-bspack.ml : ext/literals.mli
-bspack.ml : ext/ext_filename.ml
-bspack.ml : ext/ext_filename.mli
-bspack.ml : ext/string_map.ml
-bspack.ml : ext/string_map.mli
-bspack.ml : depends/binary_cache.ml
-bspack.ml : depends/binary_cache.mli
-bspack.ml : common/bs_exception.ml
-bspack.ml : common/bs_exception.mli
-bspack.ml : ../ocaml/utils/clflags.ml
-bspack.ml : ../ocaml/utils/clflags.mli
-bspack.ml : ../ocaml/parsing/depend.ml
-bspack.ml : ../ocaml/parsing/depend.mli
-bspack.ml : ext/ext_format.ml
-bspack.ml : ext/ext_format.mli
-bspack.ml : ext/ext_list.ml
-bspack.ml : ext/ext_list.mli
-bspack.ml : common/js_config.ml
-bspack.ml : common/js_config.mli
-bspack.ml : depends/ast_extract.ml
-bspack.ml : depends/ast_extract.mli
-bspack.ml : ext/ext_io.ml
-bspack.ml : ext/ext_io.mli
-bspack.ml : ../ocaml/parsing/docstrings.ml
-bspack.ml : ../ocaml/parsing/docstrings.mli
-bspack.ml : ../ocaml/parsing/ast_helper.ml
-bspack.ml : ../ocaml/parsing/ast_helper.mli
-bspack.ml : ../ocaml/parsing/syntaxerr.ml
-bspack.ml : ../ocaml/parsing/syntaxerr.mli
-bspack.ml : ../ocaml/parsing/parser.ml
-bspack.ml : ../ocaml/parsing/parser.mli
-bspack.ml : ../ocaml/parsing/lexer.ml
-bspack.ml : ../ocaml/parsing/lexer.mli
-bspack.ml : ../ocaml/parsing/parse.ml
-bspack.ml : ../ocaml/parsing/parse.mli
-bspack.ml : ./bspack_main.ml
-bspack.ml : ./bspack_main.mli
+bin/bspack.ml : ../ocaml/utils/terminfo.ml
+bin/bspack.ml : ../ocaml/utils/terminfo.mli
+bin/bspack.ml : ../ocaml/utils/warnings.ml
+bin/bspack.ml : ../ocaml/utils/warnings.mli
+bin/bspack.ml : ../ocaml/parsing/location.ml
+bin/bspack.ml : ../ocaml/parsing/location.mli
+bin/bspack.ml : ../ocaml/parsing/asttypes.mli
+bin/bspack.ml : ../ocaml/utils/misc.ml
+bin/bspack.ml : ../ocaml/utils/misc.mli
+bin/bspack.ml : ../ocaml/parsing/longident.ml
+bin/bspack.ml : ../ocaml/parsing/longident.mli
+bin/bspack.ml : ../ocaml/parsing/parsetree.mli
+bin/bspack.ml : depends/binary_ast.ml
+bin/bspack.ml : depends/binary_ast.mli
+bin/bspack.ml : ext/ext_pervasives.ml
+bin/bspack.ml : ext/ext_pervasives.mli
+bin/bspack.ml : ext/ext_bytes.ml
+bin/bspack.ml : ext/ext_bytes.mli
+bin/bspack.ml : ext/ext_string.ml
+bin/bspack.ml : ext/ext_string.mli
+bin/bspack.ml : ext/literals.ml
+bin/bspack.ml : ext/literals.mli
+bin/bspack.ml : ext/ext_filename.ml
+bin/bspack.ml : ext/ext_filename.mli
+bin/bspack.ml : ext/string_map.ml
+bin/bspack.ml : ext/string_map.mli
+bin/bspack.ml : depends/binary_cache.ml
+bin/bspack.ml : depends/binary_cache.mli
+bin/bspack.ml : common/bs_exception.ml
+bin/bspack.ml : common/bs_exception.mli
+bin/bspack.ml : ../ocaml/utils/clflags.ml
+bin/bspack.ml : ../ocaml/utils/clflags.mli
+bin/bspack.ml : ../ocaml/parsing/depend.ml
+bin/bspack.ml : ../ocaml/parsing/depend.mli
+bin/bspack.ml : ext/ext_format.ml
+bin/bspack.ml : ext/ext_format.mli
+bin/bspack.ml : ext/ext_list.ml
+bin/bspack.ml : ext/ext_list.mli
+bin/bspack.ml : common/js_config.ml
+bin/bspack.ml : common/js_config.mli
+bin/bspack.ml : depends/ast_extract.ml
+bin/bspack.ml : depends/ast_extract.mli
+bin/bspack.ml : ext/ext_io.ml
+bin/bspack.ml : ext/ext_io.mli
+bin/bspack.ml : ../ocaml/parsing/docstrings.ml
+bin/bspack.ml : ../ocaml/parsing/docstrings.mli
+bin/bspack.ml : ../ocaml/parsing/ast_helper.ml
+bin/bspack.ml : ../ocaml/parsing/ast_helper.mli
+bin/bspack.ml : ../ocaml/parsing/syntaxerr.ml
+bin/bspack.ml : ../ocaml/parsing/syntaxerr.mli
+bin/bspack.ml : ../ocaml/parsing/parser.ml
+bin/bspack.ml : ../ocaml/parsing/parser.mli
+bin/bspack.ml : ../ocaml/parsing/lexer.ml
+bin/bspack.ml : ../ocaml/parsing/lexer.mli
+bin/bspack.ml : ../ocaml/parsing/parse.ml
+bin/bspack.ml : ../ocaml/parsing/parse.mli
+bin/bspack.ml : core/bspack_main.ml
+bin/bspack.ml : core/bspack_main.mli

rescript-lang/rescript-compiler

jscomp/bin/bsb.d
@@ -1,35 +1,35 @@
-bsb.ml : ext/ext_pervasives.ml
-bsb.ml : ext/ext_pervasives.mli
-bsb.ml : ext/ext_bytes.ml
-bsb.ml : ext/ext_bytes.mli
-bsb.ml : ext/ext_string.ml
-bsb.ml : ext/ext_string.mli
-bsb.ml : ext/literals.ml
-bsb.ml : ext/literals.mli
-bsb.ml : ext/ext_filename.ml
-bsb.ml : ext/ext_filename.mli
-bsb.ml : ext/string_map.ml
-bsb.ml : ext/string_map.mli
-bsb.ml : depends/binary_cache.ml
-bsb.ml : depends/binary_cache.mli
-bsb.ml : ./bs_build_schemas.ml
-bsb.ml : ./bs_dir.ml
-bsb.ml : ./bs_dir.mli
-bsb.ml : ext/ext_array.ml
-bsb.ml : ext/ext_array.mli
-bsb.ml : ./bs_json.ml
-bsb.ml : ./bs_json.mli
-bsb.ml : ext/ext_file_pp.ml
-bsb.ml : ext/ext_file_pp.mli
-bsb.ml : ./bs_build_ui.ml
-bsb.ml : ./bs_build_ui.mli
-bsb.ml : ./bs_dep_infos.ml
-bsb.ml : ./bs_dep_infos.mli
-bsb.ml : ext/string_set.ml
-bsb.ml : ext/string_set.mli
-bsb.ml : ./bs_ninja.ml
-bsb.ml : ./bs_ninja.mli
-bsb.ml : ext/ext_list.ml
-bsb.ml : ext/ext_list.mli
-bsb.ml : ./bsb_main.ml
-bsb.ml : ./bsb_main.mli
+bin/bsb.ml : ext/ext_pervasives.ml
+bin/bsb.ml : ext/ext_pervasives.mli
+bin/bsb.ml : ext/ext_bytes.ml
+bin/bsb.ml : ext/ext_bytes.mli
+bin/bsb.ml : ext/ext_string.ml
+bin/bsb.ml : ext/ext_string.mli
+bin/bsb.ml : ext/literals.ml
+bin/bsb.ml : ext/literals.mli
+bin/bsb.ml : ext/ext_filename.ml
+bin/bsb.ml : ext/ext_filename.mli
+bin/bsb.ml : ext/string_map.ml
+bin/bsb.ml : ext/string_map.mli
+bin/bsb.ml : common/binary_cache.ml
+bin/bsb.ml : common/binary_cache.mli
+bin/bsb.ml : bsb/bs_build_schemas.ml
+bin/bsb.ml : bsb/bs_dir.ml
+bin/bsb.ml : bsb/bs_dir.mli
+bin/bsb.ml : ext/ext_array.ml
+bin/bsb.ml : ext/ext_array.mli
+bin/bsb.ml : bsb/bs_json.ml
+bin/bsb.ml : bsb/bs_json.mli
+bin/bsb.ml : ext/ext_file_pp.ml
+bin/bsb.ml : ext/ext_file_pp.mli
+bin/bsb.ml : bsb/bs_build_ui.ml
+bin/bsb.ml : bsb/bs_build_ui.mli
+bin/bsb.ml : bsb/bs_dep_infos.ml
+bin/bsb.ml : bsb/bs_dep_infos.mli
+bin/bsb.ml : ext/string_set.ml
+bin/bsb.ml : ext/string_set.mli
+bin/bsb.ml : bsb/bs_ninja.ml
+bin/bsb.ml : bsb/bs_ninja.mli
+bin/bsb.ml : ext/ext_list.ml
+bin/bsb.ml : ext/ext_list.mli
+bin/bsb.ml : ./bsb_main.ml
+bin/bsb.ml : ./bsb_main.mli

jscomp/bin/bspack.d
@@ -1,58 +1,58 @@
-bspack.ml : ../ocaml/utils/terminfo.ml
-bspack.ml : ../ocaml/utils/terminfo.mli
-bspack.ml : ../ocaml/utils/warnings.ml
-bspack.ml : ../ocaml/utils/warnings.mli
-bspack.ml : ../ocaml/parsing/location.ml
-bspack.ml : ../ocaml/parsing/location.mli
-bspack.ml : ../ocaml/parsing/asttypes.mli
-bspack.ml : ../ocaml/utils/misc.ml
-bspack.ml : ../ocaml/utils/misc.mli
-bspack.ml : ../ocaml/parsing/longident.ml
-bspack.ml : ../ocaml/parsing/longident.mli
-bspack.ml : ../ocaml/parsing/parsetree.mli
-bspack.ml : depends/binary_ast.ml
-bspack.ml : depends/binary_ast.mli
-bspack.ml : ext/ext_pervasives.ml
-bspack.ml : ext/ext_pervasives.mli
-bspack.ml : ext/ext_bytes.ml
-bspack.ml : ext/ext_bytes.mli
-bspack.ml : ext/ext_string.ml
-bspack.ml : ext/ext_string.mli
-bspack.ml : ext/literals.ml
-bspack.ml : ext/literals.mli
-bspack.ml : ext/ext_filename.ml
-bspack.ml : ext/ext_filename.mli
-bspack.ml : ext/string_map.ml
-bspack.ml : ext/string_map.mli
-bspack.ml : depends/binary_cache.ml
-bspack.ml : depends/binary_cache.mli
-bspack.ml : common/bs_exception.ml
-bspack.ml : common/bs_exception.mli
-bspack.ml : ../ocaml/utils/clflags.ml
-bspack.ml : ../ocaml/utils/clflags.mli
-bspack.ml : ../ocaml/parsing/depend.ml
-bspack.ml : ../ocaml/parsing/depend.mli
-bspack.ml : ext/ext_format.ml
-bspack.ml : ext/ext_format.mli
-bspack.ml : ext/ext_list.ml
-bspack.ml : ext/ext_list.mli
-bspack.ml : common/js_config.ml
-bspack.ml : common/js_config.mli
-bspack.ml : depends/ast_extract.ml
-bspack.ml : depends/ast_extract.mli
-bspack.ml : ext/ext_io.ml
-bspack.ml : ext/ext_io.mli
-bspack.ml : ../ocaml/parsing/docstrings.ml
-bspack.ml : ../ocaml/parsing/docstrings.mli
-bspack.ml : ../ocaml/parsing/ast_helper.ml
-bspack.ml : ../ocaml/parsing/ast_helper.mli
-bspack.ml : ../ocaml/parsing/syntaxerr.ml
-bspack.ml : ../ocaml/parsing/syntaxerr.mli
-bspack.ml : ../ocaml/parsing/parser.ml
-bspack.ml : ../ocaml/parsing/parser.mli
-bspack.ml : ../ocaml/parsing/lexer.ml
-bspack.ml : ../ocaml/parsing/lexer.mli
-bspack.ml : ../ocaml/parsing/parse.ml
-bspack.ml : ../ocaml/parsing/parse.mli
-bspack.ml : ./bspack_main.ml
-bspack.ml : ./bspack_main.mli
+bin/bspack.ml : ../ocaml/utils/terminfo.ml
+bin/bspack.ml : ../ocaml/utils/terminfo.mli
+bin/bspack.ml : ../ocaml/utils/warnings.ml
+bin/bspack.ml : ../ocaml/utils/warnings.mli
+bin/bspack.ml : ../ocaml/parsing/location.ml
+bin/bspack.ml : ../ocaml/parsing/location.mli
+bin/bspack.ml : ../ocaml/parsing/asttypes.mli
+bin/bspack.ml : ../ocaml/utils/misc.ml
+bin/bspack.ml : ../ocaml/utils/misc.mli
+bin/bspack.ml : ../ocaml/parsing/longident.ml
+bin/bspack.ml : ../ocaml/parsing/longident.mli
+bin/bspack.ml : ../ocaml/parsing/parsetree.mli
+bin/bspack.ml : common/binary_ast.ml
+bin/bspack.ml : common/binary_ast.mli
+bin/bspack.ml : ext/ext_pervasives.ml
+bin/bspack.ml : ext/ext_pervasives.mli
+bin/bspack.ml : ext/ext_bytes.ml
+bin/bspack.ml : ext/ext_bytes.mli
+bin/bspack.ml : ext/ext_string.ml
+bin/bspack.ml : ext/ext_string.mli
+bin/bspack.ml : ext/literals.ml
+bin/bspack.ml : ext/literals.mli
+bin/bspack.ml : ext/ext_filename.ml
+bin/bspack.ml : ext/ext_filename.mli
+bin/bspack.ml : ext/string_map.ml
+bin/bspack.ml : ext/string_map.mli
+bin/bspack.ml : common/binary_cache.ml
+bin/bspack.ml : common/binary_cache.mli
+bin/bspack.ml : common/bs_exception.ml
+bin/bspack.ml : common/bs_exception.mli
+bin/bspack.ml : ../ocaml/utils/clflags.ml
+bin/bspack.ml : ../ocaml/utils/clflags.mli
+bin/bspack.ml : ../ocaml/parsing/depend.ml
+bin/bspack.ml : ../ocaml/parsing/depend.mli
+bin/bspack.ml : ext/ext_format.ml
+bin/bspack.ml : ext/ext_format.mli
+bin/bspack.ml : ext/ext_list.ml
+bin/bspack.ml : ext/ext_list.mli
+bin/bspack.ml : common/js_config.ml
+bin/bspack.ml : common/js_config.mli
+bin/bspack.ml : depends/ast_extract.ml
+bin/bspack.ml : depends/ast_extract.mli
+bin/bspack.ml : ext/ext_io.ml
+bin/bspack.ml : ext/ext_io.mli
+bin/bspack.ml : ../ocaml/parsing/docstrings.ml
+bin/bspack.ml : ../ocaml/parsing/docstrings.mli
+bin/bspack.ml : ../ocaml/parsing/ast_helper.ml
+bin/bspack.ml : ../ocaml/parsing/ast_helper.mli
+bin/bspack.ml : ../ocaml/parsing/syntaxerr.ml
+bin/bspack.ml : ../ocaml/parsing/syntaxerr.mli
+bin/bspack.ml : ../ocaml/parsing/parser.ml
+bin/bspack.ml : ../ocaml/parsing/parser.mli
+bin/bspack.ml : ../ocaml/parsing/lexer.ml
+bin/bspack.ml : ../ocaml/parsing/lexer.mli
+bin/bspack.ml : ../ocaml/parsing/parse.ml
+bin/bspack.ml : ../ocaml/parsing/parse.mli
+bin/bspack.ml : core/bspack_main.ml
+bin/bspack.ml : core/bspack_main.mli