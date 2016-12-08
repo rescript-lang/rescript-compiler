a8e136bf013991aaa291f7aa0f75dfb9fb32775f
Hongbo Zhang <bobzhang1988@gmail.com>
2016-12-08 15:12:02 +0000

    remove coverage which takes too much time

--- jscomp/test/gpr_974_test.js
+++ jscomp/test/gpr_974_test.js
@@ -4,7 +4,7 @@ var Caml_obj                = require("../../lib/js/caml_obj");
 var Caml_builtin_exceptions = require("../../lib/js/caml_builtin_exceptions");
 var Js_primitive            = require("../../lib/js/js_primitive");
 
-if (!Caml_obj.caml_equal(Js_primitive.js_from_def(""), /* Some */[""])) {
+if (!Caml_obj.caml_equal(Js_primitive.js_from_nullable_def(""), /* Some */[""])) {
   throw [
         Caml_builtin_exceptions.assert_failure,
         [
@@ -15,12 +15,23 @@ if (!Caml_obj.caml_equal(Js_primitive.js_from_def(""), /* Some */[""])) {
       ];
 }
 
-if (!Caml_obj.caml_equal(Js_primitive.js_from_nullable(""), /* Some */[""])) {
+if (!Caml_obj.caml_equal(Js_primitive.js_from_def(""), /* Some */[""])) {
   throw [
         Caml_builtin_exceptions.assert_failure,
         [
           "gpr_974_test.ml",
           6,
+          4
+        ]
+      ];
+}
+
+if (!Caml_obj.caml_equal(Js_primitive.js_from_nullable(""), /* Some */[""])) {
+  throw [
+        Caml_builtin_exceptions.assert_failure,
+        [
+          "gpr_974_test.ml",
+          7,
           4
         ]
       ];
