5dea11bcc3bcd6d961184a81623da48b186af6f1
Hongbo Zhang <bobzhang1988@gmail.com>
2016-12-16 09:11:45 -0500

    windows support

---
+++ scripts/build_util.js
@@ -0,0 +1,116 @@
+var child_process = require('child_process')
+var process = require('process')
+var fs = require('fs')
+var path = require('path')
+var os = require('os')
+
+var dest_bin = path.join(__dirname, '..', 'bin')
+var dest_lib = path.join(__dirname,'..','lib','ocaml')
+
+
+var jscomp = path.join(__dirname, '..', 'jscomp')
+var jscomp_bin = path.join(jscomp, 'bin')
+
+
+/*
+ * It is weird reaname cause Busy resource or lock error
+ */
+function copyFile(file,target){
+    var stat = fs.statSync(file)
+    fs.createReadStream(file).pipe(
+	fs.createWriteStream(target,
+			     {mode : stat.mode}))
+    
+}
+
+
+/**
+ * @param {string} from 
+ * @param {string} to 
+ */
+function renameAsync(from,to){
+    console.log(from , '----->', to)
+    fs.rename(from,to)
+    // fs.renameSync(from,to)
+
+}
+
+/**
+ * @param {string} from
+ * @param {string} to
+ */
+function poor_copy_sync(from,to){
+    console.log(from , '----->', to)
+    fs.renameSync(from,to)
+    // fs.renameSync(from,to)
+
+}
+
+/**
+ * @param {string} dir 
+ * @param {string} dest_lib
+ */
+function install_directory(dir,dest_lib){
+    var files = fs.readdirSync(dir)
+    files.forEach(function(file){
+	var installed_fmt = ['.cmt','.cmti', '.cmj','.ml','.mli','.cmi']
+	var format_file = path.parse(file)
+	if(format_file && 
+	   (installed_fmt.indexOf(format_file.ext) !== -1)
+	  ){
+	    var from = path.join(dir,file)
+	    var to = path.join(dest_lib,file)
+	    renameAsync(from,to)
+	}
+    })
+
+}
+function install(){
+    if(!fs.existsSync(dest_bin)){
+	fs.mkdirSync(dest_bin)
+    }
+    if(!fs.existsSync(dest_lib)){
+	fs.mkdirSync(dest_lib)
+    }
+
+
+
+    var jscomp_runtime = path.join(jscomp,'runtime')
+
+    files = fs.readdirSync(jscomp_runtime)
+    files.forEach(function(file){
+	var format_file = path.parse(file)
+	var special_files = ['js', 'js_unsafe', 'js_null', 'js_undefined']
+	var installed_fmt = ['.cmt','.cmti', '.cmj']
+	if( 
+	    format_file &&
+		((special_files.indexOf(format_file.name) !== -1)
+		 ||
+		 (installed_fmt.indexOf(format_file.ext) !== -1))
+	  ){
+	    var from = path.join(jscomp_runtime,file)
+	    var to = path.join(dest_lib,file)
+	    renameAsync(from,to)
+	}
+    })
+
+    install_directory(path.join(jscomp,'stdlib'), dest_lib)
+    install_directory(path.join(jscomp,'others'), dest_lib)
+
+    var files = fs.readdirSync(jscomp_bin)
+    files.forEach(function(file){
+	var format_file = path.parse(file)
+	if(format_file &&
+	   format_file.ext === '.exe' || 
+	   format_file.ext === '.js'
+	  ){
+	    var from = path.join(jscomp_bin,file)
+	    var to = path.join(dest_bin,file)
+	    copyFile(from,to)
+	}
+
+    })
+}
+
+exports.install =  install;
+exports.poor_copy_sync = poor_copy_sync;
---
+++ scripts/win_check.js
@@ -0,0 +1,70 @@
+var child_process = require('child_process')
+var process = require('process')
+var fs = require('fs')
+var path = require('path')
+var os = require('os')
+
+var is_windows = ! (os.type().indexOf('Windows') < 0)
+var jscomp = path.join(__dirname,'..','jscomp')
+var working_dir = process.cwd()
+console.log("Working dir", working_dir)
+var working_config = {cwd: jscomp, stdio:[0,1,2]}
+
+
+child_process.execSync('make -j4 -B -C bin all', working_config)
+
+var result = child_process.execSync('make -B -j1 --dry-run libs',{cwd:jscomp,encoding:'utf8'})
+
+
+var jobs = 
+    result.split('\n').reduce(function(acc,line){
+
+    if(line.indexOf('Entering') !== -1){
+	var line_segs = line.split(' ')
+	var new_path = 
+	    path.parse(line_segs[line_segs.length - 1].slice(1,-1))
+	acc.push({base : new_path.base, jobs : []})
+	return acc
+    } else if(line.indexOf('bsc.exe') !== -1){
+	var current_job = acc [acc.length - 1]
+	var new_line = line
+	if(current_job.base==='stdlib'){
+	    var shell_commands =  line.split('`')
+	    if( shell_commands.length === 3){
+		var command = shell_commands[1]
+		console.log('-')
+		var command_output = 
+		    child_process.execSync('sh ' + command, {cwd: path.join(jscomp, current_job.base), encoding : 'utf8'})
+		new_line = shell_commands[0] + ' ' + command_output.trim() + ' ' + shell_commands[2]
+		// console.log('command output', command_output)
+	    } else {
+		console.log('weird > ', shell_commands)
+	    }
+
+	}
+	current_job.jobs.push(new_line);
+	return acc
+    } else {
+	return acc 
+    } 
+},[])
+
+
+var bat_commands = jobs.map(function(current){
+    return `cd ${current.base}\n` + current.jobs.map(function(job)
+    						     {return job.replace(/-bs-diagnose/g,' ').replace(/\//g,'\\')}).join('\n')   + `\ncd ..`
+}
+).join('\n')
+
+fs.writeFileSync(path.join(__dirname,'win_build.bat'), bat_commands, 'utf8')
+
+// rename exe to .win
+fs.readdirSync(path.join(jscomp,'bin')).forEach(function(f){
+    var last_index = f.lastIndexOf('.exe')
+    if(last_index !== -1){
+	var new_file = f.slice(0,-4) + ".win"
+	console.log( f + " --> " + new_file)
+	fs.renameSync(path.join(jscomp, 'bin',f), path.join(jscomp,'bin',new_file))
+    } 
+})
+
