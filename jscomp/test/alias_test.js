f5a8fd4118cff82e195afbd6b280715a80bb6512
Hongbo Zhang <bobzhang1988@gmail.com>
2016-12-22 16:51:53 -0500

    wip

--- jscomp/test/alias_test.js
+++ jscomp/test/alias_test.js
@@ -5,11 +5,13 @@ var a0 = "hello ";
 
 var a10 = a0 + "world";
 
+var a20 = a10 + "not";
+
 function f(x) {
   return (111 + x | 0) + 111 | 0;
 }
 
-var b10 = 112;
+var b20 = 112;
 
 var a1 = a0;
 
@@ -28,6 +30,24 @@ var a7 = a0;
 var a8 = a0;
 
 var a9 = a0;
+
+var a11 = a10;
+
+var a12 = a10;
+
+var a13 = a10;
+
+var a14 = a10;
+
+var a15 = a10;
+
+var a16 = a10;
+
+var a17 = a10;
+
+var a18 = a10;
+
+var a19 = a10;
 
 var b0 = 111;
 
@@ -49,6 +69,26 @@ var b8 = 111;
 
 var b9 = 111;
 
+var b10 = 111;
+
+var b11 = 111;
+
+var b12 = 111;
+
+var b13 = 111;
+
+var b14 = 111;
+
+var b15 = 111;
+
+var b16 = 111;
+
+var b17 = 111;
+
+var b18 = 111;
+
+var b19 = 111;
+
 exports.a0  = a0;
 exports.a1  = a1;
 exports.a2  = a2;
@@ -60,6 +100,16 @@ exports.a7  = a7;
 exports.a8  = a8;
 exports.a9  = a9;
 exports.a10 = a10;
+exports.a11 = a11;
+exports.a12 = a12;
+exports.a13 = a13;
+exports.a14 = a14;
+exports.a15 = a15;
+exports.a16 = a16;
+exports.a17 = a17;
+exports.a18 = a18;
+exports.a19 = a19;
+exports.a20 = a20;
 exports.b0  = b0;
 exports.b1  = b1;
 exports.b2  = b2;
@@ -72,4 +122,14 @@ exports.b8  = b8;
 exports.b9  = b9;
 exports.f   = f;
 exports.b10 = b10;
+exports.b11 = b11;
+exports.b12 = b12;
+exports.b13 = b13;
+exports.b14 = b14;
+exports.b15 = b15;
+exports.b16 = b16;
+exports.b17 = b17;
+exports.b18 = b18;
+exports.b19 = b19;
+exports.b20 = b20;
 /* No side effect */
--- jscomp/test/alias_test.ml
+++ jscomp/test/alias_test.ml
@@ -23,6 +23,25 @@ let a9 = a8
 
 let a10 = a9 ^ "world" 
 
+let a11 = a10 
+
+let a12 = a11
+
+let a13 = a12 
+
+let a14 = a13 
+
+let a15 = a14 
+
+let a16 = a15
+
+let a17 = a16
+
+let a18 = a17
+
+let a19 = a18 
+
+let a20 = a19 ^ "not"
 
 let b0 = 111
 
@@ -43,6 +62,28 @@ let b7 = b6
 let b8 = b7
 
 let b9 = b8 
+
 let f x = 
   b9 + x + b8 
-let b10 = b9 + 1
+
+let b10 = b9 
+
+let b11 = b10 
+
+let b12 = b11
+
+let b13 = b12 
+
+let b14 = b13 
+
+let b15 = b14 
+
+let b16 = b15
+
+let b17 = b16
+
+let b18 = b17
+
+let b19 = b18 
+
+let b20 = b19 + 1
