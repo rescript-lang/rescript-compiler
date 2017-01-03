db02d72dc246c3faa8f4b3bc74d0e7742b378d5f
Hongbo Zhang <bobzhang1988@gmail.com>
2017-01-03 10:43:25 -0500

    more unit tests for sexp parser

--- jscomp/core/script.ml
+++ jscomp/core/script.ml
@@ -12,32 +12,3 @@ let print_position fmt (pos : Lexing.position) =
 #install_printer print_position;;
 
 Bs_json.parse_json_from_file "../bsconfig.json";;
-#mod_use "sexp_lexer.ml";;
-#mod_use "sexp_eval.ml";;
-
-#install_printer Sexp_eval.print_env;;
-#install_printer Sexp_eval.print;;
-(try ignore @@ Sexp_lexer.token (Lexing.from_string {|  (1 2 3  ( a (b) ) |}); 
-   raise Not_found  with e -> ());;
-
-Sexp_lexer.token (Lexing.from_string {|    ( a  ) |});;
-Sexp_lexer.token (Lexing.from_string {|    ( a () ) |});;
-Sexp_lexer.token (Lexing.from_string {|    ( a (b) ) |});;
-
-Sexp_lexer.token (Lexing.from_string {|    ( a (b) ) (c d) |});;
-Sexp_lexer.token (Lexing.from_string {|    ( a (b 1 2 3)  c  d) (c d) |});;
-
-
-Sexp_eval.eval_string {|
-(setq 
- bsc 
- "../bin/bsc.exe"
- bs-external-includes
- '( "../runtime" "../stdlib"  "./others")
- bs-package-name
- "bs-platform")
-
-(setq 
- bsc-flags '("-w" "-40" "-bs-no-version-header " "-bs-diagnose" "-bs-cross-module-opt"))
-
-|}
--- jscomp/ext/ext_sexp.ml
+++ jscomp/ext/ext_sexp.ml
@@ -509,10 +509,12 @@ let
         paren_depth = 0; 
         top = [];
         has_prime = false } lexbuf
+    let from_string str = 
+      token (Lexing.from_string str)    
     let from_file file = 
       let in_channel =  open_in file in 
       match  token (Lexing.from_channel in_channel) with 
       | exception  e -> close_in in_channel; raise e 
       | sexps -> close_in in_channel ; sexps
 
-# 519 "ext/ext_sexp.ml"
+# 521 "ext/ext_sexp.ml"
