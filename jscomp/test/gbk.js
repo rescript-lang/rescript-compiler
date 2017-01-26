9bde38026cda6a3c79998d1b0dbc798fc7558bc3
Hongbo Zhang <bobzhang1988@gmail.com>
2017-01-26 09:38:32 -0500

    add some unicodes for future test reference

---
+++ jscomp/test/gbk.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+console.log("\xe4\xbd\xa0\xe5\xa5\xbd");
+
+console.log("\xe4\xbd\xa0\xe5\xa5\xbd");
+
+console.log(("你好"));
+
+console.log(("你好你好"));
+
+console.log(("\u4f60\u597d"));
+
+/*  Not a pure module */
---
+++ jscomp/test/gbk.ml
@@ -0,0 +1,11 @@
+
+
+Js.log "你好" ; 
+
+Js.log {|你好|};
+
+Js.log [%raw {|"你好"|}];
+
+Js.log ([%raw ({|"你好你好"|} )] : string);
+
+Js.log [%raw {|"\u4f60\u597d"|}];
