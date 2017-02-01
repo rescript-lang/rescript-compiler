c3813afdeac0d3f86546f2610a5f9b0c512ea561
Hongbo Zhang <bobzhang1988@gmail.com>
2017-02-01 14:24:18 -0500

    make sure windows cygwin work on master branch

--- scripts/install.js
+++ scripts/install.js
@@ -26,24 +26,8 @@ var working_config = { cwd: jscomp, stdio: [0, 1, 2] }
 var clean = require('./clean.js')
 var build_util = require('./build_util')
 
-if (is_windows) {
-    process.env.WIN32 = '1'
-    console.log("Installing on Windows")
-    fs.readdirSync(jscomp_bin).forEach(function (f) {
-        var last_index = f.lastIndexOf('.win')
-        if (last_index !== -1) {
-            var new_file = f.slice(0, -4) + ".exe"
-            build_util.poor_copy_sync(path.join(jscomp_bin, f), path.join(jscomp_bin, new_file))
-        }
 
-    })
-    child_process.execFileSync(path.join(__dirname, 'win_build.bat'), working_config)
-    clean.clean()
-    console.log("Installing")
-    build_util.install()
-}
-else {
-
+function non_windows_npm_release() {
     try {
         child_process.execSync('node config.js', working_config)
         console.log("Build the compiler and runtime .. ")
@@ -52,18 +36,47 @@ else {
     } catch (e) {
         child_process.execSync(path.join(__dirname, 'buildocaml.sh')) // TODO: sh -c ? this will be wrong if we have white space in the path
         process.env.PATH = path.join(__dirname, '..', 'bin') + path.delimiter + process.env.PATH
-    console.log('configure again with local ocaml installed')
-    if(process.env.BS_TRAVIS_CI){
-        child_process.execSync("make travis-world-test", working_config)
-    } else {
-        child_process.execSync("make world", working_config)
-    }
+        console.log('configure again with local ocaml installed')
+        if (process.env.BS_TRAVIS_CI) {
+            child_process.execSync("make travis-world-test", working_config)
+        } else {
+            child_process.execSync("make world", working_config)
+        }
 
-    clean.clean()
-}
+        clean.clean()
+    }
 
     console.log("Installing")
     child_process.execSync('make VERBOSE=true install', working_config)
 
+}
+
+if (is_windows) {
+    process.env.WIN32 = '1'
+    console.log("Installing on Windows")
+    var indeed_windows_release = 0
+    fs.readdirSync(jscomp_bin).forEach(function (f) {
+        var last_index = f.lastIndexOf('.win')
+        if (last_index !== -1) {
+            var new_file = f.slice(0, -4) + ".exe"
+            build_util.poor_copy_sync(path.join(jscomp_bin, f), path.join(jscomp_bin, new_file));
+            // we do have .win file which means windows npm release
+            ++indeed_windows_release
+        }
+
+    })
+    if (indeed_windows_release > 1) {
+        // only ninja.win in this case
+        child_process.execFileSync(path.join(__dirname, 'win_build.bat'), working_config)
+        clean.clean()
+        console.log("Installing")
+        build_util.install()
+    } else {
+        console.log("It is on windows, but seems to be that you are building against master branch, so we are going to depend on cygwin on master")
+        non_windows_npm_release()
+    }
+}
+else {
+    non_windows_npm_release()
 }
 
