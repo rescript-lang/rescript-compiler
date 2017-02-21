0cb46661d0c4d5bfca7479c7b9dc2f127357d042
Hongbo Zhang <bobzhang1988@gmail.com>
2017-02-21 10:16:32 -0500

    add a test case for #1245

---
+++ jscomp/test/gpr_1240_missing_unbox.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+function f(x, y) {
+  var match_000 = [x];
+  var match_001 = [y];
+  return /* tuple */[
+          match_000[0],
+          match_001[0]
+        ];
+}
+
+exports.f = f;
+/* No side effect */
--- jscomp/test/gpr_1240_missing_unbox.ml
+++ jscomp/test/gpr_1240_missing_unbox.ml
@@ -1,6 +1,8 @@
 
 
 
+
+
 let f x y =
     let x, y = ref x , ref y in 
     !x, !y 
