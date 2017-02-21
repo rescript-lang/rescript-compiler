7e68e39a9674edb505797a6b8051d8fe2d3bb473
glennsl <glenn@slotte.net>
2017-02-21 13:00:29 +0100

    Added Js_obj.empty + test
    Signed-Off-By: Glenn Slotte <glenn@slotte.net>

--- jscomp/test/js_obj_test.ml
+++ jscomp/test/js_obj_test.ml
@@ -13,10 +13,10 @@ let f_js u = u#@say 32
 let suites = Mt.[
   "caml_obj", (fun _ -> Eq (33, f (object method say x = 1 + x end)));
   "js_obj", (fun _ ->
-  Eq(34, f_js [%bs.obj{ say = fun [@bs]  x -> x + 2 } ]));
-  "js_obj2",(fun _ -> 
-  Eq(34,  [%bs.obj { say = fun [@bs]  x -> x + 2 } #@say 32 ]));
-    
+    Eq(34, f_js [%bs.obj{ say = fun [@bs]  x -> x + 2 } ]));
+  "js_obj2", (fun _ ->
+    Eq(34,  [%bs.obj { say = fun [@bs]  x -> x + 2 } #@say 32 ]));
+  "empty", fun _ -> (Eq(0, Js_obj.empty () |> Js_obj.keys |> Array.length));
 ]
 
 ;; Mt.from_pair_suites __FILE__ suites
