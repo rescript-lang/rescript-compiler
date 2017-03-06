e7d832bdcea5756a939973e5386af5bf2e617d68
Hongbo Zhang <bobzhang1988@gmail.com>
2017-03-06 13:05:01 -0500

    add more tests

--- jscomp/test/return_check.js
+++ jscomp/test/return_check.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var Js_primitive            = require("../../lib/js/js_primitive");
 var Caml_builtin_exceptions = require("../../lib/js/caml_builtin_exceptions");
 
 function test(dom) {
@@ -13,7 +14,7 @@ function test(dom) {
   }
 }
 
-function f(xs, i) {
+function f_undefined(xs, i) {
   var match = xs[i];
   if (match !== undefined) {
     return match;
@@ -30,6 +31,73 @@ function f(xs, i) {
   }
 }
 
-exports.test = test;
-exports.f    = f;
+function f_escaped_not(xs, i) {
+  var x = xs[i];
+  console.log("hei");
+  if (x !== undefined) {
+    return x;
+  }
+  else {
+    return 1;
+  }
+}
+
+function f_escaped_1(xs, i) {
+  var x = xs[i];
+  return function () {
+    if (x !== undefined) {
+      return x;
+    }
+    else {
+      return 1;
+    }
+  };
+}
+
+function f_escaped_2(xs, i) {
+  console.log(Js_primitive.js_from_def(xs[i]));
+  return /* () */0;
+}
+
+function f_null(xs, i) {
+  var match = xs[i];
+  if (match !== null) {
+    return match;
+  }
+  else {
+    throw [
+          Caml_builtin_exceptions.assert_failure,
+          [
+            "return_check.ml",
+            57,
+            14
+          ]
+        ];
+  }
+}
+
+function f_null_undefined(xs, i) {
+  var match = xs[i];
+  if (Js_primitive.js_is_nil_undef(match)) {
+    throw [
+          Caml_builtin_exceptions.assert_failure,
+          [
+            "return_check.ml",
+            66,
+            14
+          ]
+        ];
+  }
+  else {
+    return match;
+  }
+}
+
+exports.test             = test;
+exports.f_undefined      = f_undefined;
+exports.f_escaped_not    = f_escaped_not;
+exports.f_escaped_1      = f_escaped_1;
+exports.f_escaped_2      = f_escaped_2;
+exports.f_null           = f_null;
+exports.f_null_undefined = f_null_undefined;
 /* No side effect */
--- jscomp/test/return_check.ml
+++ jscomp/test/return_check.ml
@@ -20,12 +20,49 @@ let test dom =
 
 
 
-external get : int array -> int -> int option = "" 
+external get_undefined : int array -> int -> int option = "" 
 [@@bs.get_index] [@@bs.return { undefined_to_opt }]
 
 
-let f xs i = 
-    match get xs i with 
+let f_undefined xs i = 
+    match get_undefined xs i with 
+    | None -> assert false 
+    | Some k -> k 
+
+
+let f_escaped_not xs i =
+    let x = get_undefined xs i in 
+    Js.log "hei" ; 
+    match x with 
+    | Some k -> k 
+    | None -> 1 
+
+let f_escaped_1 xs i = 
+    let x = get_undefined xs i in 
+    fun () -> 
+        match x with 
+        | Some k -> k 
+        | None -> 1 (* still okay *)
+
+let f_escaped_2 xs i = 
+    Js.log (get_undefined xs i )        
+
+
+external get_null : int array -> int -> int option = "" 
+[@@bs.get_index] [@@bs.return { null_to_opt }]
+
+
+let f_null xs i = 
+    match get_null xs i with 
+    | None -> assert false 
+    | Some k -> k 
+
+external get_null_undefined : int array -> int -> int option = "" 
+[@@bs.get_index] [@@bs.return { null_undefined_to_opt }]
+
+
+let f_null_undefined xs i = 
+    match get_null_undefined xs i with 
     | None -> assert false 
     | Some k -> k 
 
