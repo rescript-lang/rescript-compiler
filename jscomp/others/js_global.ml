9bf01b00541bb3a0fdf0c17d4ed21d62232b2a54
glennsl <glenn@slotte.net>
2017-03-15 13:35:03 +0100

    Properly documented Js_re and Js_global

--- jscomp/others/js_global.ml
+++ jscomp/others/js_global.ml
@@ -1,8 +1,88 @@
+(** Contains functions available in the global scope ([window] in a browser context) *)
+
+(** identifies an interval started by {! setInterval} *)
 type intervalId
+
+(** identifies a timeout started by {! setTimeout} *)
 type timeoutId
 
+(** clears an interval started by {! setInterval}
+
+@see <https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/clearInterval> MDN
+
+@example {[
+(* API for a somewhat aggressive snoozing alarm clock *)
+
+let interval = Js.Undefined.empty
+
+let remind () =
+  Js.log "Wake Up!";
+  IO.punchSleepyGuy ()
+
+let snooze mins =
+  interval := Js.Undefined.return \@\@ Js.Global.setInterval remind (mins * 60 * 1000)
+
+let cancel () =
+  Js.Undefined.iter Js.Global.clearInterval !interval
+]}
+*)
 external clearInterval : intervalId -> unit = "" [@@bs.val]
+
+
+(** clears a timeout started by {! setTimeout}
+
+@see <https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/clearTimeout> MDN
+
+@example {[
+(* A simple model of a code monkey's brain *)
+
+let timer = Js.Undefined.empty
+
+let work () =
+  IO.closeHackerNewsTab ()
+
+let procrastinate mins =
+  Js.Undefined.iter fun Js.Global.clearTimeout !timer
+  Js.Global.setTimeout work (mins * 60 * 1000)
+]}
+*)
 external clearTimeout : timeoutId -> unit = "" [@@bs.val]
 
+
+(** {i repeatedly} executes a callback with a specified interval (in milliseconds) between calls
+
+{b returns} an {! intervalId} that can be passed to {! clearInterval} to cancel the timeout
+
+@see <https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setInterval> MDN
+
+@example {[
+(* Will count up and print the count to the console every second *)
+
+let count = ref 0
+
+let tick () =
+  count := !count + 1; Js.log (string_of_int !count)
+
+let _ =
+  Js.Global.setInterval tick 1000
+]}
+*)
 external setInterval : (unit -> unit) -> int -> intervalId = "" [@@bs.val]
+
+
+(** executes a callback after a specified delay (in milliseconds)
+
+{b returns} a {! timeoutId} that can be passed to {! clearTimeout} to cancel the timeout
+
+@see <https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/setTimeout> MDN
+
+@example {[
+(* Prints "Timed out!" in the console after one second *)
+
+let message = "Timed out!"
+
+let _ =
+  Js.Global.setTimeout (fun () -> Js.log message) 1000
+]}
+*)
 external setTimeout : (unit -> unit) -> int -> timeoutId = "" [@@bs.val]
--- jscomp/others/js_re.mli
+++ jscomp/others/js_re.mli
@@ -21,24 +21,162 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *)
+
+(** Provides bindings for JavaScript Regular Expressions
+ 
+{4 Syntax sugar}
+BuckleScript provides a bit of syntax sugar for regex literals: [\[%re "/foo/g"\]]
+will evaluate to a {! t} that can be passed around and used like usual.
+
+{b Note:} This is not an immutable API. A RegExp object with the [global] ("g")
+flag set will modify the {! lastIndex} property when the RegExp object is used,
+and subsequent uses will ocntinue the search from the previous {! lastIndex}.
+
+@example {[
+let maybeMatches = "banana" |> Js.String.match_ [\[%re "/na+/g"\]]
+]}
+
+@see
+  <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp>
+  JavaScript API reference on MDN
+
+@see
+  <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions>
+  JavaScript Regular Expressions Guide on MDN
+*)
+
+(** the RegExp object *)
 type t
+
+(** the result of a executing a RegExp on a string *)
 type result
 
+(** an array of the matches, the first is the full match and the remaining are the substring matches *)
 external matches : result -> string array = "%identity"
+
+(** 0-based index of the match in the input string *)
 external index : result -> int = "" [@@bs.get]
+
+(** the original input string *)
 external input : result -> string = "" [@@bs.get]
 
-external fromString : string -> t = "RegExp" [@@bs.new]
-external fromStringWithFlags : string -> string -> t = "RegExp" [@@bs.new]
 
+(** Constructs a RegExp object ({! t}) from a string
+
+Regex literals ([\[%re "/.../"\]]) should generally be preferred, but
+[fromString] is very useful when you need to insert a string into a regex.
+
+@example {[
+(* A function that extracts the content of the first element with the given tag *)
+
+let contentOf tag xmlString =
+  Js.Re.fromString ("<" ^ tag ^ ">(.|\n)*?<\/" ^ tag ^">")
+    |> Js.Re.exec xmlString
+    |> function
+      | Some result => Some (Js.Re.matches result).(1)
+      | None -> None
+]}
+*)
+external fromString : string -> t = "RegExp" [@@bs.new]
+
+(** Constructs a RegExp object ({! t}) from a string with the given [flags]
+
+See {! fromString}
+
+Valid flags:
+{%html:
+<table>
+  <tr> <td>g <td>global
+  <tr> <td>i <td>ignore case
+  <tr> <td>m <td>multiline
+  <tr> <td>u <td>unicode <td>(es2015)
+  <tr> <td>y <td>sticky <td>(es2015)
+</table>
+%}
+*)
+external fromStringWithFlags : string -> flags:string -> t = "RegExp" [@@bs.new]
+
+(** returns the enabled flags as a string *)
 external flags : t -> string = "" [@@bs.get]
+
+(** returns a bool indicating whether the [global] flag is set *)
 external global : t -> bool = "" [@@bs.get]
+
+(** returns a bool indicating whether the [ignoreCase] flag is set *)
 external ignoreCase : t -> bool = "" [@@bs.get]
+
+(** returns the index where the next match will start its search
+
+This property will be modified when the RegExp object is used, if the [global] ("g")
+flag is set.
+
+@example {[
+(* Finds and prints successive matches *)
+
+let re = [%re "/ab*/g"] in
+let str = "abbcdefabh" in
+
+let break = ref false in
+while not break do
+  match re |> Js.Re.exec str with
+  | None -> ()
+  | Some result =>
+    let match_ = (Js.Re.matches result).(0) in
+    let next = string_of_int (Js.Re.lastIndex re) in
+    Js.log ("Found " ^ match_ ^ ". Next match starts at " ^ next)
+done
+]}
+
+@see <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/lastIndex> MDN
+*)
 external lastIndex : t -> int = "" [@@bs.get]
+
+(** returns a bool indicating whether the [multiline] flag is set *)
 external multiline : t -> bool = "" [@@bs.get]
+
+(** returns the pattern as a string *)
 external source : t -> string = "" [@@bs.get]
+
+(** returns a bool indicating whether the [sticky] flag is set *)
 external sticky : t -> bool = "" [@@bs.get]
+
+(** returns a bool indicating whether the [unicode] flag is set *)
 external unicode : t -> bool = "" [@@bs.get]
 
-external exec : string -> result Js_null.t = "" [@@bs.send.pipe: t]
+(** executes a search on a given string using the given RegExp object
+
+{b returns} [Some] {! result} if a match is found, [None] otherwise
+
+@example {[
+(* Match "quick brown" followed by "jumps", ignoring characters in between
+ * Remember "brown" and "jumps"
+ * Ignore case
+ *)
+
+let re = [%re "/quick\s(brown).+?(jumps)/ig" in
+let result = re |> Js.Re.exec "The Quick Brown Fox Jumps Over The Lazy Dog"
+]}
+
+@see <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/exec> MDN
+*)
+external exec : string -> result option = "" [@@bs.send.pipe: t] [@@bs.return {null_to_opt}]
+
+(** tests whether the given RegExp object will match a given string
+
+{b returns} [true] if a match is found, [false] otherwise
+
+@example {[
+(* A simple implementation of Js.String.startsWith *)
+
+let str = "hello world!"
+
+let startsWith substring target =
+  Js.Re.fromString ("^" ^ substring)
+    |> Js.Re.test target
+
+let _ = Js.log (str |> startsWith "hello") (* prints "true" *)
+]}
+
+@see <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/RegExp/test> MDN
+*)
 external test : string -> bool = "" [@@bs.send.pipe: t]
