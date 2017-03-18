61c11215bdf02e85e96570fb154fccf24033d4c7
Bob Fang <dorafmon@gmail.com>
2017-03-18 15:10:06 +0000

    Issue #1351 : bug fix

--- jscomp/bin/whole_compiler.ml
+++ jscomp/bin/whole_compiler.ml
@@ -91580,6 +91580,7 @@ let translate (prim_name : string)
      Not good for inline *)
 
   | "caml_array_blit"
+  | "caml_array_safe_get"
   | "caml_make_vect" -> 
     call Js_config.array
   | "caml_ml_flush"
@@ -91589,7 +91590,6 @@ let translate (prim_name : string)
   | "caml_ml_output_char"
   | "caml_ml_output" 
   | "caml_ml_input_char"
-  | "caml_array_safe_get"
     -> 
     call Js_config.io
   | "caml_update_dummy"
