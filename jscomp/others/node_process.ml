f60c4498b2fed84605655005d28c18f606923bf0
joshaber <joshaber@gmail.com>
2017-03-23 10:16:52 -0400

    Env is always string values
    Signed-Off-By: Josh Abernathy <joshaber@gmail.com>

--- jscomp/others/node_process.ml
+++ jscomp/others/node_process.ml
@@ -1,5 +1,5 @@
 (* Copyright (C) 2015-2016 Bloomberg Finance L.P.
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -17,14 +17,14 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *)
 
 (** Node Process API *)
 
-type 'a t =
+type t =
   < argv : string array;
     arch : string ;
     abort : unit -> unit [@bs.meth];
@@ -32,8 +32,8 @@ type 'a t =
     cwd : unit -> string [@bs.meth];
     disconnect : unit -> unit [@bs.meth];
     platform : string;
-    env : 'a Js.Dict.t;
+    env : string Js.Dict.t;
     exit : int -> unit [@bs.meth];
   >   Js.t
 
-external process : _ t = "" [@@bs.module]
+external process : t = "" [@@bs.module]
