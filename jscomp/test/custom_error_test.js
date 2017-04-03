ecca170ae513aec0f24546d91f617633c583d1ff
Hongbo Zhang <bobzhang1988@gmail.com>
2017-04-03 16:14:46 -0400

    add documentation

--- jscomp/test/custom_error_test.js
+++ jscomp/test/custom_error_test.js
@@ -41,6 +41,44 @@ function test_js_error2() {
   }
 }
 
+function example1() {
+  var exit = 0;
+  var v;
+  try {
+    v = JSON.parse(" {\"x\"  }");
+    exit = 1;
+  }
+  catch (raw_exn){
+    var exn = Js_exn.internalToOCamlException(raw_exn);
+    if (exn[0] === Js_exn.$$Error) {
+      console.log(exn[1].stack);
+      return /* None */0;
+    } else {
+      throw exn;
+    }
+  }
+  if (exit === 1) {
+    return /* Some */[v];
+  }
+  
+}
+
+function example2() {
+  try {
+    return /* Some */[JSON.parse(" {\"x\"}")];
+  }
+  catch (raw_exn){
+    var exn = Js_exn.internalToOCamlException(raw_exn);
+    if (exn[0] === Js_exn.$$Error) {
+      return /* None */0;
+    } else {
+      throw exn;
+    }
+  }
+}
+
 exports.test_js_error  = test_js_error;
 exports.test_js_error2 = test_js_error2;
+exports.example1       = example1;
+exports.example2       = example2;
 /* No side effect */
