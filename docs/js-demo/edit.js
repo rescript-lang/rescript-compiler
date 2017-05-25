c93e528f42e514387676f3aa74f6ba4e3c6cd5f3
krisaoe <krisopowell@gmail.com>
2017-05-26 01:28:32 +1000

    Changed gist share to hashed URL share. Signed-Off-By: Kris Powell <krisopowell@gmail.com>

--- docs/js-demo/edit.js
+++ docs/js-demo/edit.js
@@ -195,44 +195,18 @@ myCode1Mirror.on("changes", onEditChanges);
 
 jsCode1Mirror.setSize(null,codeMirrorDefaultHeight);
 
-
-
 //checks or unchecks the eval button
 function changeEvalButton(bool) {
   $('#option-eval').prop('checked', bool);
   onEvalButtonChange();
 }
 
-//creates a gist from OCaml code
+//creates a hashed URL from OCaml code
 $('#share').click(function (e) {
-  var state = $(this).button('loading');
-  var request =
-  {
-    "description": "BuckleScript Gist",
-    "public": true,
-    "files": {
-      "gist.ml": {
-        "content": myCode1Mirror.getValue()
-      }
-    }
-  };
-
-  $
-    .ajax({ url:'https://api.github.com/gists',
-            type: 'POST',
-            data: JSON.stringify(request)
-          })
-    .done(function (response) {
-      state.button('reset');
-      $('#shareModal').modal('show');
-      var url = 'https://bloomberg.github.io/bucklescript/js-demo/?gist=' + response.id;
-      $('#shareModalBody').html('<a href=' + '"' + url + '"' + 'target="_blank"' + '>' + url + '</a>');
-    })
-    .error(function (err) {
-      state.button('reset');
-      $('#shareModal').modal('show');
-      $('#shareModalBody').text('Sorry! Currently GitHub\'s API limits the number of requests we can send per hour. Please try again later.');
-    })
+  var hashedCode = btoa(myCode1Mirror.getValue());
+  $('#shareModal').modal('show');
+  var url = 'https://bloomberg.github.io/bucklescript/js-demo/#' + hashedCode;
+  $('#shareModalBody').html('<a href=' + '"' + url + '"' + 'target="_blank"' + '>' + url + '</a>');
 });
 
 //copy link to clipboard
