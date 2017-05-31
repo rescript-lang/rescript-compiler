1cbeb6e44b2a8685aa41e478be415d4e904ddf09
Hongbo Zhang <bobzhang1988@gmail.com>
2017-05-31 11:01:12 -0400

    fix #1656, bs.get,bs.set,bs.get_index,bs.set_index respsect bs.ignore

--- jscomp/core/lam_compile_external_call.ml
+++ jscomp/core/lam_compile_external_call.ml
@@ -316,25 +316,29 @@ let translate_ffi
     E.call ~info:{arity=Full; call_info = Call_na}  (E.dot self name) args
 
   | Js_get {js_get_name = name; js_get_scopes = scopes } -> 
+    let args,cur_eff = assemble_args call_loc ffi false arg_types args in 
+    add_eff cur_eff @@ 
     begin match args with 
       | [obj] ->
         let obj = translate_scoped_access scopes obj in 
         E.dot obj name        
-      | _ -> assert false 
+      | _ -> assert false  (* Note these assertion happens in call site *)
     end  
   | Js_set {js_set_name = name; js_set_scopes = scopes  } -> 
+    (* assert (js_splice = false) ;  *)
+    let args,cur_eff = assemble_args call_loc ffi false arg_types args in 
+    add_eff cur_eff @@
     begin match args, arg_types with 
       | [obj; v], _ -> 
         let obj = translate_scoped_access scopes obj in
         E.assign (E.dot obj name) v         
-      | [obj], [_; {arg_type = Arg_cst cst ; _ }] ->
-        let obj = translate_scoped_access scopes obj in 
-        E.assign (E.dot obj name) (Lam_compile_const.translate_arg_cst cst)
       | _ -> 
         assert false 
     end
   | Js_get_index { js_get_index_scopes = scopes }
     -> 
+    let args,cur_eff = assemble_args call_loc ffi false arg_types args in 
+    add_eff cur_eff @@ 
     begin match args with
       | [obj; v ] -> 
         Js_arr.ref_array (translate_scoped_access scopes obj) v 
@@ -342,6 +346,8 @@ let translate_ffi
     end
   | Js_set_index { js_set_index_scopes = scopes }
     -> 
+    let args,cur_eff = assemble_args call_loc ffi false arg_types args in 
+    add_eff cur_eff @@ 
     begin match args with 
       | [obj; v ; value] -> 
           Js_arr.set_array (translate_scoped_access scopes obj) v value
--- jscomp/test/ffi_js_test.js
+++ jscomp/test/ffi_js_test.js
@@ -142,6 +142,37 @@ function create_prim() {
         };
 }
 
+function ffff(x) {
+  x.setGADT = 3;
+  x.setGADT2 = /* tuple */[
+    3,
+    "3"
+  ];
+  x.setGADT2 = /* tuple */[
+    "3",
+    3
+  ];
+  var match = x[3];
+  console.log(/* tuple */[
+        match[0],
+        match[1]
+      ]);
+  console.log(x.getGADT);
+  var match$1 = x.getGADT2;
+  console.log(match$1[0], match$1[1]);
+  var match$2 = x[0];
+  console.log(match$2[0], match$2[1]);
+  x[0] = /* tuple */[
+    1,
+    "x"
+  ];
+  x[3] = /* tuple */[
+    3,
+    "x"
+  ];
+  return /* () */0;
+}
+
 Mt.from_pair_suites("ffi_js_test.ml", suites[0]);
 
 exports.keys               = keys;
@@ -159,4 +190,5 @@ exports.v                  = v;
 exports.vvv                = vvv;
 exports.vvvv               = vvvv;
 exports.create_prim        = create_prim;
+exports.ffff               = ffff;
 /* keys Not a pure module */
