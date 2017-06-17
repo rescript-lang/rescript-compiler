55c5ad96b5e9596b284a8508ef901d8b26abe563
Hongbo Zhang <bobzhang1988@gmail.com>
2017-06-17 11:01:35 -0400

    address comments

--- docs/reason-demo/stdlib/caml_obj_runtime.js
+++ docs/reason-demo/stdlib/caml_obj_runtime.js
@@ -17,7 +17,7 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 //  Copyright (c) 2015 Bloomberg LP. All rights reserved. 
-// Hongbo Zhang (hzhang295@bucklescript.net)              
+// Hongbo Zhang (bobzhang1988@gmail.com)              
 'use strict';
 define(["require", "exports", './caml_exceptions'], function (require, exports, caml_exceptions_1) {
     /**
