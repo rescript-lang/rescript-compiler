bd2adf83bb8e7542c7e48e3550d7560d65852411
Hongbo Zhang <bobzhang1988@gmail.com>
2017-07-05 03:50:05 +0800

    update test cases

--- jscomp/test/gpr_1762_test.js
+++ jscomp/test/gpr_1762_test.js
@@ -1,7 +1,45 @@
 'use strict';
 
-var Pervasives = require("../../lib/js/pervasives.js");
+var Mt    = require("./mt.js");
+var Block = require("../../lib/js/block.js");
 
-Pervasives.bool_of_string("x");
+var suites = [/* [] */0];
 
+var test_id = [0];
+
+function eq(loc, x, y) {
+  test_id[0] = test_id[0] + 1 | 0;
+  suites[0] = /* :: */[
+    /* tuple */[
+      loc + (" id " + test_id[0]),
+      (function () {
+          return /* Eq */Block.__(0, [
+                    x,
+                    y
+                  ]);
+        })
+    ],
+    suites[0]
+  ];
+  return /* () */0;
+}
+
+var v = [3];
+
+function update() {
+  v[0] = v[0] + 1 | 0;
+  return /* true */1;
+}
+
+v[0] = v[0] + 1 | 0;
+
+eq("File \"gpr_1762_test.ml\", line 22, characters 6-13", v[0], 4);
+
+Mt.from_pair_suites("gpr_1762_test.ml", suites[0]);
+
+exports.suites  = suites;
+exports.test_id = test_id;
+exports.eq      = eq;
+exports.v       = v;
+exports.update  = update;
 /*  Not a pure module */
--- jscomp/test/gpr_1762_test.ml
+++ jscomp/test/gpr_1762_test.ml
@@ -1,4 +1,25 @@
 
 
 
-if bool_of_string "x" then "" else "" 
+(*;; if bool_of_string "x" then "" else "" *)
+
+
+
+let suites :  Mt.pair_suites ref  = ref []
+let test_id = ref 0
+let eq loc x y = 
+  incr test_id ; 
+  suites := 
+    (loc ^" id " ^ (string_of_int !test_id), (fun _ -> Mt.Eq(x,y))) :: !suites
+
+
+let v = ref 3
+
+let update () = incr v ; true 
+
+;; if update () then "" else ""
+
+;; eq __LOC__ !v 4 
+
+
+;; Mt.from_pair_suites __FILE__ !suites
