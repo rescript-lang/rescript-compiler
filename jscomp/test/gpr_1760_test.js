e38c1ed119db1bfc7cb3fe8e9b9aaa66e4f389a4
Hongbo Zhang <bobzhang1988@gmail.com>
2017-07-05 10:30:22 +0800

    fix #1760

--- jscomp/test/gpr_1760_test.js
+++ jscomp/test/gpr_1760_test.js
@@ -1,5 +1,128 @@
 'use strict';
 
+var Mt         = require("./mt.js");
+var Block      = require("../../lib/js/block.js");
+var Caml_int32 = require("../../lib/js/caml_int32.js");
+var Caml_int64 = require("../../lib/js/caml_int64.js");
 
+var suites = [/* [] */0];
 
-/* No side effect */
+var test_id = [0];
+
+function eq(loc, x, y) {
+  test_id[0] = test_id[0] + 1 | 0;
+  suites[0] = /* :: */[
+    /* tuple */[
+      loc + (" id " + test_id[0]),
+      (function () {
+          return /* Eq */Block.__(0, [
+                    x,
+                    y
+                  ]);
+        })
+    ],
+    suites[0]
+  ];
+  return /* () */0;
+}
+
+var a0;
+
+try {
+  Caml_int32.div(0, 0);
+  a0 = 0;
+}
+catch (exn){
+  a0 = 1;
+}
+
+var a1;
+
+try {
+  Caml_int32.mod_(0, 0);
+  a1 = 0;
+}
+catch (exn$1){
+  a1 = 1;
+}
+
+var a4;
+
+try {
+  Caml_int32.div(0, 0);
+  a4 = 0;
+}
+catch (exn$2){
+  a4 = 1;
+}
+
+var a5;
+
+try {
+  Caml_int32.mod_(0, 0);
+  a5 = 0;
+}
+catch (exn$3){
+  a5 = 1;
+}
+
+var a6;
+
+try {
+  Caml_int64.div(/* int64 */[
+        /* hi */0,
+        /* lo */0
+      ], /* int64 */[
+        /* hi */0,
+        /* lo */0
+      ]);
+  a6 = 0;
+}
+catch (exn$4){
+  a6 = 1;
+}
+
+var a7;
+
+try {
+  Caml_int64.mod_(/* int64 */[
+        /* hi */0,
+        /* lo */0
+      ], /* int64 */[
+        /* hi */0,
+        /* lo */0
+      ]);
+  a7 = 0;
+}
+catch (exn$5){
+  a7 = 1;
+}
+
+eq("File \"gpr_1760_test.ml\", line 30, characters 5-12", /* tuple */[
+      a0,
+      a1,
+      a4,
+      a5,
+      a6,
+      a7
+    ], /* tuple */[
+      1,
+      1,
+      1,
+      1,
+      1,
+      1
+    ]);
+
+Mt.from_pair_suites("gpr_1760_test.ml", suites[0]);
+
+exports.suites  = suites;
+exports.test_id = test_id;
+exports.eq      = eq;
+exports.a0      = a0;
+exports.a1      = a1;
+exports.a4      = a4;
+exports.a5      = a5;
+exports.a6      = a6;
+exports.a7      = a7;
+/* a0 Not a pure module */
