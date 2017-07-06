64aaf4101cd13fe16917634bbd0de6ccebce562f
Sachin Shah <znprojects.apps@gmail.com>
2017-07-05 21:59:28 -0500

    added windows tests to a_filename_test.ml
    Signed-off-by: Sachin Shah <znprojects.apps@gmail.com>

--- jscomp/test/a_filename_test.ml
+++ jscomp/test/a_filename_test.ml
@@ -1,53 +1,53 @@
 let suites :  Mt.pair_suites ref  = ref []
 let test_id = ref 0
-let eq loc x y = 
-  incr test_id ; 
-  suites := 
+let eq loc x y =
+  incr test_id ;
+  suites :=
     (loc ^" id " ^ (string_of_int !test_id), (fun _ -> Mt.Eq(x,y))) :: !suites
 
-let test = Ext_filename.node_relative_path true 
+let test = Ext_filename.node_relative_path true
 let () =
-  eq __LOC__ 
-    (let (//) = Ext_filename.combine in 
+  eq __LOC__
+    (let (//) = Ext_filename.combine in
      ("/tmp"// "subdir/file.txt",
       "/tmp"// "/a/tmp.txt",
-      "/a/tmp.txt" // "subdir/file.txt"      
+      "/a/tmp.txt" // "subdir/file.txt"
     ))
     ("/tmp/subdir/file.txt",
      "/a/tmp.txt",
-     "/a/tmp.txt/subdir/file.txt"      
+     "/a/tmp.txt/subdir/file.txt"
     )
 
-  ;    
+  ;
   eq __LOC__
     (test
        (`File "./a/b.c")
        (`File "./a/u/g.c")) "./u/g.c";
 
-  eq __LOC__ 
+  eq __LOC__
     (test
        (`File "./a/b.c")
        (`File "xxxghsoghos/ghsoghso/node_modules/buckle-stdlib/list.js"))
     "buckle-stdlib/list.js" ;
 
-  eq __LOC__ 
+  eq __LOC__
     (test
            (`File "./a/b.c")
            (`File "xxxghsoghos/ghsoghso/node_modules//buckle-stdlib/list.js"))
          "buckle-stdlib/list.js" ;
 
-  eq __LOC__ 
+  eq __LOC__
     (test
            (`File "./a/b.c")
            (`File "xxxghsoghos/ghsoghso/node_modules/./buckle-stdlib/list.js"))
     "buckle-stdlib/list.js"    ;
 
-  eq __LOC__ 
+  eq __LOC__
     (test
            (`File "./a/c.js")
            (`File "./a/b"))
     "./b"         ;
-  eq __LOC__ 
+  eq __LOC__
     (test
            (`File "./a/c")
            (`File "./a/b.js"))
@@ -57,15 +57,15 @@ let () =
            (`Dir "./a/")
            (`File "./a/b.js"))
     "./b.js";
-  eq __LOC__ 
+  eq __LOC__
     (Ext_filename.get_extension "a.txt"
     )
     ".txt";
-  eq __LOC__ 
+  eq __LOC__
     (Ext_filename.get_extension "a"
     )
     "";
-  eq __LOC__ 
+  eq __LOC__
     (Ext_filename.get_extension ".txt"
     )
     ".txt";
@@ -83,6 +83,16 @@ let () =
          "/a";
          "/a.txt/";
          "/a.txt"
+         "c:/gsho/./..";
+         "d:/a/b/../c../d/e/f";
+         "e:/a/b/../c/../d/e/f";
+         "f:/gsho/./../..";
+         "g:/a/b/c/d";
+         "h:/a/b/c/d/";
+         "j:/a/";
+         "k:/a";
+         "l:/a.txt/";
+         "abc:/a.txt"
        |])
      [|
        "/";
@@ -95,9 +105,19 @@ let () =
        "/a";
        "/a.txt";
        "/a.txt"
-     |] 
+       "c:/";
+       "d:/a/c../d/e/f";
+       "e:/a/d/e/f";
+       "f:/";
+       "g:/a/b/c/d" ;
+       "h:/a/b/c/d";
+       "j:/a";
+       "k:/a";
+       "l:/a.txt";
+       "abc:/a.txt"
+     |]
   ;
 
-  
+
 
 ;; Mt.from_pair_suites __FILE__ !suites
