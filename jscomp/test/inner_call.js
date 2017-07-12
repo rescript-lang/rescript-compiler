8bcf7079f0c68ab665d81b54011a18e5dd404ae9
Hongbo Zhang <bobzhang1988@gmail.com>
2017-07-12 10:03:07 +0800

    clean up, and coments

--- jscomp/test/inner_call.js
+++ jscomp/test/inner_call.js
@@ -5,4 +5,14 @@ var Inner_define = require("./inner_define.js");
 
 console.log(Curry._2(Inner_define.N[/* add */0], 1, 2));
 
+function f(x) {
+  return /* tuple */[
+          Curry._1(Inner_define.N0[/* f1 */0], x),
+          Curry._2(Inner_define.N0[/* f2 */1], x, x),
+          Curry._3(Inner_define.N0[/* f3 */2], x, x, x),
+          Curry._2(Inner_define.N1[/* f2 */0], x, x)
+        ];
+}
+
+exports.f = f;
 /*  Not a pure module */
--- jscomp/test/inner_call.ml
+++ jscomp/test/inner_call.ml
@@ -2,3 +2,10 @@
 
 
 Js.log (Inner_define.N.add 1 2)
+
+
+open Inner_define
+
+let f x =  
+  N0.f1 x , N0.f2 x x, N0.f3 x x x,
+  N1.f2 x x 
