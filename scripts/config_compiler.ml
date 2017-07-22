746f4d7788d4f1078464260c3292e0232bd802c3
lacoperon <elliot.james.williams@gmail.com>
2017-07-22 15:45:07 +0800

    Fixed spurious 'configuration failure' message
    On non-win32 systems, "configuration failure" is spit out to the console
    irregardless of a successful installation.
    
    So, I changed to code to say "configuration failure" iff it fails to find
    and use a config_map on win32 (the original intention of the code),
    or to say "Non Windows 32 Bit System" if it's not being installed
     on a Windows 32 Bit System
    (Much less confusing/ominous than 'configuration failure').
    
    Signed-Off-By: Elliot J. Williams <ejwilliams@wesleyan.edu>

--- scripts/config_compiler.ml
+++ scripts/config_compiler.ml
@@ -26,48 +26,48 @@ let delete_env_var : Process.t -> string -> unit [@bs] = [%raw{|
 
 (* need check which variables exist when we update compiler *)
 let map = [%obj {
-  _LIBDIR = "standard_library_default";
-  _BYTERUN = "standard_runtime";
-  _CCOMPTYPE = "ccomp_type";
-  _BYTECC = "bytecomp_c_compiler";
-  _BYTECCLIBS = "bytecomp_c_libraries";
-  _NATIVECC = "native_c_compiler";
-  _NATIVECCLIBS = "native_c_libraries";
-  _PACKLD = "native_pack_linker";
-  _RANLIBCMD = "ranlib";
-  _ARCMD = "ar";
-  _CC_PROFILE = "cc_profile";
+                  _LIBDIR = "standard_library_default";
+                  _BYTERUN = "standard_runtime";
+                  _CCOMPTYPE = "ccomp_type";
+                  _BYTECC = "bytecomp_c_compiler";
+                  _BYTECCLIBS = "bytecomp_c_libraries";
+                  _NATIVECC = "native_c_compiler";
+                  _NATIVECCLIBS = "native_c_libraries";
+                  _PACKLD = "native_pack_linker";
+                  _RANLIBCMD = "ranlib";
+                  _ARCMD = "ar";
+                  _CC_PROFILE = "cc_profile";
 
-  _MKDLL = "mkdll"; (* undefined *)
-  _MKEXE = "mkexe"; (* undefined *)
-  _MKMAINDLL = "mkmaindll"; (* undefined TODO= upstream to print it too *)
+                  _MKDLL = "mkdll"; (* undefined *)
+                  _MKEXE = "mkexe"; (* undefined *)
+                  _MKMAINDLL = "mkmaindll"; (* undefined TODO= upstream to print it too *)
 
-  _ARCH = "architecture";
-  _MODEL = "model";
-  _SYSTEM = "system";
-  _ASM = "asm";
-  _ASM_CFI_SUPPORTED = "asm_cfi_supported"; (* boolean *)
-  _WITH_FRAME_POINTERS = "with_frame_pointers"; (* boolean *)
-  _EXT_OBJ = "ext_obj";
-  _EXT_ASM = "ext_asm";
-  _EXT_LIB = "ext_lib";
-  _EXT_DLL = "ext_lib";
-  _HOST = "host";
-  _TARGET = "target";
-  _SYSTHREAD_SUPPORT = "systhread_supported"; (* boolean *)
-}] 
+                  _ARCH = "architecture";
+                  _MODEL = "model";
+                  _SYSTEM = "system";
+                  _ASM = "asm";
+                  _ASM_CFI_SUPPORTED = "asm_cfi_supported"; (* boolean *)
+                  _WITH_FRAME_POINTERS = "with_frame_pointers"; (* boolean *)
+                  _EXT_OBJ = "ext_obj";
+                  _EXT_ASM = "ext_asm";
+                  _EXT_LIB = "ext_lib";
+                  _EXT_DLL = "ext_lib";
+                  _HOST = "host";
+                  _TARGET = "target";
+                  _SYSTHREAD_SUPPORT = "systhread_supported"; (* boolean *)
+                }]
 (** mapping is as follows:
-    "EXT_OBJ" in "config.mlp" map to "ext_obj" based on "ocamlopt.opt -config" output 
+    "EXT_OBJ" in "config.mlp" map to "ext_obj" based on "ocamlopt.opt -config" output
     to look up, note that not all MACROS are available from "ocamlc.opt -config"
     for example:
     {[
       let ar = "%%ARCMD%%"
     ]}
-    but [ar] is not printed in `ocamlc.opt -config` as below 
+    but [ar] is not printed in `ocamlc.opt -config` as below
     {[
       ar : ar
     ]}
-    so that we could not find the replacement, which is fine, since bsc 
+    so that we could not find the replacement, which is fine, since bsc
     does not care about those native tools
 *)
 
@@ -77,22 +77,22 @@ let patch_config jscomp_dir config_map is_windows =
   let content = Fs.readFileAsUtf8Sync whole_compiler_config in
   let replace_values whole match_ offset s =
     match (match_, is_windows) with
-      | ("LIBDIR", true) ->
-        {|Filename.concat (Filename.concat (Filename.concat (Filename.dirname Sys.executable_name) "..") "lib") "ocaml"|}
-      | ("LIBDIR", false) ->
-        let origin_path = Path.join [|jscomp_dir; ".."; "lib"; "ocaml"|] in
-        Js.Json.stringify (Js.Json.string origin_path)
-      | _ ->
-        match Js.Dict.get (dictOfObj map) match_ with
-          | Some map_val -> (
-            match Js.Dict.get config_map map_val with
-              | Some a -> a
-              | None ->
-                Js.log ("No value found from ocamlopt.opt -config for \"" ^ map_val ^ "\"");
-                ""
-          )
-          | None -> assert false 
-            (** It is always there *)
+    | ("LIBDIR", true) ->
+      {|Filename.concat (Filename.concat (Filename.concat (Filename.dirname Sys.executable_name) "..") "lib") "ocaml"|}
+    | ("LIBDIR", false) ->
+      let origin_path = Path.join [|jscomp_dir; ".."; "lib"; "ocaml"|] in
+      Js.Json.stringify (Js.Json.string origin_path)
+    | _ ->
+      match Js.Dict.get (dictOfObj map) match_ with
+      | Some map_val -> (
+          match Js.Dict.get config_map map_val with
+          | Some a -> a
+          | None ->
+            Js.log ("No value found from ocamlopt.opt -config for \"" ^ map_val ^ "\"");
+            ""
+        )
+      | None -> assert false
+      (** It is always there *)
   in
   let generated = Js.String.unsafeReplaceBy1 [%re {|/%%(\w+)%%/g|}] replace_values content in
   Fs.writeFileAsUtf8Sync whole_compiler_config_output generated
@@ -107,13 +107,13 @@ let get_config_output is_windows =
     Js.log ("config_output:\n" ^ config_output);
 
     let keyvalues = Js.String.split "\n" config_output
-      |> Js.Array.filter (fun x -> Js.String.length x > 0)
-      |> Js.Array.map (fun x ->
-        let index = Js.String.indexOf ":" x in
-        let key = Js.String.substrAtMost ~from:0 ~length:index x in
-        let value = Js.String.substr ~from:(index + 1) x in
-        (Js.String.trim key, Js.String.trim value)
-      )
+                    |> Js.Array.filter (fun x -> Js.String.length x > 0)
+                    |> Js.Array.map (fun x ->
+                        let index = Js.String.indexOf ":" x in
+                        let key = Js.String.substrAtMost ~from:0 ~length:index x in
+                        let value = Js.String.substr ~from:(index + 1) x in
+                        (Js.String.trim key, Js.String.trim value)
+                      )
     in
     Js.log "keyvalues";
     Js.Array.forEach (fun (key, value) -> Js.log (key ^ ": " ^ value)) keyvalues;
@@ -125,8 +125,8 @@ let get_config_output is_windows =
 
 let should_patch config_map =
   match Js.Dict.get config_map "version" with
-    | Some version -> Js.String.indexOf "4.02.3" version >= 0
-    | None -> false
+  | Some version -> Js.String.indexOf "4.02.3" version >= 0
+  | None -> false
 
 let () =
   let dirname = match [%node __dirname] with
@@ -146,15 +146,20 @@ let () =
   *)
   let is_windows = Process.process##platform = "win32" in
   match get_config_output is_windows with
-    | Some config_map ->
-      if should_patch config_map then
-        patch_config
-            (Path.join [| dirname; ".."; "jscomp" |])
-            config_map is_windows
-      else Process.exit 2
-    | None ->
-      prerr_endline "configuration failure";
-      Process.exit 2
+  | Some config_map ->
+    if should_patch config_map then
+      patch_config
+        (Path.join [| dirname; ".."; "jscomp" |])
+        config_map is_windows
+    else Process.exit 2
+  | None ->
+    ((if is_windows then
+        prerr_endline "Configuration Failure : Cannot find config_map"
+      else
+        prerr_endline "Not Windows 32 Bit System");
+     Process.exit 2)
+
+
 
 (* local variables: *)
 (* compile-command: "bscc -c config_compiler.ml" *)
