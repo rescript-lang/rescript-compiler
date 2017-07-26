05125aa250aa3a593aad6aea1a3225d3d7c01d08
Hongbo Zhang <bobzhang1988@gmail.com>
2017-07-26 11:18:45 +0800

    remove js-demo

--- docs/js-demo/examples/tailcall.ml
+++
@@ -1,9 +0,0 @@
-let tail_sum n =
-  let rec aux acc i =
-    if i <= n then
-      aux (acc + i) (i + 1)
-    else acc
-  in aux 0 0
-
-
-let () = Js.log (tail_sum 100) 
--- docs/js-demo/examples/use_js_stdlib.ml
+++
@@ -1,7 +0,0 @@
-open Js
-let () =
-  [| 1; 2; 3; 4 |]
-  |> Array.filter (fun  x -> x > 2)
-  |> Array.mapi (fun  x i -> x + i)
-  |> Array.reduce (fun  x y -> x + y) 0
-  |> log
