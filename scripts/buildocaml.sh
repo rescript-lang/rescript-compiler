3efdf36f1df7e58cf88d8a9f3d1ed3789150590b
Hongbo Zhang <bobzhang1988@gmail.com>
2017-07-28 09:36:49 +0800

    remove tar command deps

--- scripts/buildocaml.sh
+++ scripts/buildocaml.sh
@@ -5,12 +5,5 @@ set -e
 export OCAMLPARAM='_,bin-annot=1'
 export OCAMLRUNPARAM=b
 
-cd vendor
-OCAML=ocaml
-
-rm -rf $OCAML
-mkdir -p $OCAML
-tar -zxvf ./ocaml.tar.gz -C $OCAML > /dev/null  2>&1
-cd $OCAML 
-echo $PWD
+cd vendor/ocaml
 ./configure -prefix `pwd`  -no-ocamldoc -no-ocamlbuild -no-shared-libs -no-curses -no-graph -no-pthread -no-debugger  && make -j9 world.opt && make install  && cd ..
---
+++ vendor/README.md
@@ -0,0 +1,4 @@
+BuckleScript vendors OCaml sourcetree and Ninja using git subtree
+
+https://www.atlassian.com/blog/git/alternatives-to-git-submodule-git-subtree
+
