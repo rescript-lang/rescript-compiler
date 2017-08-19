379a0429a8763faa0fb9807e8074eb08b5fe5b4e
Cheng Lou <chenglou92@gmail.com>
2017-08-18 21:41:27 -0700

    Change Js.toOpt to Js.toOption
    Per offline discussion. For consistency we'll use `to[ModuleName]`.

--- jscomp/runtime/js.mli
+++ jscomp/runtime/js.mli
@@ -73,7 +73,7 @@ type + 'a nullable
 
 type + 'a null_undefined = 'a nullable    
 
-external toOpt : 'a nullable  -> 'a option = "#null_undefined_to_opt"
+external toOption : 'a nullable  -> 'a option = "#null_undefined_to_opt"
 external test : 'a nullable -> bool = "#is_nil_undef"
 
 type boolean
