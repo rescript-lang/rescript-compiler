dc194b231cb9ea029fb33a0305ed7375e42aa1fa
Hongbo Zhang <bobzhang1988@gmail.com>
2017-08-29 16:07:52 +0800

    fix #1029

--- jscomp/core/jsoo_main.ml
+++ jscomp/core/jsoo_main.ml
@@ -45,7 +45,7 @@ let () =
   Clflags.unsafe_string := false;
   Clflags.record_event_when_debug := false
 
-let implementation impl ppf  str  =
+let implementation prefix impl ppf  str  =
   let modulename = "Test" in
   (* let env = !Toploop.toplevel_env in *)
   (* Compmisc.init_path false; *)
@@ -56,7 +56,8 @@ let implementation impl ppf  str  =
   let finalenv = ref Env.empty in
   let types_signature = ref [] in
   try 
-  impl (Lexing.from_string str )
+  impl (Lexing.from_string 
+    (if prefix then "[@@@bs.config{no_export}]\n#1 \"repl.ml\"\n"  ^ str else str ))
   |> !Ppx_entry.rewrite_implementation
   |> (fun x -> 
       let (a,b,c,signature) = Typemod.type_implementation_more modulename modulename modulename env x in
@@ -107,9 +108,10 @@ let string_of_fmt (f : Format.formatter -> 'a -> unit) v =
     Format.pp_print_flush fmt () in
   Buffer.contents buf 
 
-let compile  impl : string -> string = string_of_fmt (implementation  impl )
+let compile  impl : string -> string = string_of_fmt (implementation  false impl )
 (** TODO: add `[@@bs.config{no_export}]\n# 1 "repl.ml"`*)
-let shake_compile impl : string -> string = string_of_fmt (implementation impl )
+let shake_compile impl : string -> string = 
+  string_of_fmt (implementation true impl )
 
 
 (** *)
