716db8e3718d7ca005a886eee68a19e3dacfb53f
Hongbo Zhang <bobzhang1988@gmail.com>
2017-09-04 10:32:58 +0800

    complete test on third party libs

--- jscomp/build_tests/bscx/input.js
+++ jscomp/build_tests/bscx/input.js
@@ -2,17 +2,20 @@
 var p = require('child_process')
 var fs = require('fs')
 var assert = require('assert')
+var path = require('path')
+
+p.execSync(`bsc -bs-main main.ml`, { cwd: __dirname })
 
 
-p.execSync(`bsc -bs-main a2.ml`)
-
-assert(require(`./a2.js`).v, 2)
-
-fs.readdirSync(`.`,'utf8').forEach(x=>{
-    if (x !=='input.js' && x.endsWith('.js')){
-        // console.log(x)
-        fs.unlinkSync(x)
+try {
+    assert.equal(require(path.join(__dirname, `main.js`)).v, 5)
+} finally {
+    fs.readdirSync(`.`, 'utf8').forEach(x => {
+        if (x !== 'input.js' && x.endsWith('.js')) {
+            // console.log(x)
+            fs.unlinkSync(x)
+        }
     }
+    )
 }
-)
 
