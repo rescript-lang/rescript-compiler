954c2f73c9668ccf7687277de7e47d904faf3d31
Hongbo Zhang <bobzhang1988@gmail.com>
2017-09-07 17:16:57 +0800

    optimize a hot path

--- jscomp/ext/ext_path.mli
+++ jscomp/ext/ext_path.mli
@@ -22,10 +22,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *)
 
-
-type t = 
-  | File of string 
-  | Dir of string 
+type t 
 
 
 
