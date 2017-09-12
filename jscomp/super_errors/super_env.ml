a72a81f853b7fe91a2d185b3fe5fa7ac69cb7bf0
Cheng Lou <chenglou92@gmail.com>
2017-09-11 17:13:00 -0700

    [Super errors] Fix next line indentation

--- jscomp/super_errors/super_env.ml
+++ jscomp/super_errors/super_env.ml
@@ -1,16 +1,22 @@
-open Format
+let fprintf = Format.fprintf
 
 (* taken from https://github.com/BuckleScript/ocaml/blob/d4144647d1bf9bc7dc3aadc24c25a7efa3a67915/typing/env.ml#L1842 *)
 (* modified branches are commented *)
 let report_error ppf = function
-  | Env.Illegal_renaming(name, modname, filename) -> fprintf ppf
+  | Env.Illegal_renaming(name, modname, filename) -> 
       (* modified *)
-      "Hello what error msg goes here: %a@ contains the compiled interface for @ \
-       %s when %s was expected"
-      Location.print_filename filename name modname
-  | Inconsistent_import(name, source1, source2) -> fprintf ppf
-      (* TODO: this one needs to be muuuuch better *)
-      "@[<hov>The files %a@ and %a@ \
+      fprintf ppf
+        "@[You referred to the module %s, but we've found one called %s instead.@ \
+          Is the name's casing right?@]"
+        name modname
+  | Inconsistent_import(name, source1, source2) -> 
+      (* modified *)
+     fprintf ppf "@[<v>\
+        @[@{<info>It's possible that your build is stale.@}@ Try to clean the artifacts and build again?@]@,@,\
+        @[@{<info>Here's the original error message@}@]@,\
+      @]";
+      fprintf ppf
+        "@[<hov>The files %a@ and %a@ \
               make inconsistent assumptions@ over interface %s@]"
       Location.print_filename source1 Location.print_filename source2 name
   | Need_recursive_types(import, export) ->
@@ -31,6 +37,7 @@ let report_error ppf = function
       fprintf ppf "'%s' is not a valid value identifier."
         name
 
+(* This will be called in super_main. This is how you'd override the default error printer from the compiler & register new error_of_exn handlers *)
 let () =
   Location.register_error_of_exn
     (function
@@ -38,6 +45,6 @@ let () =
               | Illegal_value_name (loc, _)
                as err) when loc <> Location.none ->
           Some (Super_location.error_of_printer loc report_error err)
-      | Env.Error err -> Some (Location.error_of_printer_file report_error err)
+      | Env.Error err -> Some (Super_location.error_of_printer_file report_error err)
       | _ -> None
     )
