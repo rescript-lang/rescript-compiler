6413d4db1c27066a35eff496f4e9caf29ed3aab4
Cristiano Calcagno <cristianoc@fb.com>
2017-09-13 12:08:05 +0200

    [super_errors] Vars not generalized inside module
    Extend super errors with support for ReasonReact error messages when the component is defined inside a module.

--- jscomp/super_errors/super_reason_react.mli
+++ jscomp/super_errors/super_reason_react.mli
@@ -1,6 +1,9 @@
 val type_is_component_spec: Types.type_expr -> bool
 (** Used by super_typemod when we detect the message "... contains type variables that cannot be generalized" *)
 
+val module_type_is_component_spec: Types.module_type -> bool
+(** Used by super_typemod when we detect the message "... contains type variables that cannot be generalized" *)
+
 val state_escape_scope: (Types.type_expr * Types.type_expr) list -> bool
 (** Used by super_typecore when we detect the message "The type constructor state would escape its scope" *)
 
