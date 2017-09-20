b7f2d92299962ffc3bc0a50f5d573a8a819b7d23
Hongbo Zhang <bobzhang1988@gmail.com>
2017-09-20 08:11:43 +0800

    fix build error

--- jscomp/build_tests/dev/input.js
+++ jscomp/build_tests/dev/input.js
@@ -1,6 +1,7 @@
-var u = require("./examples/test.js")
+
 var assert = require('assert')
 var path = require('path')
 var p = require('child_process')
-p.execSync(`bsb`)
+p.execSync(`bsb -make-world`, {cwd:__dirname,shell:true,encoding:'utf8'})
+var u = require("./examples/test.js")
 assert.ok(path.basename(u.v)==='demo.ml')
