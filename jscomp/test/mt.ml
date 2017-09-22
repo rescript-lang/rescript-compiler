75833a9ba47587b1217f0086c3d49f0ffebea3c8
Hongbo Zhang <bobzhang1988@gmail.com>
2017-09-22 16:52:28 +0800

    poor man test for promise

--- jscomp/test/mt.ml
+++ jscomp/test/mt.ml
@@ -2,46 +2,49 @@
 
 
 external describe : string -> (unit -> unit[@bs]) -> unit = "describe"
-    [@@bs.val]
+[@@bs.val]
 
-external it : string -> (unit -> unit) -> unit = "it"
-    [@@bs.val]
+external it : string -> (unit -> unit[@bs.uncurry]) -> unit = "it"
+[@@bs.val]
+
+external it_promise : string -> (unit -> _ Js.Promise.t [@bs.uncurry]) -> unit = "it"
+[@@bs.val]
 
 external eq : 'a -> 'a -> unit = "deepEqual"
-    [@@bs.val]
-    [@@bs.module "assert"]
+[@@bs.val]
+[@@bs.module "assert"]
 
 external neq : 'a -> 'a -> unit = "notDeepEqual"
-    [@@bs.val]
-    [@@bs.module "assert"]
+[@@bs.val]
+[@@bs.module "assert"]
 
 external strict_eq : 'a -> 'a -> unit = "strictEqual"
-    [@@bs.val]
-    [@@bs.module "assert"]
+[@@bs.val]
+[@@bs.module "assert"]
 
 external strict_neq : 'a -> 'a -> unit = "notStrictEqual"
-    [@@bs.val]
-    [@@bs.module "assert"]
+[@@bs.val]
+[@@bs.module "assert"]
 
 external ok : Js.boolean -> unit = "ok"
-    [@@bs.val]
-    [@@bs.module "assert"]
+[@@bs.val]
+[@@bs.module "assert"]
 
 external fail : 'a -> 'a -> string Js.undefined -> string -> unit = "fail"
-    [@@bs.val]
-    [@@bs.module "assert"]
+[@@bs.val]
+[@@bs.module "assert"]
 
 
 external dump : 'a array -> unit = "console.log"
-  [@@bs.val]
-  [@@bs.splice]
+[@@bs.val]
+[@@bs.splice]
 
 external throws : (unit -> unit) -> unit = "throws"
-  [@@bs.val]
-  [@@bs.module "assert"]
+[@@bs.val]
+[@@bs.module "assert"]
 (** There is a problem --
     it does not return [unit]
- *)
+*)
 
 let assert_equal = eq
 let assert_notequal = neq
@@ -81,25 +84,41 @@ type eq =
   (* TODO: | Exception : exn -> (unit -> unit) -> _ eq  *)
 
 type  pair_suites = (string * (unit ->  eq)) list
-
+type promise_suites = (string * eq Js.Promise.t) list 
 let close_enough ?(threshold=0.0000001 (* epsilon_float *)) a b =
   abs_float (a -. b) < threshold
 
 let node_from_pair_suites (name : string) (suites :  pair_suites) =
   Js.log (name, "testing");
   List.iter (fun (name, code) ->
-              match code () with
-              | Eq(a,b) -> Js.log (name , a, "eq?", b )
-              | Neq(a,b) -> Js.log (name, a, "neq?",   b )
-              | StrictEq(a,b) -> Js.log (name , a, "strict_eq?", b )
-              | StrictNeq(a,b) -> Js.log (name, a, "strict_neq?",   b )
-              | Approx(a,b) -> Js.log (name, a, "~",  b)
-              | ApproxThreshold(t, a, b) -> Js.log (name, a, "~", b, " (", t, ")")
-              | ThrowAny fn -> ()
-              | Fail _ -> Js.log "failed"
-              | FailWith msg -> Js.log ("failed: " ^ msg)
-              | Ok a -> Js.log (name, a, "ok?")
-              ) suites
+      match code () with
+      | Eq(a,b) -> Js.log (name , a, "eq?", b )
+      | Neq(a,b) -> Js.log (name, a, "neq?",   b )
+      | StrictEq(a,b) -> Js.log (name , a, "strict_eq?", b )
+      | StrictNeq(a,b) -> Js.log (name, a, "strict_neq?",   b )
+      | Approx(a,b) -> Js.log (name, a, "~",  b)
+      | ApproxThreshold(t, a, b) -> Js.log (name, a, "~", b, " (", t, ")")
+      | ThrowAny fn -> ()
+      | Fail _ -> Js.log "failed"
+      | FailWith msg -> Js.log ("failed: " ^ msg)
+      | Ok a -> Js.log (name, a, "ok?")
+    ) suites
+
+let handleCode spec = 
+
+  match spec with
+  | Eq(a,b) -> assert_equal a b
+  | Neq(a,b) -> assert_notequal a b
+  | StrictEq(a,b) -> assert_strict_equal a b
+  | StrictNeq(a,b) -> assert_strict_notequal a b
+  | Ok(a) -> assert_ok a
+  | Approx(a, b) ->
+    if not (close_enough a b) then assert_equal a b (* assert_equal gives better ouput *)
+  | ApproxThreshold(t, a, b) ->
+    if not (close_enough ~threshold:t a b) then assert_equal a b (* assert_equal gives better ouput *)
+  | ThrowAny fn -> throws fn
+  | Fail _ -> assert_fail "failed"
+  | FailWith msg -> assert_fail msg
 
 let from_pair_suites name (suites :  pair_suites) =
   match Array.to_list Node.Process.process##argv with
@@ -109,25 +128,29 @@ let from_pair_suites name (suites :  pair_suites) =
           suites |>
           List.iter (fun (name, code) ->
               it name (fun _ ->
-                  match code () with
-                  | Eq(a,b) -> assert_equal a b
-                  | Neq(a,b) -> assert_notequal a b
-                  | StrictEq(a,b) -> assert_strict_equal a b
-                  | StrictNeq(a,b) -> assert_strict_notequal a b
-                  | Ok(a) -> assert_ok a
-                  | Approx(a, b) ->
-                    if not (close_enough a b) then assert_equal a b (* assert_equal gives better ouput *)
-                  | ApproxThreshold(t, a, b) ->
-                    if not (close_enough ~threshold:t a b) then assert_equal a b (* assert_equal gives better ouput *)
-                  | ThrowAny fn -> throws fn
-                  | Fail _ -> assert_fail "failed"
-                  | FailWith msg -> assert_fail msg
+                 handleCode (code ())
                 )
             )
         )
     else node_from_pair_suites name suites
   | _ -> ()
-
+let val_unit = Js.Promise.resolve ()
+let from_promise_suites name (suites : (string * _ Js.Promise.t ) list) =
+  match Array.to_list Node.Process.process##argv with
+  | cmd :: _ ->
+    if is_mocha () then
+      describe name (fun [@bs] () ->
+          suites |>
+          List.iter (fun (name, code) ->
+              it_promise name (fun _ ->
+                code |> Js.Promise.then_ (fun x -> handleCode x; val_unit)
+                 
+                )
+            )
+        )
+    else Js.log "promise suites" (* TODO*)
+  | _ -> ()
+  
 (*
 Note that [require] is a file local value,
 we need type [require]
