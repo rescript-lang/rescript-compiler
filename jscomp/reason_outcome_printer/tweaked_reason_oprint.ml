6e0dff75bf29803f378b085352ccae0b2bff76fa
Cheng Lou <chenglou92@gmail.com>
2017-10-10 17:42:22 -0700

    Fix reason outcome printer for bs tags (#2102)
    The tag `[@bs]` should come after the parentheses, not before.
    This also cleans up some bits so that the syncing can be a bit easier; see https://github.com/facebook/reason/pull/1484

--- jscomp/reason_outcome_printer/tweaked_reason_oprint.ml
+++ jscomp/reason_outcome_printer/tweaked_reason_oprint.ml
@@ -251,71 +251,81 @@ and print_simple_out_type ppf =
     Otyp_class (ng, id, tyl) ->
       fprintf ppf "@[%s#%a%a@]" (if ng then "_" else "")
         print_ident id print_typargs tyl
-#if undefined BS_NO_COMPILER_PATCH then         
-  | Otyp_constr ( (Oide_dot ((Oide_dot (Oide_ident "Js", "Internal")),
-                             ("fn" | "meth" as name )) as id) ,
-                 ([Otyp_variant(_,Ovar_fields [ variant, _, tys], _,_); result] as tyl))
-    ->
-      (* Otyp_arrow*)
+
+  (* BuckleScript-specific external. See the manual for the usage of [@bs]. This
+    [@bs] is processed into a type that looks like `Js.Internal.fn ...`. This
+    leaks during error reporting, where the type is printed. Here, we print it
+    back from `Js.Internal.fn [ `Arity_2 ('c, 'd) ] 'e` into `('a => 'b => int) [@bs]` *)
+  (* same for `Js_internal.fn ...`. Either might shown *)
+  | Otyp_constr (
+      (Oide_dot (
+        (Oide_dot ((Oide_ident "Js"), "Internal") | Oide_ident "Js_internal"),
+        ("fn" | "meth" as name)
+      ) as id),
+      ([Otyp_variant(_, Ovar_fields [variant, _, tys], _, _); result] as tyl)
+    ) ->
+      (* Otyp_arrow *)
       let make tys result =
         if tys = [] then
           Otyp_arrow ("", Otyp_constr (Oide_ident "unit", []),result)
         else
-            match tys with
-          | [ Otyp_tuple tys as single] -> 
-              if variant = "Arity_1" then
-                Otyp_arrow ("", single, result)
-              else 
-                List.fold_right (fun x acc  -> Otyp_arrow("",x,acc) ) tys result
-          | [single] ->
-              Otyp_arrow ("", single, result)
-          | _ -> 
-              raise_notrace Not_found
-      in
-      begin match (make tys result) with
-      | exception _ ->
-          begin 
-            pp_open_box ppf 0;
-            print_typargs ppf tyl;
-            print_ident ppf id;
-            pp_close_box ppf ()
-          end
-      | res ->
-          begin match name  with
-          | "fn" -> 
-              fprintf ppf "@[<0>(%a@ [@bs])@]" print_out_type_1 res
-          | "meth" ->
-              fprintf ppf "@[<0>(%a@ [@bs.meth])@]" print_out_type_1 res
-          | _ -> assert false 
-          end
-      end
-  | Otyp_constr ((Oide_dot (Oide_dot (Oide_ident "Js", "Internal"), "meth_callback" ) as id) ,
-                 ([Otyp_variant(_,Ovar_fields [ variant, _, tys], _,_); result] as tyl))
-    ->
-      let make tys result =
           match tys with
-          | [ Otyp_tuple tys as single ] -> 
-              if variant = "Arity_1" then Otyp_arrow ("", single, result)
-              else 
-                List.fold_right (fun x acc  -> Otyp_arrow("",x,acc) ) tys result
-          | [single] ->
+          | [ Otyp_tuple tys as single] -> 
+            if variant = "Arity_1" then
               Otyp_arrow ("", single, result)
+            else 
+              List.fold_right (fun x acc -> Otyp_arrow ("", x, acc)) tys result
+          | [single] ->
+            Otyp_arrow ("", single, result)
           | _ -> 
-              raise_notrace Not_found
+            raise_notrace Not_found
       in
       begin match (make tys result) with
       | exception _ ->
-          begin 
-            pp_open_box ppf 0;
-            print_typargs ppf tyl;
-            print_ident ppf id;
-            pp_close_box ppf ()
-          end
+        begin 
+          pp_open_box ppf 0;
+          print_typargs ppf tyl;
+          print_ident ppf id;
+          pp_close_box ppf ()
+        end
       | res ->
-          fprintf ppf "@[<0>(%a@ [@bs.this])@]" print_out_type_1 res
-
+        begin match name  with
+        | "fn" -> fprintf ppf "@[<0>(%a)@ [@bs]@]" print_out_type_1 res
+        | "meth" -> fprintf ppf "@[<0>(%a)@ [@bs.meth]@]" print_out_type_1 res
+        | _ -> assert false 
+        end
       end
-#end         
+  (* also BuckleScript-specific. See the comment in the previous pattern *)
+  | Otyp_constr (
+      (Oide_dot (
+        (Oide_dot ((Oide_ident "Js"), "Internal") | Oide_ident "Js_internal"), "meth_callback" ) as id
+      ),
+      ([Otyp_variant(_, Ovar_fields [variant, _, tys], _,_); result] as tyl)
+    ) ->
+      let make tys result =
+        match tys with
+        | [Otyp_tuple tys as single] -> 
+          if variant = "Arity_1" then
+            Otyp_arrow ("", single, result)
+          else 
+            List.fold_right (fun x acc  -> Otyp_arrow("", x, acc) ) tys result
+        | [single] ->
+          Otyp_arrow ("", single, result)
+        | _ -> 
+          raise_notrace Not_found
+      in
+      begin match (make tys result) with
+      | exception _ ->
+        begin 
+          pp_open_box ppf 0;
+          print_typargs ppf tyl;
+          print_ident ppf id;
+          pp_close_box ppf ()
+        end
+      | res ->
+        fprintf ppf "@[<0>(%a)@ [@bs.this]@]" print_out_type_1 res
+      end
+
   | Otyp_constr (id, tyl) ->
       pp_open_box ppf 0;
       print_ident ppf id;
@@ -562,30 +572,43 @@ and print_out_sig_item ppf =
           | Orec_first -> "type"
           | Orec_next  -> "and")
         ppf td
-  | Osig_value(name, ty, prims) ->
-    let kwd = if prims = [] then "let" else "external" in
+#if defined BS_NO_COMPILER_PATCH then
+  | Osig_value {oval_name; oval_type; oval_prims; oval_attributes} ->
+    let kwd = if oval_prims = [] then "let" else "external" in
+    let pr_prims ppf =
+      function
+        [] -> ()
+      | s :: sl ->
+          fprintf ppf "@ = \"%s\"" s;
+          List.iter (fun s -> fprintf ppf "@ \"%s\"" s) sl
+    in
+    fprintf ppf "@[<2>%s %a :@ %a%a%a@]" kwd value_ident oval_name
+        !out_type oval_type pr_prims oval_prims
+        (fun ppf -> List.iter (fun a -> fprintf ppf "@ [@@@@%s]" a.oattr_name))
+        oval_attributes
+  | Osig_ellipsis ->
+    fprintf ppf "..."
+#else
+  | Osig_value(oval_name, oval_type, oval_prims) ->
+    let kwd = if oval_prims = [] then "let" else "external" in
     let pr_prims ppf =
       function
         [] -> ()
       | s :: sl ->
           fprintf ppf "@ = \"%s\"" s;
           List.iter (fun s -> 
-(* TODO: in general, we should print bs attributes, some attributes like
-  bs.splice does need it *)
-#if undefined BS_NO_COMPILER_PATCH then
+    (* TODO: in general, we should print bs attributes, some attributes like
+      bs.splice does need it *)
     let len = String.length s in
     if len >= 3 && s.[0] = 'B' && s.[1] = 'S' && s.[2] = ':' then
-      fprintf ppf "@ \"BS-EXTERNAL\"" 
+      fprintf ppf "@ \"BuckleScript External\"" 
     else
       fprintf ppf "@ \"%s\"" s
-#else    
-      fprintf ppf "@ \"%s\"" s
-#end               
     ) sl
     in
-    fprintf ppf "@[<2>%s %a :@ %a%a@]" kwd value_ident name !out_type 
-      ty pr_prims prims
-
+    fprintf ppf "@[<2>%s %a :@ %a%a@]" kwd value_ident oval_name
+        !out_type oval_type pr_prims oval_prims
+#end
 
 and print_out_type_decl kwd ppf td =
   let print_constraints ppf =
