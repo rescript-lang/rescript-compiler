1dfbb81b85f6992802f962b37239d614f20346f4
Hongbo Zhang <bobzhang1988@gmail.com>
2017-10-25 13:36:12 +0800

    more comments

--- jscomp/syntax/ast_external_mk.ml
+++ jscomp/syntax/ast_external_mk.ml
@@ -22,7 +22,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *)
 
-let create_local_external loc 
+let local_external loc 
      ?(pval_attributes=[])
      ~pval_prim
      ~pval_type 
