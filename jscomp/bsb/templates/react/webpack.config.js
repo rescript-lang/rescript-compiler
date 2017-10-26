1b53ad611b96c71eb56df08cb6c44ecb5e2c3647
glennsl <glenn@slotte.net>
2017-10-26 03:06:47 +0200

    added in-source and suffix config, .gitignored .bsb.lock and upgraded webpack

--- jscomp/bsb/templates/react/webpack.config.js
+++ jscomp/bsb/templates/react/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './lib/js/src/index.js',
+  entry: './src/index.bs.js',
   output: {
     path: path.join(__dirname, "build"),
     filename: 'index.js',
