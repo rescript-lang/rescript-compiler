21284fe6c71bd3d747535edebdac54c16ab4fe85
Hongbo Zhang <bobzhang1988@gmail.com>
2017-10-27 09:22:11 +0800

    fix ci

--- jscomp/build_tests/rerror/input.js
+++ jscomp/build_tests/rerror/input.js
@@ -3,6 +3,6 @@ var assert = require('assert')
 var o = child_process.spawnSync(`bsb `,{cwd:__dirname, encoding:'utf8',shell:true})
 
 
-
-var u = o.stdout.match(/int => int/g)
+// verify the output is in reason syntax
+var u = o.stdout.match(/=>/g)
 assert.ok(u.length === 2)
