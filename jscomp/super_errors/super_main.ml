0b88328299b33a63a86f550eeb52393e44a086cb
Jared Forsyth <jared@jaredforsyth.com>
2017-11-03 21:47:31 -0600

    setup colors globally

--- jscomp/super_errors/super_main.ml
+++ jscomp/super_errors/super_main.ml
@@ -1,10 +1,9 @@
 (* the entry point. This is used by js_main.ml *)
 let setup () =
+  Super_location.setup_colors();
   Super_location.setup ();
-  Misc.Color.setup !Clflags.color;
   Super_typetexp.setup ();
   Super_typemod.setup ();
   Super_typecore.setup ();
-  Misc.Color.setup !Clflags.color;
   Super_env.setup ();
   Super_pparse.setup ();
