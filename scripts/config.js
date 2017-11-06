ae44e8b69a6f7be3dfba786d8fcdfd59dc6961ed
Hongbo Zhang <hongbozhang@fb.com>
2017-11-06 11:13:38 +0800

    clean up Makefile

---
+++ scripts/config.js
@@ -0,0 +1,22 @@
+//@ts-check
+var os = require('os')
+var os_type = os.type()
+var os_arch = os.arch()
+var is_windows = !(os_type.indexOf('Windows') < 0)
+var is_bsd = !(os_type.indexOf('BSD') < 0)
+
+exports.is_windows = is_windows
+var sys_extension;
+switch (os.type()) {
+    case 'Darwin':
+        sys_extension = ".darwin"; break;
+    case 'Linux':
+        sys_extension = ".linux64"; break;
+    case 'Windows_NT':
+        sys_extension = ".win"; break;
+    default: throw ("Not supported" + os.type())
+}
+
+exports.sys_extension  = sys_extension
+var make = is_bsd ? 'gmake' : 'make'
+exports.make  = make
--- scripts/install.js
+++ scripts/install.js
@@ -12,76 +12,83 @@
 // This will be run in npm postinstall, don't use too fancy features here
 
 var child_process = require('child_process')
-
 var fs = require('fs')
 var path = require('path')
 var os = require('os')
 
 var os_type = os.type()
-var os_arch = os.arch()
-var is_windows = !(os_type.indexOf('Windows') < 0)
-var is_bsd = !(os_type.indexOf('BSD') < 0)
+
+
+
+
 var root_dir = path.join(__dirname, '..')
 var lib_dir = path.join(root_dir, 'lib')
-var jscomp = path.join(root_dir, 'jscomp')
-
-
-var working_dir = process.cwd()
-console.log("Working dir", working_dir)
-var jscomp_dir_config = { cwd: jscomp, stdio: [0, 1, 2] }
 var root_dir_config = { cwd: root_dir, stdio: [0, 1, 2] }
-var build_util = require('./build_util')
-var vendor_ninja_version = '1.8.2'
 
-var ninja_bin_output = path.join(root_dir, 'lib', 'ninja.exe')
-var ninja_source_dir = path.join(root_dir, 'vendor', 'ninja')
-var ninja_build_dir = path.join(root_dir, 'vendor', 'ninja-build')
+var config = require('./config.js')
+var make = config.make
+var is_windows = config.is_windows
+var sys_extension = 
+// Add vendor bin path
+// So that second try will work
+process.env.PATH =
+    path.join(__dirname, '..', 'vendor', 'ocaml', 'bin') +
+    path.delimiter +
+    process.env.PATH
 
-function build_ninja() {
-    console.log('No prebuilt Ninja, building Ninja now')
-    var build_ninja_command = "./configure.py --bootstrap"
-    child_process.execSync(build_ninja_command, { cwd: ninja_source_dir, stdio: [0, 1, 2] })
-    fs.renameSync(path.join(ninja_source_dir, 'ninja'), ninja_bin_output)
-    console.log('ninja binary is ready: ', ninja_bin_output)
-}
+function setUpNinja() {
+    var vendor_ninja_version = '1.8.2'
+    var ninja_bin_output = path.join(root_dir, 'lib', 'ninja.exe')
+    var ninja_source_dir = path.join(root_dir, 'vendor', 'ninja')
+    var ninja_build_dir = path.join(root_dir, 'vendor', 'ninja-build')
 
-// sanity check to make sure the binary actually runs. Used for Linux. Too many variants
-function test_ninja_compatible(binary_path) {
-    var version;
-    try {
-        version = child_process.execSync(JSON.stringify(binary_path) + ' --version', {
-            encoding: 'utf8',
-            stdio: ['pipe', 'pipe', 'ignore'] // execSync outputs to stdout even if we catch the error. Silent it here
-        }).trim();
-    } catch (e) {
-        console.log('ninja not compatible?', String(e))
-        return false;
+    function build_ninja() {
+        console.log('No prebuilt Ninja, building Ninja now')
+        var build_ninja_command = "./configure.py --bootstrap"
+        child_process.execSync(build_ninja_command, { cwd: ninja_source_dir, stdio: [0, 1, 2] })
+        fs.renameSync(path.join(ninja_source_dir, 'ninja'), ninja_bin_output)
+        console.log('ninja binary is ready: ', ninja_bin_output)
     }
-    return version === vendor_ninja_version;
-};
+
+    // sanity check to make sure the binary actually runs. Used for Linux. Too many variants
+    function test_ninja_compatible(binary_path) {
+        var version;
+        try {
+            version = child_process.execSync(JSON.stringify(binary_path) + ' --version', {
+                encoding: 'utf8',
+                stdio: ['pipe', 'pipe', 'ignore'] // execSync outputs to stdout even if we catch the error. Silent it here
+            }).trim();
+        } catch (e) {
+            console.log('ninja not compatible?', String(e))
+            return false;
+        }
+        return version === vendor_ninja_version;
+    };
 
 
-var ninja_os_path
-if (is_windows) {
-    ninja_os_path = path.join(ninja_build_dir, 'ninja.win')
-} else if (os_type === 'Darwin') {
-    ninja_os_path = path.join(ninja_build_dir, 'ninja.darwin')
-} else if (os_type === 'Linux') {
-    ninja_os_path = path.join(ninja_build_dir, 'ninja.linux64')
-}
-if (fs.existsSync(ninja_bin_output) && test_ninja_compatible(ninja_bin_output)) {
-    console.log("ninja binary is already cached: ", ninja_bin_output)
-} else if (fs.existsSync(ninja_os_path)) {
-    fs.renameSync(ninja_os_path, ninja_bin_output)
-    if (test_ninja_compatible(ninja_bin_output)) {
-        console.log("ninja binary is copied from pre-distribution")
+    var ninja_os_path = path.join(ninja_build_dir,'ninja' + sys_extension )
+    // if (is_windows) {
+    //     ninja_os_path = path.join(ninja_build_dir, 'ninja.win')
+    // } else if (os_type === 'Darwin') {
+    //     ninja_os_path = path.join(ninja_build_dir, 'ninja.darwin')
+    // } else if (os_type === 'Linux') {
+    //     ninja_os_path = path.join(ninja_build_dir, 'ninja.linux64')
+    // }
+    if (fs.existsSync(ninja_bin_output) && test_ninja_compatible(ninja_bin_output)) {
+        console.log("ninja binary is already cached: ", ninja_bin_output)
+    } else if (fs.existsSync(ninja_os_path)) {
+        fs.renameSync(ninja_os_path, ninja_bin_output)
+        if (test_ninja_compatible(ninja_bin_output)) {
+            console.log("ninja binary is copied from pre-distribution")
+        } else {
+            build_ninja()
+        }
     } else {
         build_ninja()
     }
-} else {
-    build_ninja()
 }
 
+setUpNinja()
 /**
  * raise an exception if not matched
  */
@@ -96,10 +103,7 @@ function matchedCompilerExn() {
     }
 }
 
-// Add vendor bin path
-// So that second try will work
-process.env.PATH = path.join(__dirname, '..', 'vendor', 'ocaml', 'bin') + path.delimiter + process.env.PATH
-var make = is_bsd ? 'gmake' : 'make';
+
 
 function non_windows_npm_release() {
 
@@ -129,6 +133,7 @@ function non_windows_npm_release() {
 
 }
 
+var build_util = require('./build_util.js')
 if (is_windows) {
     process.env.WIN32 = '1'
     console.log("Installing on Windows")
@@ -146,7 +151,10 @@ if (is_windows) {
     if (indeed_windows_release > 1) {
         // Make it more fault tolerant
         // =1 can still be okay (only ninja.win in this case)
-        child_process.execFileSync(path.join(__dirname, 'win_build.bat'), jscomp_dir_config)
+        child_process.execFileSync(
+            path.join(__dirname, 'win_build.bat'),
+            { cwd: path.join(root_dir, 'jscomp'), stdio: [0, 1, 2] }
+        )
         // clean.clean()
         console.log("Installing")
         build_util.install()
