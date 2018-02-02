2138ffc91a27bdf0fa31d1d736dc00a3ba9264cf
Hongbo Zhang <bobzhang1988@gmail.com>
2018-02-02 14:42:08 +0800

    tweak

--- jscomp/docgen.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -e 
-echo $BUCKLESCRIPT_API_DOC
-ocamlbuild -build-dir $BUCKLESCRIPT_API_DOC -cflags -I,+compiler-libs -docflags -sort,-I,+compiler-libs,-css-style,../style.css compiler.docdir/index.html 2>> ./build.compile
-
