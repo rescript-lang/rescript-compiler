c1063d6081479c211ed26bba0b19e8d063cf3a5e
glennsl <glenn@slotte.net>
2018-02-13 11:14:17 +0100

    undo removal of Js.Null.to_opt

---
+++ jscomp/test/js_null.js
@@ -0,0 +1,43 @@
+'use strict';
+
+
+function getExn(f) {
+  if (f !== null) {
+    return f;
+  } else {
+    throw new Error("Js.Null.getExn");
+  }
+}
+
+function bind(x, f) {
+  if (x !== null) {
+    return f(x);
+  } else {
+    return null;
+  }
+}
+
+function iter(x, f) {
+  if (x !== null) {
+    return f(x);
+  } else {
+    return /* () */0;
+  }
+}
+
+function fromOption(x) {
+  if (x) {
+    return x[0];
+  } else {
+    return null;
+  }
+}
+
+var from_opt = fromOption;
+
+exports.getExn = getExn;
+exports.bind = bind;
+exports.iter = iter;
+exports.fromOption = fromOption;
+exports.from_opt = from_opt;
+/* No side effect */
