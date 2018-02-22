a12a35661f13a565ba49441962d4cab77d2c61ca
Hongbo Zhang <bobzhang1988@gmail.com>
2018-02-22 12:12:22 +0800

    more regerssion tests

--- jscomp/test/gpr_2503_test.js
+++ jscomp/test/gpr_2503_test.js
@@ -1,5 +1,18 @@
 'use strict';
 
+var Mt = require("./mt.js");
+
+var suites = [/* [] */0];
+
+var test_id = [0];
+
+function eq(loc, x, y) {
+  return Mt.eq_suites(test_id, suites, loc, x, y);
+}
+
+function b(loc, b$1) {
+  return Mt.bool_suites(test_id, suites, loc, b$1);
+}
 
 function makeWrapper(foo, _) {
   var tmp = { };
@@ -35,6 +48,34 @@ function makeWrapper2(foo, _) {
 
 makeWrapper2(/* a */97, /* () */0);
 
+function makeWrapper3(foo, _) {
+  console.log(2);
+  var tmp = { };
+  if (foo) {
+    tmp.foo = (function () {
+          switch (foo[0]) {
+            case 97 : 
+                return "a";
+            case 98 : 
+                return "b";
+            
+          }
+        })();
+  }
+  return tmp;
+}
+
+b("File \"gpr_2503_test.ml\", line 25, characters 5-12", +("a" === makeWrapper3(/* Some */[/* a */97], /* () */0).foo));
+
+b("File \"gpr_2503_test.ml\", line 28, characters 5-12", +(undefined === makeWrapper3(/* None */0, /* () */0).foo));
+
+Mt.from_pair_suites("gpr_2503_test.ml", suites[0]);
+
+exports.suites = suites;
+exports.test_id = test_id;
+exports.eq = eq;
+exports.b = b;
 exports.makeWrapper = makeWrapper;
 exports.makeWrapper2 = makeWrapper2;
+exports.makeWrapper3 = makeWrapper3;
 /*  Not a pure module */
--- jscomp/test/gpr_2503_test.ml
+++ jscomp/test/gpr_2503_test.ml
@@ -1,5 +1,8 @@
+let suites = ref []
+let test_id = ref 0 
+let eq loc x y = Mt.eq_suites ~suites ~test_id loc x y 
 
-
+let b loc b = Mt.bool_suites ~suites ~test_id loc b 
 (* TODO:*)
 
 
@@ -13,4 +16,17 @@ let makeWrapper2 foo () = Js.log (make2 foo ())
 
 let _ = 
   makeWrapper2 `a () 
+
+
+external make3: ?foo:([`a|`b] [@bs.string]) -> unit -> _ = "" [@@bs.obj]
+
+let makeWrapper3 ?foo () = Js.log 2;  (make ?foo ())  
   
+;; b __LOC__ 
+( Js.eqUndefined "a" (makeWrapper3 ~foo:`a ())##foo)
+
+;; b __LOC__ 
+( Js.undefined =  (makeWrapper3  ())##foo)
+
+
+;; Mt.from_pair_suites __FILE__ !suites  
