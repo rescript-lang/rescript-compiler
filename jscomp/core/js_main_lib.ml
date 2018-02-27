f5f6f521f7dfa3086eb2fdda4e7282b9890a0ba3
Ryan Tsao <ryan.j.tsao@gmail.com>
2018-02-26 22:40:57 -0800

    Build whole_compiler_lib.ml for consumption in OCaml projects
    Signed-off-by: Ryan Tsao <ryan.j.tsao@gmail.com>

---
+++ jscomp/core/js_main_lib.ml
@@ -0,0 +1,19 @@
+open Js_implementation
+
+open Js_config
+
+open Bs_conditional_initial
+
+open Outcome_printer_ns
+
+open Oprint
+
+open Ocaml_batch_compile
+
+open Super_main
+
+open Ocaml_options
+
+open Tweaked_reason_oprint
+
+open Reason_outcome_printer_main
