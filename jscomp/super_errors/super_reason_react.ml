400593afc833986449382f1d6212d9edc3857802
Cheng Lou <chenglou92@gmail.com>
2018-03-01 02:24:55 -0800

    [Super errors] Tweak message for creating trying to create a reactElement without JSX
    Fixes https://github.com/reasonml-community/error-message-improvement/issues/29

--- jscomp/super_errors/super_reason_react.ml
+++ jscomp/super_errors/super_reason_react.ml
@@ -40,13 +40,19 @@ let rec get_to_bottom_of_aliases f = function
 let state_escape_scope = get_to_bottom_of_aliases (function
   (* https://github.com/BuckleScript/ocaml/blob/ddf5a739cc0978dab5e553443825791ba7b0cef9/typing/printtyp.ml?#L1348 *)
   (* so apparently that's the logic for detecting "the constructor out of scope" error *)
-  | ({desc = Tconstr (p, _, _)}, {desc = Tvar _; level}) 
+  | ({desc = Tconstr (p, _, _)}, {desc = Tvar _; level})
     when level < Path.binding_time p -> true
   | _ -> false
 )
 
 let is_array_wanted_reactElement = get_to_bottom_of_aliases (function
-  | ({desc = Tconstr (path1, _, _)}, {desc = Tconstr (path2, _, _)}) 
+  | ({desc = Tconstr (path1, _, _)}, {desc = Tconstr (path2, _, _)})
     when Path.last path1 = "array" && Path.last path2 = "reactElement" -> true
+  | _ -> false
+)
+
+let is_componentSpec_wanted_reactElement = get_to_bottom_of_aliases (function
+  | ({desc = Tconstr (path1, _, _)}, {desc = Tconstr (path2, _, _)})
+    when Path.last path1 = "componentSpec" && Path.last path2 = "reactElement" -> true
   | _ -> false
 )
