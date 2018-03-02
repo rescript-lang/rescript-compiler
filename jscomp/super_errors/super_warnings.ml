25ed5b329558efbe7d26a029090d7ab4500030cf
Cheng Lou <chenglou92@gmail.com>
2018-03-02 00:49:25 -0800

    [Super errors] Better warning msg
    Fixes https://github.com/reasonml-community/error-message-improvement/issues/3

--- jscomp/super_errors/super_warnings.ml
+++ jscomp/super_errors/super_warnings.ml
@@ -8,12 +8,12 @@ let message (warning : Warnings.t)  =
   | Partial_match s ->
       "You forgot to handle a possible value here, for example: \n" ^ s
   | Unerasable_optional_argument ->
-      String.concat "\n\n"
-        ["This is an optional argument at the final position of the function; omitting it while calling the function might be confused with currying. For example:";
-        "  let myTitle = displayTitle(\"hello!\");";
-        "if `displayTitle` accepts an optional argument at the final position, it'd be unclear whether `myTitle` is a curried function or the final result.";
-        "Here's the language's rule: an optional argument is erased as soon as the 1st positional (i.e. neither labeled nor optional) argument defined after it is passed in.";
-        "To solve this, you'd conventionally add an extra () argument at the end of the function declaration."]
+      String.concat ""
+        ["This optional parameter in final position will, in practice, not be optional.\n";
+        "  Reorder the parameters so that at least one non-optional one is in final position or, if all parameters are optional, insert a final ().\n\n";
+        "  Explanation: If the final parameter is optional, it'd be unclear whether a function application that omits it should be considered fully applied, or partially applied. Imagine writing `let title = display(\"hello!\")`, only to realize `title` isn't your desired result, but a curried call that takes a final optional argument, e.g. `~showDate`.\n\n";
+        "  Formal rule: an optional argument is considered intentionally omitted when the 1st positional (i.e. neither labeled nor optional) argument defined after it is passed in."
+        ]
   | Bad_module_name (modname) ->
       "This file's name is potentially invalid. The build systems conventionally turn a file name into a module name by upper-casing the first letter. " ^ modname ^ " isn't a valid module name.\n" ^
       "Note: some build systems might e.g. turn kebab-case into CamelCase module, which is why this isn't a hard error."
