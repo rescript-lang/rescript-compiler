36035be70a8b3eef7f9f3dc6989d4f915d113736
Hongbo Zhang <bobzhang1988@gmail.com>
2018-03-03 20:46:14 +0800

    add test case

--- jscomp/test/block_alias_test.js
+++ jscomp/test/block_alias_test.js
@@ -1,27 +1,77 @@
 'use strict';
 
+var Mt = require("./mt.js");
+var List = require("../../lib/js/list.js");
 var Block = require("../../lib/js/block.js");
+var Caml_obj = require("../../lib/js/caml_obj.js");
 
-var Block = /* module */[];
+var suites = [/* [] */0];
+
+var test_id = [0];
+
+function eq(loc, x, y) {
+  return Mt.eq_suites(test_id, suites, loc, x, y);
+}
+
+function b(loc, x) {
+  return Mt.bool_suites(test_id, suites, loc, x);
+}
 
 var Block$1 = /* module */[];
-
-var N_001 = /* v1 : A */Block.__(1, [
-    0,
-    1
-  ]);
-
-var N = /* module */[
-  /* Block */Block$1,
-  N_001
-];
 
 var v0 = /* A */Block.__(1, [
     0,
     1
   ]);
 
-exports.Block = Block;
+var Block$2 = /* module */[];
+
+var v1 = /* A */Block.__(1, [
+    0,
+    1
+  ]);
+
+var N = /* module */[
+  /* Block */Block$2,
+  /* v1 */v1
+];
+
+var Caml_obj$1 = /* module */[];
+
+var List$1 = /* module */[];
+
+var V = /* module */[/* List */List$1];
+
+var f = Caml_obj.caml_equal;
+
+eq("File \"block_alias_test.ml\", line 32, characters 6-13", List.length(/* :: */[
+          1,
+          /* :: */[
+            2,
+            /* [] */0
+          ]
+        ]), 2);
+
+b("File \"block_alias_test.ml\", line 33, characters 5-12", Caml_obj.caml_equal(v0, /* A */Block.__(1, [
+            0,
+            1
+          ])));
+
+eq("File \"block_alias_test.ml\", line 34, characters 6-13", v0, v1);
+
+Mt.from_pair_suites("block_alias_test.ml", suites[0]);
+
+var h = List.length;
+
+exports.suites = suites;
+exports.test_id = test_id;
+exports.eq = eq;
+exports.b = b;
+exports.Block = Block$1;
 exports.v0 = v0;
 exports.N = N;
-/* No side effect */
+exports.Caml_obj = Caml_obj$1;
+exports.V = V;
+exports.f = f;
+exports.h = h;
+/*  Not a pure module */
--- jscomp/test/block_alias_test.ml
+++ jscomp/test/block_alias_test.ml
@@ -1,4 +1,8 @@
 
+let suites =  ref []
+let test_id = ref 0 
+let eq loc x y = Mt.eq_suites ~test_id ~suites loc x y
+let b loc x = Mt.bool_suites ~test_id ~suites loc x
 
 module Block = struct 
 end 
@@ -13,3 +17,20 @@ module N = struct
   module Block = struct end
   let v1 = A (0,1) 
 end 
+
+module Caml_obj = struct 
+end 
+
+module V = struct 
+  module List = struct 
+  end 
+end 
+let f a b = a = b
+
+let h = List.length 
+
+;; eq __LOC__ (h [1;2]) 2 
+;; b __LOC__ (f v0 (A(0,1))) 
+;; eq __LOC__ v0 N.v1
+
+;; Mt.from_pair_suites __FILE__ !suites
