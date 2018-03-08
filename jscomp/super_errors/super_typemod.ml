50f58f32368ccdf335149d71a6fd00d72a6d36e9
Cheng Lou <chenglou92@gmail.com>
2018-03-08 05:57:43 -0800

    [Super errors] Improve the worst ReasonReact error
    Specialize the msg for each case.

--- jscomp/super_errors/super_typemod.ml
+++ jscomp/super_errors/super_typemod.ml
@@ -2,23 +2,48 @@ open Printtyp
 
 let fprintf = Format.fprintf
 
-
-let pp_component_type_not_generalizable_pre ppf =
-  fprintf ppf "@[<v>\
-    @[@{<info>Is this a ReasonReact reducerComponent or component with retained props?@}@ \
-    If so, you can fix it by doing one of these:@]@,\
-    @[- Defining the component's `make` function@]@,\
-    @[- Annotating the state once with a type, wherever it's used (e.g. render)@]@,\
-    @[- Annotating actions (in e.g. reducer)@]@,\
-    @[- Annotating retained props, if any@]@,@,\
-    @[@{<info>Here's the original error message@}@]\
-  @]@,"
-
-let pp_component_type_not_generalizable_post ppf () =
-  fprintf ppf
-  "@[This happens when the type system senses there's a mutation/side-effect,@ in combination with a polymorphic value.@,\
-  Using or annotating that value usually solves it.@ \
-  More info:@ https://realworldocaml.org/v1/en/html/imperative-programming-1.html#side-effects-and-weak-polymorphism@]"
+let non_generalizable_msg ppf ~result ~is_module print_fallback_msg =
+  let contain_vs_be =
+    if is_module then "This module seems to contain" else "This seems to be" in
+  match result with
+  | (_, true, _) :: _ ->
+    fprintf ppf "@[<v>\
+      @{<info>%s a ReasonReact reducerComponentWithRetainedProps?@}@ \
+      The retained props feature is deprecated.@ \
+      Please use a regular @{<info>reducerComponent@} and keep the props you want to retain in state.\
+    @]"
+    contain_vs_be
+  | (true, _, _) :: _->
+    fprintf ppf "@[<v>\
+      @[\
+        @{<info>%s a ReasonReact reducerComponent?@}@ \
+        We don't have@ all@ the@ type@ info@ for@ its@ @{<info>state@}.@ \
+        Make sure you've done the following: @]@,@,\
+        @[- Define the component `make` function@]@,\
+        @[- Define `reducer` in that `make` body@]@,\
+        @[- Annotate reducer's second parameter (state) with the desired state type\
+      @]\
+    @]"
+    contain_vs_be
+  | (_, _, true) :: _->
+    fprintf ppf "@[<v>\
+      @[\
+        @{<info>%s a ReasonReact reducerComponent?@}@ \
+        We don't have@ all@ the@ type@ info@ for@ its@ @{<info>action@}.@ \
+        Make sure you've done the following: @]@,@,\
+        @[- Define the component `make` function@]@,\
+        @[- Define `reducer` in that `make` body@]@,\
+        @[- Annotate reducer's first parameter (action) with the desired action type\
+      @]\
+    @]"
+    contain_vs_be
+  | _ ->
+    fprintf ppf
+      "%a@,@,\
+      @[This happens when the type system senses there's a mutation/side-effect,@ in combination with a polymorphic value.@,\
+      @{<info>Using or annotating that value usually solves it.@}@ \
+      More info:@ https://realworldocaml.org/v1/en/html/imperative-programming-1.html#side-effects-and-weak-polymorphism@]"
+    print_fallback_msg ()
 
 (* taken from https://github.com/BuckleScript/ocaml/blob/d4144647d1bf9bc7dc3aadc24c25a7efa3a67915/typing/typemod.ml#L1754 *)
 (* modified branches are commented *)
@@ -57,14 +82,14 @@ let report_error ppf = Typemod.(function
   | Non_generalizable typ ->
       (* modified *)
       fprintf ppf "@[<v>";
-      if Super_reason_react.type_is_component_spec typ then begin
-        pp_component_type_not_generalizable_pre ppf
-      end;
-      fprintf ppf
-        "@[This expression's type contains type variables that can't be generalized:@,@{<error>%a@}@]@,@,\
-         %a"
-        type_scheme typ
-        pp_component_type_not_generalizable_post ();
+      non_generalizable_msg
+        ppf
+        ~result:([Super_reason_react.component_spec_weak_type_variables typ])
+        ~is_module:false
+        (fun ppf () ->
+          fprintf ppf
+          "@[This expression's type contains type variables that can't be generalized:@,@{<error>%a@}@]"
+          type_scheme typ);
       fprintf ppf "@]"
   | Non_generalizable_class (id, desc) ->
       fprintf ppf
@@ -74,15 +99,15 @@ let report_error ppf = Typemod.(function
   | Non_generalizable_module mty ->
       (* modified *)
       fprintf ppf "@[<v>";
-      if Super_reason_react.module_type_is_component_spec mty then begin
-        pp_component_type_not_generalizable_pre ppf
-      end;
-      fprintf ppf
-        "@[The type of this module contains type variables that cannot be generalized:@,@{<error>%a@}@]@,@,\
-         %a"
-        modtype mty
-        pp_component_type_not_generalizable_post ();
-      fprintf ppf "@]"
+      non_generalizable_msg
+        ppf
+        ~result:(Super_reason_react.component_spec_weak_type_variables_in_module_type mty)
+        ~is_module:true
+        (fun ppf () ->
+          fprintf ppf
+            "@[The type of this module contains type variables that cannot be generalized:@,@{<error>%a@}@]"
+            modtype mty);
+        fprintf ppf "@]"
   | Implementation_is_required intf_name ->
       fprintf ppf
         "@[The interface %a@ declares values, not just types.@ \
