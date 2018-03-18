899114f2939aa304d4f1dbc5a441fc1a8d8ad9f9
Javier Chávarri <javier.chavarri@gmail.com>
2018-03-18 23:35:55 +0100

    Finish prepublish.sh

--- scripts/prepublish.sh
+++ scripts/prepublish.sh
@@ -6,13 +6,13 @@ BELT_PACKAGE_VERSION=$(node -p -e "require('./$BELT_FOLDER_NAME/package.json').v
 echo "**Preparing and publishing belt-js...**\n"
 
 if [ $MAIN_PACKAGE_VERSION == $BELT_PACKAGE_VERSION ]; then
-  # make && make install && \
+  make && make install && \
   cd jscomp && cd ../belt-js && \
   mkdir -p ./lib/js && cp ../lib/js/* ./lib/js && \
   mkdir -p ./lib/amdjs && cp ../lib/amdjs/* ./lib/amdjs && \
   mkdir -p ./lib/es6 && cp ../lib/es6/* ./lib/es6 && \
   echo "Finished prepublishing $BELT_FOLDER_NAME, version $BELT_PACKAGE_VERSION"
-  # && npm publish
+  && npm publish
 else
   echo "Error: bs-platform and belt-js don't have the same version.\nbs-platform is $MAIN_PACKAGE_VERSION and belt-js is $BELT_PACKAGE_VERSION"
 fi
