5108323b21e140b7c67c34b9787338106073a352
Hongbo Zhang <bobzhang1988@gmail.com>
2018-03-20 11:56:58 +0800

    tweak

--- jscomp/syntax/ast_derive_abstract.ml
+++ jscomp/syntax/ast_derive_abstract.ml
@@ -1,5 +1,5 @@
 (* Copyright (C) 2017 Authors of BuckleScript
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -17,7 +17,7 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *)
@@ -26,110 +26,112 @@
 let derivingName = "abstract"
 module U = Ast_derive_util
 open Ast_helper
-type tdcls = Parsetree.type_declaration list 
+type tdcls = Parsetree.type_declaration list
 
-let handle_config (config : Parsetree.expression option) = 
-  match config with 
-  | Some config -> 
+let handle_config (config : Parsetree.expression option) =
+  match config with
+  | Some config ->
     U.invalid_config config
   | None -> ()
 
-(* see #2337 
+(* see #2337
    TODO: relax it to allow (int -> int [@bs])
-*)  
-let rec checkNotFunciton (ty : Parsetree.core_type) = 
-  match ty.ptyp_desc with 
-  | Ptyp_poly (_,ty) -> checkNotFunciton ty 
-  | Ptyp_alias (ty,_) -> checkNotFunciton ty 
-  | Ptyp_arrow _ -> 
-    Location.raise_errorf 
-      ~loc:ty.ptyp_loc 
+*)
+let rec checkNotFunciton (ty : Parsetree.core_type) =
+  match ty.ptyp_desc with
+  | Ptyp_poly (_,ty) -> checkNotFunciton ty
+  | Ptyp_alias (ty,_) -> checkNotFunciton ty
+  | Ptyp_arrow _ ->
+    Location.raise_errorf
+      ~loc:ty.ptyp_loc
       "syntactic function type is not allowed when working with abstract bs.deriving, create a named type as work around"
-  | Ptyp_any 
+  | Ptyp_any
   | Ptyp_var _
-  | Ptyp_tuple _ 
+  | Ptyp_tuple _
   | Ptyp_constr _
-  | Ptyp_object _  
-  | Ptyp_class _ 
+  | Ptyp_object _
+  | Ptyp_class _
   | Ptyp_variant _
   | Ptyp_package _
-  | Ptyp_extension _ -> () 
-let handleTdcl (tdcl : Parsetree.type_declaration) =   
-  let core_type = U.core_type_of_type_declaration tdcl in 
-  let loc = tdcl.ptype_loc in 
-  let name = tdcl.ptype_name.txt in 
+  | Ptyp_extension _ -> ()
+
+
+let handleTdcl (tdcl : Parsetree.type_declaration) =
+  let core_type = U.core_type_of_type_declaration tdcl in
+  let loc = tdcl.ptype_loc in
+  let type_name = tdcl.ptype_name.txt in
   let newTdcl = {
-    tdcl with 
+    tdcl with
     ptype_kind = Ptype_abstract;
     ptype_attributes = [];
     (* avoid non-terminating*)
-  } in 
-  match tdcl.ptype_kind with 
-  | Ptype_record label_declarations -> 
-    let ty =   
-      Ext_list.fold_right (fun (label_declaration : Parsetree.label_declaration) acc -> 
-          Typ.arrow
-            label_declaration.pld_name.txt label_declaration.pld_type acc 
-        ) label_declarations  core_type in 
-    let setter_accessor =     
-      Ext_list.fold_right (fun (x: Parsetree.label_declaration) acc -> 
-          let pld_name = x.pld_name.txt in 
-          let pld_loc = x.pld_name.loc in 
+  } in
+  match tdcl.ptype_kind with
+  | Ptype_record label_declarations ->
+    let setter_accessor =
+      Ext_list.fold_right (fun (x: Parsetree.label_declaration) acc ->
+          let pld_name = x.pld_name.txt in
+          let pld_loc = x.pld_name.loc in
           let pld_type = x.pld_type in
-          let () = checkNotFunciton pld_type in 
-          let setter = 
-            Val.mk 
+          let () = checkNotFunciton pld_type in
+          let setter =
+            Val.mk
               {loc = pld_loc; txt = pld_name}
               ~attrs:[Ast_attributes.bs_get]
               ~prim:[pld_name]
-              (Typ.arrow "" core_type pld_type) :: acc in 
-          match x.pld_mutable with 
-          | Mutable -> 
-            Val.mk 
+              (Typ.arrow "" core_type pld_type) :: acc in
+          match x.pld_mutable with
+          | Mutable ->
+            Val.mk
               {loc = pld_loc; txt = pld_name ^ "Set"}
               ~attrs:[Ast_attributes.bs_set]
               ~prim:[pld_name]
               (Typ.arrow "" core_type (Typ.arrow "" pld_type (Ast_literal.type_unit ()))) :: setter
-          | Immutable -> setter 
+          | Immutable -> setter
         ) label_declarations []
-    in 
+    in
 
-    newTdcl, 
-    (match tdcl.ptype_private with 
+    newTdcl,
+    (match tdcl.ptype_private with
      | Private -> setter_accessor
-     | Public -> 
-       let maker =   
-         Val.mk  {loc; txt = name}
+     | Public ->
+       let ty =
+         Ext_list.fold_right (fun (label_declaration : Parsetree.label_declaration) acc ->
+             Typ.arrow
+               label_declaration.pld_name.txt label_declaration.pld_type acc
+           ) label_declarations  core_type in
+       let maker =
+         Val.mk  {loc; txt = type_name}
            ~attrs:[Ast_attributes.bs_obj]
-           ~prim:[""] ty in 
+           ~prim:[""] ty in
        (maker :: setter_accessor))
 
-  | Ptype_abstract 
-  | Ptype_variant _ 
-  | Ptype_open -> 
+  | Ptype_abstract
+  | Ptype_variant _
+  | Ptype_open ->
     (* Looks obvious that it does not make sense to warn *)
     (* U.notApplicable tdcl.ptype_loc derivingName;  *)
     tdcl, []
 
-let handleTdclsInStr tdcls =     
-  let tdcls, code = 
-    List.fold_right (fun tdcl (tdcls, sts)  -> 
-        match handleTdcl tdcl with 
-          ntdcl, value_descriptions -> 
-          ntdcl::tdcls, 
+let handleTdclsInStr tdcls =
+  let tdcls, code =
+    List.fold_right (fun tdcl (tdcls, sts)  ->
+        match handleTdcl tdcl with
+          ntdcl, value_descriptions ->
+          ntdcl::tdcls,
           Ext_list.map_append (fun x -> Str.primitive x) value_descriptions sts
 
-      ) tdcls ([],[])  in 
-  Str.type_ tdcls :: code    
+      ) tdcls ([],[])  in
+  Str.type_ tdcls :: code
 (* still need perform transformation for non-abstract type*)
 
-let handleTdclsInSig tdcls =     
-  let tdcls, code = 
-    List.fold_right (fun tdcl (tdcls, sts)  -> 
-        match handleTdcl tdcl with 
-          ntdcl, value_descriptions -> 
-          ntdcl::tdcls, 
+let handleTdclsInSig tdcls =
+  let tdcls, code =
+    List.fold_right (fun tdcl (tdcls, sts)  ->
+        match handleTdcl tdcl with
+          ntdcl, value_descriptions ->
+          ntdcl::tdcls,
           Ext_list.map_append (fun x -> Sig.value x) value_descriptions sts
 
-      ) tdcls ([],[])  in 
-  Sig.type_ tdcls :: code  
+      ) tdcls ([],[])  in
+  Sig.type_ tdcls :: code
