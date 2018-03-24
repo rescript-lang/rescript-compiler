9a56ab45fb348cff58d48e1fd1995ad0fd1f8ad2
Hongbo Zhang <bobzhang1988@gmail.com>
2018-03-24 11:46:35 +0800

    update

--- jscomp/build_tests/serror/input.js
+++ jscomp/build_tests/serror/input.js
@@ -2,4 +2,4 @@ var p = require('child_process')
 var assert = require('assert')
 var o = p.spawnSync(`bsb`, {cwd:__dirname,encoding:'utf8',shell:true})
 
-assert.ok(o.stdout.match(/a bug for you/).length > 0)
+assert.ok(o.stdout.match(/syntax error/).length > 0)
