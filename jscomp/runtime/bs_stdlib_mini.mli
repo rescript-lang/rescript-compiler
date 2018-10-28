e96264ac65d18d073c0c1dcb22c006165fe513fe
Hongbo <bob.hongbo.zhang@foxmail.com>
2018-10-28 22:14:29 +0800

    add docs and build fix

--- jscomp/runtime/bs_stdlib_mini.mli
+++ jscomp/runtime/bs_stdlib_mini.mli
@@ -1,4 +1,21 @@
+(**
+  Since [others] depend on this file, its public mli files should not
+  export types introduced here, otherwise it would cause 
+  conflicts here.
 
+  Types defined here but should not export:
+
+  - ref (make sure not exported in public others/*.mli)
+  - in_channel (runtime only)
+  - fpclass (runtime only)
+  - Obj.t (runtime only)
+  - Lexing.lex_tables, Lexing.lexbuf (runtime only)
+  - Parsing.parse_tables, Parsing.parser_env (runtime only)
+  - Printexc.raw_backtrace_slot,  Printexc.backtrace_slot (runtiem only)
+  - Gc.stat, Gc.control (runtime only) 
+  - CamlinternalOO.obj CamlinternalOO.closure (runtime only)
+  - CamlinternalMod.shape (runtime only)
+*)
 
 external (^) : string -> string -> string = "#string_append"
 external ( = ) : 'a -> 'a -> bool = "%equal"
