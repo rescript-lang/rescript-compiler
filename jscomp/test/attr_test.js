4820ba16ff873163655addfcb6dcdf125a71daaf
Hongbo <bob.hongbo.zhang@foxmail.com>
2018-10-29 10:57:52 +0800

    more tests

--- jscomp/test/attr_test.js
+++ jscomp/test/attr_test.js
@@ -13,8 +13,17 @@ function max2(x, y) {
 
 var hh = max2(1, 2);
 
+function f(x) {
+  des(x, (function () {
+          console.log("hei");
+          return /* () */0;
+        }));
+  return /* () */0;
+}
+
 exports.u = u;
 exports.h = h;
 exports.max2 = max2;
 exports.hh = hh;
+exports.f = f;
 /* h Not a pure module */
