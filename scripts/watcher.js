fed3f0273b0c9024d879b36d1efe4e7383c8f6aa
Hongbo <bob.hongbo.zhang@foxmail.com>
2018-10-30 16:03:07 +0800

    tweak

--- scripts/watcher.js
+++ scripts/watcher.js
@@ -1,82 +1,73 @@
 'use strict';
 
+var Process = require('process');
 var Fs = require('fs');
 var Path = require('path');
-var Process = require('process');
 var Child_process = require('child_process');
 
-function getExn(x) {
-  if (x) {
-    return x[0];
+var undefinedHeader = /* array */[];
+
+function some(x) {
+  if (x === undefined) {
+    var block = /* tuple */[
+      undefinedHeader,
+      0
+    ];
+    block.tag = 256;
+    return block;
+  } else if (x !== null && x[0] === undefinedHeader) {
+    var nid = x[1] + 1 | 0;
+    var block$1 = /* tuple */[
+      undefinedHeader,
+      nid
+    ];
+    block$1.tag = 256;
+    return block$1;
   } else {
-    throw new Error("Bs_option.getExn");
+    return x;
   }
 }
 
+function undefined_to_opt(x) {
+  if (x === undefined) {
+    return undefined;
+  } else {
+    return some(x);
+  }
+}
 
+function valFromOption(x) {
+  if (x !== null && x[0] === undefinedHeader) {
+    var depth = x[1];
+    if (depth === 0) {
+      return undefined;
+    } else {
+      return /* tuple */[
+              undefinedHeader,
+              depth - 1 | 0
+            ];
+    }
+  } else {
+    return x;
+  }
+}
 /* No side effect */
 
-var invalid_argument = /* tuple */[
-  "Invalid_argument",
-  -3
-];
-
-invalid_argument.tag = 248;
-
-
-/*  Not a pure module */
-
+function getExn(x) {
+  if (x !== undefined) {
+    return valFromOption(x);
+  } else {
+    throw new Error("getExn");
+  }
+}
 /* No side effect */
 
-/* No side effect */
-
-/* No side effect */
-
-/* No side effect */
-
-/* stdin Not a pure module */
-
-/* No side effect */
-
-/* imul Not a pure module */
-
-/* repeat Not a pure module */
-
-/* two_ptr_32_dbl Not a pure module */
-
-/* float_of_string Not a pure module */
-
-/* No side effect */
-
-/* No side effect */
-
-/* not_implemented Not a pure module */
-
-/* No side effect */
-
-/* No side effect */
-
-/* No side effect */
-
-function iter$$1(f, xs) {
+function iter(f, xs) {
   for(var i = 0 ,i_finish = xs.length - 1 | 0; i <= i_finish; ++i){
     f(xs[i]);
   }
   return /* () */0;
 }
-
-
-/* No side effect */
-
-function undefined_to_opt(x) {
-  if (x === undefined) {
-    return /* None */0;
-  } else {
-    return /* Some */[x];
-  }
-}
-
-
 /* No side effect */
 
 /* unsafeDeleteKey Not a pure module */
@@ -85,70 +76,67 @@ function putEnvVar(key, $$var) {
   Process.env[key] = $$var;
   return /* () */0;
 }
-
-
 /* Js_dict Not a pure module */
 
 function findFile(_prev, _cwd, f) {
   while(true) {
-    var cwd$$1 = _cwd;
+    var cwd = _cwd;
     var prev = _prev;
-    if (prev.length === cwd$$1.length) {
+    if (prev.length === cwd.length) {
       var str = " " + (String(f) + " not found ");
       throw new Error(str);
-    } else if (Fs.existsSync(Path.join(cwd$$1, f))) {
-      return cwd$$1;
+    } else if (Fs.existsSync(Path.join(cwd, f))) {
+      return cwd;
     } else {
-      _cwd = Path.dirname(cwd$$1);
-      _prev = cwd$$1;
+      _cwd = Path.dirname(cwd);
+      _prev = cwd;
       continue ;
-      
     }
-  }
-}
+  }}
 
-function makeEventObj() {
+function makeEventObj(param) {
   return {
           events: /* array */[],
-          empty: function () {
-            var self = this;
-            var a = self.events;
-            a.splice(0);
-            return /* () */0;
-          },
-          push: function (a) {
-            var self = this;
-            var xs = self.events;
-            xs.push(a);
-            return /* () */0;
-          },
-          needRebuild: function () {
-            var self = this;
-            return +(self.events.length !== 0);
-          },
-          currentEvents: function () {
-            var self = this;
-            return self.events;
-          }
+          empty: (function () {
+              var self = this ;
+              var a = self.events;
+              a.splice(0);
+              return /* () */0;
+            }),
+          push: (function (a) {
+              var self = this ;
+              var xs = self.events;
+              xs.push(a);
+              return /* () */0;
+            }),
+          needRebuild: (function () {
+              var self = this ;
+              return self.events.length !== 0;
+            }),
+          currentEvents: (function () {
+              var self = this ;
+              return self.events;
+            })
         };
 }
 
-function makeLock() {
+function makeLock(param) {
   return {
-          isBuilding: /* false */0,
-          acquire: function () {
-            var self = this;
-            if (self.isBuilding) {
-              return /* false */0;
-            } else {
-              self.isBuilding = /* true */1;
-              return /* true */1;
-            }
-          },
-          release: function () {
-            var self = this;
-            return self.isBuilding = /* false */0;
-          }
+          isBuilding: false,
+          acquire: (function () {
+              var self = this ;
+              if (self.isBuilding) {
+                return false;
+              } else {
+                self.isBuilding = true;
+                return true;
+              }
+            }),
+          release: (function () {
+              var self = this ;
+              self.isBuilding = false;
+              return /* () */0;
+            })
         };
 }
 
@@ -158,15 +146,15 @@ function build(cmd, $$event, lock, idle) {
     var events = $$event.currentEvents();
     console.log("Rebuilding since " + (String(events) + " "));
     $$event.empty();
-    Child_process.spawn(cmd, ( [ ]), ( { "stdio" : "inherit" })).on("exit", function () {
-          console.log(">>>> Finish compiling");
-          lock.release();
-          if ($$event.needRebuild()) {
-            return build(cmd, $$event, lock, idle);
-          } else {
-            return idle();
-          }
-        });
+    Child_process.spawn(cmd, ( [ ]), ( { "stdio" : "inherit" })).on("exit", (function () {
+            console.log(">>>> Finish compiling");
+            lock.release();
+            if ($$event.needRebuild()) {
+              return build(cmd, $$event, lock, idle);
+            } else {
+              return idle();
+            }
+          }));
     return /* () */0;
   } else {
     return 0;
@@ -179,15 +167,15 @@ function buildWithShell(cmd, $$event, lock, idle) {
     var events = $$event.currentEvents();
     console.log("Rebuilding since " + (String(events) + " "));
     $$event.empty();
-    Child_process.spawn(cmd, ( [ ]), ( { "stdio" : "inherit", "shell" : true })).on("exit", function () {
-          console.log(">>>> Finish compiling");
-          lock.release();
-          if ($$event.needRebuild()) {
-            return build(cmd, $$event, lock, idle);
-          } else {
-            return idle();
-          }
-        });
+    Child_process.spawn(cmd, ( [ ]), ( { "stdio" : "inherit", "shell" : true })).on("exit", (function () {
+            console.log(">>>> Finish compiling");
+            lock.release();
+            if ($$event.needRebuild()) {
+              return build(cmd, $$event, lock, idle);
+            } else {
+              return idle();
+            }
+          }));
     return /* () */0;
   } else {
     return 0;
@@ -200,17 +188,15 @@ function makeWatcher(file, onChange) {
           /* watcher */Fs.watch(file, onChange)
         ];
 }
-
-
-/*  Not a pure module */
+/* fs Not a pure module */
 
 getExn(undefined_to_opt(typeof (__dirname) === "undefined" ? undefined : (__dirname)));
 
-var cwd$1 = Process.cwd();
+var cwd = Process.cwd();
 
 var bsconfig = "bsconfig.json";
 
-var root = Fs.existsSync(Path.join(cwd$1, bsconfig)) ? cwd$1 : findFile(cwd$1, Path.dirname(cwd$1), bsconfig);
+var root = Fs.existsSync(Path.join(cwd, bsconfig)) ? cwd : findFile(cwd, Path.dirname(cwd), bsconfig);
 
 console.log(" Root: " + (String(root) + " "));
 
@@ -222,20 +208,20 @@ var events = makeEventObj(/* () */0);
 
 var command = "./watch-build.sh";
 
-function exec() {
-  return buildWithShell(command, events, lock, function () {
-              return /* () */0;
-            });
+function exec(param) {
+  return buildWithShell(command, events, lock, (function () {
+                return /* () */0;
+              }));
 }
 
-function watch$1(dir) {
-  return makeWatcher(dir, function (_, fileName) {
-              if (fileName.endsWith(".ml") || fileName.endsWith(".mli") || fileName.endsWith(".cppo") || fileName.endsWith(".js") || fileName === "Makefile" || fileName === "Makefile.shared" || fileName.endsWith(".mll")) {
-                return exec(/* () */0);
-              } else {
-                return 0;
-              }
-            });
+function watch(dir) {
+  return makeWatcher(dir, (function (_event, fileName) {
+                if (fileName.endsWith(".ml") || fileName.endsWith(".mli") || fileName.endsWith(".cppo") || fileName.endsWith(".js") || fileName === "Makefile" || fileName === "Makefile.shared") {
+                  return exec(/* () */0);
+                } else {
+                  return 0;
+                }
+              }));
 }
 
 putEnvVar("BS_VSCODE", "1");
@@ -256,10 +242,10 @@ if (match.length !== 1) {
 }
 
 if (exit === 1) {
-  iter$$1(function (x) {
-        watch$1(Path.join(jscomp, x));
-        return /* () */0;
-      }, /* array */[
+  iter((function (x) {
+          watch(Path.join(jscomp, x));
+          return /* () */0;
+        }), /* array */[
         "core",
         "syntax",
         "ext",
@@ -273,12 +259,9 @@ if (exit === 1) {
         "bsb",
         "common",
         "super_errors",
-        "outcome_printer",
         ".",
-
+        "stdlib-406"
       ]);
   exec(/* () */0);
 }
-
-
 /*  Not a pure module */
