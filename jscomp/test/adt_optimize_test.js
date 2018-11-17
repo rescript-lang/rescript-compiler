7249048f324fc7ebc2d0e89555b15785a884a70a
Hongbo <bob.hongbo.zhang@foxmail.com>
2018-11-17 20:07:33 +0800

    Add a util `g` and snapshot previous changes

--- jscomp/test/adt_optimize_test.js
+++ jscomp/test/adt_optimize_test.js
@@ -160,7 +160,7 @@ function f11(x) {
           Caml_builtin_exceptions.assert_failure,
           /* tuple */[
             "adt_optimize_test.ml",
-            155,
+            191,
             9
           ]
         ];
