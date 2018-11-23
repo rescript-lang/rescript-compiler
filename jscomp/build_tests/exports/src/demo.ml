9331c41a4d02030a196f494eda6b5b7beab11955
Hongbo <bob.hongbo.zhang@foxmail.com>
2018-11-23 19:28:44 +0800

    Add a test to verify caml_exceptions is needed

--- jscomp/build_tests/exports/src/demo.ml
+++ jscomp/build_tests/exports/src/demo.ml
@@ -3,3 +3,11 @@
 let _ = 
   Js.Int.toString,
   Js.Float.isNaN
+
+
+(* make sure exception runtime is there *)
+let f = function [@bs.open]
+  | Not_found -> 0 
+  | ( Invalid_argument _
+  | Stack_overflow ) -> 1 
+  | Sys_error _ -> 2 
