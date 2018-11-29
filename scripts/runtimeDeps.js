065d30ef4961fcc2ebe0b3c982fc6129c8c8be5b
Hongbo <bob.hongbo.zhang@foxmail.com>
2018-11-29 23:01:15 +0800

    more rigorous dependency tracking

--- scripts/runtimeDeps.js
+++ scripts/runtimeDeps.js
@@ -3,17 +3,51 @@
 
 var fs = require('fs')
 var path = require('path')
+var cp  = require('child_process')
 
 var runtimeDir = path.join(__dirname, '..', 'jscomp', 'runtime')
 var jsDir = path.join(__dirname, '..', 'lib', 'js')
 var files = fs.readdirSync(runtimeDir, 'utf8')
 
-var possibleJsFiles = [... 
-    new Set(files.filter(x => !x.startsWith("bs_stdlib_mini") &&
-        (x.endsWith('.ml') || x.endsWith('.mli'))).map(x => x.substr(0, x.indexOf('.'))))
-]
+var mlFiles = files.filter(x=> !x.startsWith("bs_stdlib_mini") && x.endsWith('.ml') && x!=="js.ml")
+var mliFiles = files.filter(x=> !x.startsWith("bs_stdlib_mini") && x.endsWith('.mli') && x!=="js.mli")
 
-// possibleJsFiles.map(x=>path.join(jsDir,x + ".js")).every(x=>fs.existsSync(x))
+var depsMap = new Map()
+
+function updateMapSingle(key,value){
+    if(depsMap.has(key)){
+        depsMap.get(key).add(value)
+    } else {
+        depsMap.set(key, new Set([value]))
+    }
+}
+function updateMapMany(key, arr){
+    if (depsMap.has(key)){
+        var s = depsMap.get(key)
+        for(var i = 0; i < arr.length; ++i){
+            s.add(arr[i])
+        }
+        
+    } else {
+        depsMap.set(key, new Set(arr))
+    }
+}
+
+function replaceCmj(x){
+    return x.trim().replace('cmx','cmj')
+}
+
+function runOCamlDep() {
+    var pairs = cp.execSync(`ocamldep.opt -one-line -native ${mlFiles.join(' ')} ${mliFiles.join(' ')}`, { cwd: runtimeDir, encoding: 'utf8' }).split('\n').map(x => x.split(':'))
+
+    pairs.forEach(x => {
+        var deps;
+        if (x[1] !== undefined && (deps = x[1].trim())) {
+            deps = deps.split(' ');
+            updateMapMany(replaceCmj(x[0]), deps.map(x => replaceCmj(x)))
+        }
+    })
+}
 
 function getDeps(text) {
     var deps = []
@@ -23,28 +57,60 @@ function getDeps(text) {
     return deps;
 }
 
-
+function baseName(x){
+    return x.substr(0,x.indexOf('.'))
+}
 function readDeps(name) {
     var jsFile = path.join(jsDir, name + ".js")
     var fileContent = fs.readFileSync(jsFile, 'utf8')
     return getDeps(fileContent).map(x => path.parse(x).name)
 }
 
-function create() {
-    var deps = possibleJsFiles.filter((x) => {
-        return readDeps(x).length !== 0
-    })
-    .map(x => {
-       var deps = readDeps(x).map(x => x + '.cmj')
-       if(fs.existsSync(path.join(runtimeDir, x+".mli"))){
-           deps.push(x + ".cmi")
-       }
-       return  `${x}.cmj: ${deps.join(' ')}`
-    })
-    .reduce((x, y) => x + '\n' + y)
-    fs.writeFileSync(path.join(__dirname, '..', 'jscomp', 'runtime', '.extradepend'), deps + '\n', 'utf8')
+function runJSCheck() {
+    var possibleJsFiles = [...
+        new Set(
+            Array.prototype.concat(mlFiles, mliFiles)
+                .map(baseName)
+        )
+    ]
+    possibleJsFiles
+        .filter((x) => {
+            return readDeps(x).length !== 0
+        })
+        .forEach(x => {
+            var deps = readDeps(x).map(x => x + '.cmj')
+            if (fs.existsSync(path.join(runtimeDir, x + ".mli"))) {
+                deps.push(x + ".cmi")
+            }
+            updateMapMany(`${x}.cmj`, deps)
+        })
 }
 
+function toDeps(){
+    var output = []
+    for(var [key,set] of depsMap){
+        var deps = [...set]
+        if (deps.length) {
+            output.push(`${key} : ${deps.join(' ')}`)
+        }
+    }
+    return output.join('\n')
+}
+
+function create(){    
+    mliFiles.forEach(x=>{
+        updateMapMany(baseName(x)+".cmi",["js.cmi","bs_stdlib_mini.cmi"])
+    })
+    mlFiles.forEach(x=>{
+        updateMapMany(baseName(x)+".cmj",["js.cmj","js.cmi","bs_stdlib_mini.cmi"])
+    })
+    updateMapSingle("js.cmj", "js.cmi")
+    updateMapSingle("js.cmi","bs_stdlib_mini.cmi")
+    runJSCheck()    
+    runOCamlDep()    
+    var output = toDeps()
+    fs.writeFileSync(path.join(runtimeDir,'.depend'),output,'utf8')
+}
 
 if (require.main === module){
     create()
