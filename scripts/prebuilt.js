8a0a7f34fb7782a7673a11f2e64c8a3bbc141312
Hongbo Zhang <bobzhang1988@gmail.com>
2018-12-04 13:52:43 +0000

    not generating .depend under windows

--- scripts/prebuilt.js
+++ scripts/prebuilt.js
@@ -88,7 +88,10 @@ function buildCompiler() {
 		}
 	})
 }
+if(os.platform()=== 'darwin'){
+	// only run it in my dev machine
+	require('./runtimeDeps.js').create()
+}
 
-require('./runtimeDeps.js').create()
 buildCompiler()
 
