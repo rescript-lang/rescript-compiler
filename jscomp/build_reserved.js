f94ab0e85b764b4c8a656c3e1c5bcece4b0b1093
Hongbo <bob.hongbo.zhang@foxmail.com>
2019-01-14 12:41:41 +0800

    add reserved keywords
    Note that we are not using js for post-processing since we want the string comparison to be consistent

---
+++ jscomp/build_reserved.js
@@ -0,0 +1,14 @@
+//@ts-check
+
+var fs = require('fs');
+var puppeteer = require('puppeteer');
+(async function(){
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    /**
+     * @type string[]
+     */
+    const result = await page.evaluate(`Object.getOwnPropertyNames(window)`)
+    fs.writeFileSync('keywords.list',result.join('\n'),'utf8')
+    await browser.close()
+}())
