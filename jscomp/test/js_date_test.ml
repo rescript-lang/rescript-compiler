a907636ddb66dba33aee25d70367cdd4e258384a
Digitake <digitake@gmail.com>
2019-01-16 12:26:43 +0700

    Fix test on  toLocaleDateStringWithLocale

--- jscomp/test/js_date_test.ml
+++ jscomp/test/js_date_test.ml
@@ -440,7 +440,7 @@ let suites = Mt.[
     "toJSONUnsafe", (fun _ ->
       Eq("1976-03-08T11:11:56.789Z", Js.Date.toJSONUnsafe (date ())));
     "toLocaleDateStringWithLocale", (fun _ ->
-      Eq("Montag, 8. März 1976", Js.Date.toLocaleDateStringWithLocale (date ()) ~locale:"de-DE"));
+      Eq("Montag, 8. März 1976", Js.Date.toLocaleDateStringWithLocale (date ()) ~options:None ~locale:"de-DE"));
     (* locale dependent
     "toLocaleString", (fun _ ->
       Eq("3/8/1976, 12:11:56 PM", Js.Date.toLocaleString (date ())));
