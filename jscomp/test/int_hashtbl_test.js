279139011e690f3aad3bafdda698bdc107e3982d
Hongbo <bob.hongbo.zhang@foxmail.com>
2019-03-27 17:54:44 +0800

    update js files

--- jscomp/test/int_hashtbl_test.js
+++ jscomp/test/int_hashtbl_test.js
@@ -14,7 +14,7 @@ function f(H) {
   Curry._3(H[/* add */4], tbl, 2, /* "2" */50);
   return List.sort((function (param, param$1) {
                 return Caml_primitive.caml_int_compare(param[0], param$1[0]);
-              }), Curry._3(H[/* fold */11], (function (k, v, acc) {
+              }), Curry._3(H[/* fold */13], (function (k, v, acc) {
                     return /* :: */[
                             /* tuple */[
                               k,
@@ -28,12 +28,12 @@ function f(H) {
 function g(H, count) {
   var tbl = Curry._1(H[/* create */0], 17);
   for(var i = 0; i <= count; ++i){
-    Curry._3(H[/* replace */8], tbl, (i << 1), String(i));
+    Curry._3(H[/* replace */9], tbl, (i << 1), String(i));
   }
   for(var i$1 = 0; i$1 <= count; ++i$1){
-    Curry._3(H[/* replace */8], tbl, (i$1 << 1), String(i$1));
+    Curry._3(H[/* replace */9], tbl, (i$1 << 1), String(i$1));
   }
-  var v = Curry._3(H[/* fold */11], (function (k, v, acc) {
+  var v = Curry._3(H[/* fold */13], (function (k, v, acc) {
           return /* :: */[
                   /* tuple */[
                     k,
