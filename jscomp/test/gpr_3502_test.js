ad9a99e6fbb4574c6a0c0a6128bf348afc48353f
Hongbo <bob.hongbo.zhang@foxmail.com>
2019-04-16 09:48:06 +0800

    fix #3502

---
+++ jscomp/test/gpr_3502_test.js
@@ -0,0 +1,27 @@
+'use strict';
+
+
+function toString(param) {
+  return "a";
+}
+
+function name(param) {
+  return 2;
+}
+
+var Language = /* module */[
+  /* toString */toString,
+  /* name */name
+];
+
+var language = "a";
+
+var shortName = "a";
+
+var name$1 = 2;
+
+exports.Language = Language;
+exports.language = language;
+exports.shortName = shortName;
+exports.name = name$1;
+/* No side effect */
