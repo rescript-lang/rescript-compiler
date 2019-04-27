db62dfa50be4b96b0a7e664a227bd2636479ba32
Hongbo <bob.hongbo.zhang@foxmail.com>
2019-04-27 11:08:30 +0800

    tentative fix #3536

---
+++ jscomp/test/gpr_3536_test.js
@@ -0,0 +1,13 @@
+'use strict';
+
+
+var X = /* module */[];
+
+var v = 5;
+
+var u = /* Some */[3];
+
+exports.v = v;
+exports.X = X;
+exports.u = u;
+/* No side effect */
---
+++ jscomp/test/gpr_3536_test.ml
@@ -0,0 +1,9 @@
+
+let v = 
+  2 |. (3 |. (+))
+
+module X = struct  
+  type t = Some of int 
+end 
+
+let u =  3 |. X.Some 
