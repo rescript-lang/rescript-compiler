687d28cda96d34c1fb2238ac1ad400ae9906b4a8
Hongbo <bob.hongbo.zhang@foxmail.com>
2019-05-27 20:29:57 +0800

    add an edge case test

--- jscomp/test/splice_test.js
+++ jscomp/test/splice_test.js
@@ -12,6 +12,8 @@ function eq(loc, x, y) {
   return Mt.eq_suites(test_id, suites, loc, x, y);
 }
 
+var Caml_splice_call$1 = /* module */[];
+
 Math.max(1);
 
 function f00(a, b) {
@@ -22,7 +24,7 @@ var a = /* array */[];
 
 a.push(1, 2, 3, 4);
 
-eq("File \"splice_test.ml\", line 28, characters 5-12", a, /* array */[
+eq("File \"splice_test.ml\", line 29, characters 5-12", a, /* array */[
       1,
       2,
       3,
@@ -35,7 +37,7 @@ function dynamic(arr) {
         1,
         arr
       ]);
-  return eq("File \"splice_test.ml\", line 33, characters 5-12", a, Caml_array.caml_array_concat(/* :: */[
+  return eq("File \"splice_test.ml\", line 34, characters 5-12", a, Caml_array.caml_array_concat(/* :: */[
                   /* array */[1],
                   /* :: */[
                     arr,
@@ -62,7 +64,7 @@ var a$1 = /* array */[];
 
 a$1.push(1, 2, 3, 4);
 
-eq("File \"splice_test.ml\", line 50, characters 7-14", a$1, /* array */[
+eq("File \"splice_test.ml\", line 51, characters 7-14", a$1, /* array */[
       1,
       2,
       3,
@@ -75,7 +77,7 @@ function dynamic$1(arr) {
         1,
         arr
       ]);
-  return eq("File \"splice_test.ml\", line 55, characters 7-14", a, Caml_array.caml_array_concat(/* :: */[
+  return eq("File \"splice_test.ml\", line 56, characters 7-14", a, Caml_array.caml_array_concat(/* :: */[
                   /* array */[1],
                   /* :: */[
                     arr,
@@ -107,17 +109,18 @@ function f1(c) {
             ]);
 }
 
-eq("File \"splice_test.ml\", line 66, characters 6-13", Math.max(1, 2, 3), 3);
+eq("File \"splice_test.ml\", line 67, characters 6-13", Math.max(1, 2, 3), 3);
 
-eq("File \"splice_test.ml\", line 67, characters 6-13", Math.max(1), 1);
+eq("File \"splice_test.ml\", line 68, characters 6-13", Math.max(1), 1);
 
-eq("File \"splice_test.ml\", line 68, characters 6-13", Math.max(1, 1, 2, 3, 4, 5, 2, 3), 5);
+eq("File \"splice_test.ml\", line 69, characters 6-13", Math.max(1, 1, 2, 3, 4, 5, 2, 3), 5);
 
 Mt.from_pair_suites("splice_test.ml", suites[0]);
 
 exports.suites = suites;
 exports.test_id = test_id;
 exports.eq = eq;
+exports.Caml_splice_call = Caml_splice_call$1;
 exports.f00 = f00;
 exports.dynamic = dynamic;
 exports.Pipe = Pipe;
