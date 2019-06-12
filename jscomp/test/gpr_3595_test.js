85875f90b3a95441404c1392fe0d50a2d06eb967
Hongbo <bob.hongbo.zhang@foxmail.com>
2019-06-12 18:40:30 +0800

    Add a test case

---
+++ jscomp/test/gpr_3595_test.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var Mt = require("./mt.js");
+var Caml_builtin_exceptions = require("../../lib/js/caml_builtin_exceptions.js");
+
+var suites = /* record */[/* contents : [] */0];
+
+var test_id = /* record */[/* contents */0];
+
+function eq(loc, x, y) {
+  return Mt.eq_suites(test_id, suites, loc, x, y);
+}
+
+var match = /* array */[1];
+
+if (match.length !== 1) {
+  throw [
+        Caml_builtin_exceptions.match_failure,
+        /* tuple */[
+          "gpr_3595_test.ml",
+          9,
+          4
+        ]
+      ];
+} else {
+  var a = match[0];
+}
+
+exports.suites = suites;
+exports.test_id = test_id;
+exports.eq = eq;
+exports.a = a;
+exports.x = x;
+/*  Not a pure module */
---
+++ jscomp/test/gpr_3595_test.ml
@@ -0,0 +1,16 @@
+let suites :  Mt.pair_suites ref  = ref []
+let test_id = ref 0
+let eq loc x y = Mt.eq_suites ~test_id ~suites loc x y 
+
+
+[@@@ocaml.warning "-a"]
+
+
+let [|a|] = [|1|]
+let x = 1
+
+(* let () = 
+  eq __LOC__ a 1 
+
+
+let () = Mt.from_pair_suites __FILE__ !suites   *)
