31a250f399bdcc3a1738291db24e52a257e7e711
Hongbo <bob.hongbo.zhang@foxmail.com>
2019-07-07 15:18:32 +0800

    Add snapshot js file for testing

---
+++ jscomp/test/gpr_3519_jsx_test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+var React = /* module */[/* null : () */0];
+
+function make(param, param$1) {
+  return /* () */0;
+}
+
+var Foo = /* module */[/* make */make];
+
+exports.React = React;
+exports.Foo = Foo;
+/* No side effect */
