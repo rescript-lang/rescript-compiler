79fff918e2f0f93ab6f3a593228fc0cfd93f5402
Javier Chávarri <javier.chavarri@gmail.com>
2019-07-10 21:30:43 +0200

    Use loc_ghost instead

--- jscomp/syntax/reactjs_jsx_ppx.cppo.ml
+++ jscomp/syntax/reactjs_jsx_ppx.cppo.ml
@@ -789,8 +789,8 @@ let jsxMapper () =
           let expression = match (default) with
           | (Some default) -> Exp.match_ expression [
             Exp.case
-              (Pat.construct {loc; txt=Lident "Some"} (Some (Pat.var ~loc {txt = "$value"; loc})))
-              (Exp.ident ~loc {txt = (Lident "$value"); loc});
+              (Pat.construct {loc; txt=Lident "Some"} (Some (Pat.var ~loc {txt = labelString; loc})))
+              (Exp.ident ~loc {txt = (Lident labelString); loc = { loc with Location.loc_ghost = true }});
             Exp.case
               (Pat.construct {loc; txt=Lident "None"} None)
               default
