d92c2ed0306d7d0af74963becc77b0be28c10f75
Daniel Poul Purkhús <daniel@gangverk.is>
2019-07-27 11:12:12 +0000

    Debug ci build

--- jscomp/build_tests/bs_dependencies_node_path_override/testcase.js
+++ jscomp/build_tests/bs_dependencies_node_path_override/testcase.js
@@ -2,6 +2,6 @@
 var assert = require('assert')
 var path = require('path')
 var p = require('child_process')
-p.execSync(`bsb -make-world`, {cwd:__dirname,shell:true,encoding:'utf8',stdio:"inherit"})
+p.execSync(`bsb -version; bsb -make-world`, { cwd: __dirname, shell: true, encoding: 'utf8', stdio: "inherit" })
 var u = require("./examples/test.js")
-assert.equal(path.basename(u.v),'demo.mldemo.ml')
+assert.equal(path.basename(u.v), 'demo.mldemo.ml')
