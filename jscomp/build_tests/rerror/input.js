a6f0f7abb4143c86b82aafaee92d30017827f745
Hongbo <bob.hongbo.zhang@foxmail.com>
2019-08-08 15:56:01 +0800

    more checks on reason error
    Ensure the filename is the reason file name, the error message is in reason syntax

--- jscomp/build_tests/rerror/input.js
+++ jscomp/build_tests/rerror/input.js
@@ -5,4 +5,8 @@ var o = child_process.spawnSync(`bsb `,{cwd:__dirname, encoding:'utf8',shell:tru
 
 // verify the output is in reason syntax
 var u = o.stdout.match(/=>/g)
+
+var lines = o.stdout.split('\n').map(x=>x.trim()).filter(Boolean)
+assert.ok(/rerror\/src\/demo.re 3:23-25/.test(lines[4]))
+assert.equal(lines[3],"We've found a bug for you!")
 assert.ok(u.length === 2)
