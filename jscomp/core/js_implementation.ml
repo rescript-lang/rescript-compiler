0b7599d2aece08eb2c51a8a64a783d9101433926
Hongbo <bob.hongbo.zhang@foxmail.com>
2019-08-14 20:07:13 +0800

    tweak

--- jscomp/core/js_implementation.ml
+++ jscomp/core/js_implementation.ml
@@ -79,18 +79,12 @@ let after_parsing_sig ppf  outputprefix ast  =
       end
     end
 
-let parse_interface ppf sourcefile = 
-  Ppx_entry.rewrite_signature (Pparse.parse_interface ~tool_name:Js_config.tool_name ppf sourcefile)
 
-let parse_implementation ppf sourcefile = 
-  Ppx_entry.rewrite_implementation
-    (
-      Pparse.parse_implementation ~tool_name:Js_config.tool_name ppf sourcefile
-      )
 
-let interface ppf sourcefile outputprefix =
+let interface ppf fname outputprefix =
   Compmisc.init_path false;
-  parse_interface ppf sourcefile
+  Pparse.parse_interface ~tool_name:Js_config.tool_name ppf fname
+  |> Ppx_entry.rewrite_signature
   |> print_if ppf Clflags.dump_parsetree Printast.interface
   |> print_if ppf Clflags.dump_source Pprintast.signature 
   |> after_parsing_sig ppf  outputprefix 
@@ -152,7 +146,8 @@ let after_parsing_impl ppf  outputprefix ast =
     end
 let implementation ppf fname outputprefix =
   Compmisc.init_path false;
-  parse_implementation ppf fname
+  Pparse.parse_implementation ~tool_name:Js_config.tool_name ppf fname
+  |> Ppx_entry.rewrite_implementation
   |> print_if ppf Clflags.dump_parsetree Printast.implementation
   |> print_if ppf Clflags.dump_source Pprintast.structure
   |> after_parsing_impl ppf outputprefix 
