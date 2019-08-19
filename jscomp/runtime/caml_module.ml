e737e1dfe1e638f866c9dd543b4c8ff4b54f0be0
Cristiano Calcagno <ccrisccris@gmail.com>
2019-08-19 18:39:24 +0200

    Simplistic implementation of runtime for recursive modules: emply object, and Object.assign.

--- jscomp/runtime/caml_module.ml
+++ jscomp/runtime/caml_module.ml
@@ -89,3 +89,14 @@ let update_mod (shape : shape)  (o : Caml_obj_extern.t)  (n : Caml_obj_extern.t)
       aux comps.(i) (Caml_obj_extern.field o i) (Caml_obj_extern.field n i) o  i
     done
   |  _ -> assert false 
+
+
+let init_mod (loc : string * int * int) (shape : shape) =
+  ([%raw {|{}|}]: Caml_obj_extern.t)
+
+let module_assign : Caml_obj_extern.t -> Caml_obj_extern.t -> unit [@bs]  = fun%raw o n ->  {|
+Object.assign(o, n);
+|}
+
+let update_mod (shape : shape)  (o : Caml_obj_extern.t)  (n : Caml_obj_extern.t) :  unit =
+  module_assign o n [@bs]
