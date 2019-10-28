364f30565162c06d37a9ccf7915ff81c19c1e947
Hongbo <bob.hongbo.zhang@foxmail.com>
2019-10-28 12:48:55 +0800

    Add with tests for inline records

--- jscomp/test/record_extension_test.js
+++ jscomp/test/record_extension_test.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var Block = require("../../lib/js/block.js");
 var Caml_format = require("../../lib/js/caml_format.js");
 var Caml_exceptions = require("../../lib/js/caml_exceptions.js");
 
@@ -12,15 +13,34 @@ function f(x) {
   
 }
 
+var v0 = [
+  Inline_record,
+  /* x */3,
+  /* y */"4"
+];
+
 function f2(x) {
-  if (x) {
-    return x[/* x */0];
-  } else {
+  if (typeof x === "number" || x.tag) {
     return 0;
+  } else {
+    return x[/* x */0];
+  }
+}
+
+function f2_with(x) {
+  if (typeof x === "number" || x.tag) {
+    return x;
+  } else {
+    return /* C */Block.__(0, [
+              /* x */0,
+              /* y */x[/* y */1]
+            ]);
   }
 }
 
 exports.Inline_record = Inline_record;
 exports.f = f;
+exports.v0 = v0;
 exports.f2 = f2;
+exports.f2_with = f2_with;
 /* No side effect */
--- jscomp/test/record_extension_test.ml
+++ jscomp/test/record_extension_test.ml
@@ -9,7 +9,7 @@ let f x =
   match x with 
   | Inline_record {x; y} -> Some (x + int_of_string y)  
   | _ -> None 
-
+let v0 = Inline_record {x = 3; y = "4"}
 (* Record_unboxed *)
 type t1 = | A of { x : int } [@ocaml.unboxed]
 
@@ -17,11 +17,18 @@ type t1 = | A of { x : int } [@ocaml.unboxed]
 (* Record_inlined *)
 type t2 = 
   | B 
-  | C of  {x : int }
-
+  | C of  {x : int ;y : string }
+  | D of { w : int}
 let f2 x = 
   match x with 
+  | D _
   | B -> 0
+
   | C {x } -> x 
 
+let f2_with x = 
+  match x with 
+  | D _
+  | B -> x 
+  | C u -> C {u with x = 0}
 #end
