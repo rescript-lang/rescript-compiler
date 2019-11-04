731fe3966bb0902dae57a83a43aa5fcfb5232e12
Hongbo <bob.hongbo.zhang@foxmail.com>
2019-11-04 16:43:14 +0800

    add test cases when compiler can not do the inlining

--- jscomp/test/recursive_records_test.ml
+++ jscomp/test/recursive_records_test.ml
@@ -15,11 +15,19 @@ let rec rec_cell = {
     next = rec_cell
   } (* over records *)
 
+let f0 x = 
+  let rec rec_cell = 
+  {
+    content = x * x - 6;
+    next = rec_cell
+  } in rec_cell 
+
 let a0 x = 
   x.content + x.next.content + x.next.next.content
 
 let () = 
-  eq __LOC__ (a0 rec_cell) 9   
+  eq __LOC__ (a0 rec_cell) 9 ;  
+  eq __LOC__ (a0 (f0 3) ) 9
 
 type  cell2 =
   | Nil  
@@ -29,6 +37,11 @@ type  cell2 =
 let rec rec_cell2 = 
     Cons {content = 3; next = rec_cell2 }
     (* over inline records *)
+let f2 x =     
+  let rec rec_cell2 = 
+    Cons {content = x * x - 6; next = rec_cell2 }
+  in rec_cell2 
+
 let hd x = 
   match x with 
   | Nil -> 0 
@@ -42,14 +55,27 @@ let tl_exn x =
 let () = 
    eq __LOC__ 
     (hd rec_cell2 + hd (tl_exn rec_cell2) + hd( tl_exn (tl_exn (rec_cell2))))   
+    9;
+  let rec_cell2 = f2 3 in 
+  eq __LOC__ 
+    (hd rec_cell2 + hd (tl_exn rec_cell2) + hd( tl_exn (tl_exn (rec_cell2))))   
     9
 
 let rec rec_cell3 = 
   3::rec_cell3 (* over variant *)
 
+let f3 x =   
+  let rec rec_cell3 = 
+  (x * x - 6)::rec_cell3 (* over variant *)
+  in rec_cell3 
+
+
 let () = 
   eq __LOC__ 
-  List.(hd rec_cell3 + hd(tl rec_cell3) + hd (tl (tl rec_cell3))) 9
+    List.(hd rec_cell3 + hd(tl rec_cell3) + hd (tl (tl rec_cell3))) 9;
+  let rec_cell3 = f3 3 in 
+  eq __LOC__ 
+    List.(hd rec_cell3 + hd(tl rec_cell3) + hd (tl (tl rec_cell3))) 9
 
 let () = 
   Mt.from_pair_suites __FILE__ !suites
