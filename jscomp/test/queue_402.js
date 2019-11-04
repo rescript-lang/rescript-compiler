bc194811731e5784e0995843cb0a081e0582b30f
Hongbo <bob.hongbo.zhang@foxmail.com>
2019-11-04 10:38:14 +0800

    tests for queue 402 using recursive let and 406 inline records

---
+++ jscomp/test/queue_402.js
@@ -0,0 +1,195 @@
+'use strict';
+
+var Curry = require("../../lib/js/curry.js");
+var Caml_obj = require("../../lib/js/caml_obj.js");
+var Caml_exceptions = require("../../lib/js/caml_exceptions.js");
+
+var Empty = Caml_exceptions.create("Queue_402.Empty");
+
+function create(param) {
+  return /* record */[
+          /* length */0,
+          /* tail */undefined
+        ];
+}
+
+function clear(q) {
+  q[/* length */0] = 0;
+  q[/* tail */1] = undefined;
+  return /* () */0;
+}
+
+function add(x, q) {
+  if (q[/* length */0] === 0) {
+    var cell = [];
+    cell[0] = x;
+    cell[1] = cell;
+    q[/* length */0] = 1;
+    q[/* tail */1] = cell;
+    return /* () */0;
+  } else {
+    var tail = q[/* tail */1];
+    var head = tail[/* next */1];
+    var cell$1 = /* record */[
+      /* content */x,
+      /* next */head
+    ];
+    q[/* length */0] = q[/* length */0] + 1 | 0;
+    tail[/* next */1] = cell$1;
+    q[/* tail */1] = cell$1;
+    return /* () */0;
+  }
+}
+
+function peek(q) {
+  if (q[/* length */0] === 0) {
+    throw Empty;
+  }
+  return q[/* tail */1][/* next */1][/* content */0];
+}
+
+function take(q) {
+  if (q[/* length */0] === 0) {
+    throw Empty;
+  }
+  q[/* length */0] = q[/* length */0] - 1 | 0;
+  var tail = q[/* tail */1];
+  var head = tail[/* next */1];
+  if (head === tail) {
+    q[/* tail */1] = undefined;
+  } else {
+    tail[/* next */1] = head[/* next */1];
+  }
+  return head[/* content */0];
+}
+
+function copy(q) {
+  if (q[/* length */0] === 0) {
+    return /* record */[
+            /* length */0,
+            /* tail */undefined
+          ];
+  } else {
+    var tail = q[/* tail */1];
+    var tail$prime = [];
+    Caml_obj.caml_update_dummy(tail$prime, /* record */[
+          /* content */tail[/* content */0],
+          /* next */tail$prime
+        ]);
+    var copy$1 = function (_prev, _cell) {
+      while(true) {
+        var cell = _cell;
+        var prev = _prev;
+        if (cell !== tail) {
+          var res = /* record */[
+            /* content */cell[/* content */0],
+            /* next */tail$prime
+          ];
+          prev[/* next */1] = res;
+          _cell = cell[/* next */1];
+          _prev = res;
+          continue ;
+        } else {
+          return 0;
+        }
+      };
+    };
+    copy$1(tail$prime, tail[/* next */1]);
+    return /* record */[
+            /* length */q[/* length */0],
+            /* tail */tail$prime
+          ];
+  }
+}
+
+function is_empty(q) {
+  return q[/* length */0] === 0;
+}
+
+function length(q) {
+  return q[/* length */0];
+}
+
+function iter(f, q) {
+  if (q[/* length */0] > 0) {
+    var tail = q[/* tail */1];
+    var _cell = tail[/* next */1];
+    while(true) {
+      var cell = _cell;
+      Curry._1(f, cell[/* content */0]);
+      if (cell !== tail) {
+        _cell = cell[/* next */1];
+        continue ;
+      } else {
+        return 0;
+      }
+    };
+  } else {
+    return 0;
+  }
+}
+
+function fold(f, accu, q) {
+  if (q[/* length */0] === 0) {
+    return accu;
+  } else {
+    var tail = q[/* tail */1];
+    var _accu = accu;
+    var _cell = tail[/* next */1];
+    while(true) {
+      var cell = _cell;
+      var accu$1 = _accu;
+      var accu$2 = Curry._2(f, accu$1, cell[/* content */0]);
+      if (cell === tail) {
+        return accu$2;
+      } else {
+        _cell = cell[/* next */1];
+        _accu = accu$2;
+        continue ;
+      }
+    };
+  }
+}
+
+function transfer(q1, q2) {
+  var length1 = q1[/* length */0];
+  if (length1 > 0) {
+    var tail1 = q1[/* tail */1];
+    clear(q1);
+    if (q2[/* length */0] > 0) {
+      var tail2 = q2[/* tail */1];
+      var head1 = tail1[/* next */1];
+      var head2 = tail2[/* next */1];
+      tail1[/* next */1] = head2;
+      tail2[/* next */1] = head1;
+    }
+    q2[/* length */0] = q2[/* length */0] + length1 | 0;
+    q2[/* tail */1] = tail1;
+    return /* () */0;
+  } else {
+    return 0;
+  }
+}
+
+var push = add;
+
+var top = peek;
+
+var pop = take;
+
+exports.Empty = Empty;
+exports.create = create;
+exports.clear = clear;
+exports.add = add;
+exports.push = push;
+exports.peek = peek;
+exports.top = top;
+exports.take = take;
+exports.pop = pop;
+exports.copy = copy;
+exports.is_empty = is_empty;
+exports.length = length;
+exports.iter = iter;
+exports.fold = fold;
+exports.transfer = transfer;
+/* No side effect */
---
+++ jscomp/test/recursive_records_test.ml
@@ -0,0 +1,57 @@
+
+let suites :  Mt.pair_suites ref  = ref []
+let test_id = ref 0
+let eq loc x y = Mt.eq_suites ~test_id ~suites loc x y 
+
+type 'a cell = {
+  content: 'a;
+  mutable next: 'a cell
+}
+
+
+
+let rec rec_cell = {
+    content = 3;
+    next = rec_cell
+  } (* over records *)
+
+let a0 x = 
+  x.content + x.next.content + x.next.next.content
+
+let () = 
+  eq __LOC__ (a0 rec_cell) 9   
+
+type  cell2 =
+  | Nil  
+  | Cons of { content: int; mutable next: cell2 }
+
+
+let rec rec_cell2 = 
+    Cons {content = 3; next = rec_cell2 }
+    (* over inline records *)
+let hd x = 
+  match x with 
+  | Nil -> 0 
+  | Cons x -> x.content 
+
+let tl_exn x =   
+  match x with 
+  | Nil -> assert false 
+  | Cons x -> x.next 
+
+let () = 
+   eq __LOC__ 
+    (hd rec_cell2 + hd (tl_exn rec_cell2) + hd( tl_exn (tl_exn (rec_cell2))))   
+    9
+
+let rec rec_cell3 = 
+  3::rec_cell3 (* over variant *)
+
+let () = 
+  eq __LOC__ 
+  List.(hd rec_cell3 + hd(tl rec_cell3) + hd (tl (tl rec_cell3))) 9
+
+let () = 
+  Mt.from_pair_suites __FILE__ !suites
+
+
