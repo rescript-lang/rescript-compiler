03757a09a395b9699d4d883ffa6308be3a661793
Hongbo <bob.hongbo.zhang@foxmail.com>
2019-11-15 22:10:35 -0800

    Add a test case for functor type signature coercion

---
+++ jscomp/test/functors.js
@@ -0,0 +1,86 @@
+'use strict';
+
+var Curry = require("../../lib/js/curry.js");
+
+function O(X) {
+  var cow = function (x) {
+    return Curry._1(X.foo, x);
+  };
+  var sheep = function (x) {
+    return 1 + Curry._1(X.foo, x) | 0;
+  };
+  return {
+          cow: cow,
+          sheep: sheep
+        };
+}
+
+function F(X, Y) {
+  var cow = function (x) {
+    return Curry._1(Y.foo, Curry._1(X.foo, x));
+  };
+  var sheep = function (x) {
+    return 1 + cow(x) | 0;
+  };
+  return {
+          cow: cow,
+          sheep: sheep
+        };
+}
+
+function F1(X, Y) {
+  var cow = function (x) {
+    return Curry._1(Y.foo, Curry._1(X.foo, x));
+  };
+  var sheep = function (x) {
+    return 1 + cow(x) | 0;
+  };
+  return {
+          sheep: sheep
+        };
+}
+
+function F2(X, Y) {
+  var X$1 = {
+    foo: X.foo
+  };
+  var Y$1 = {
+    foo: Y.foo
+  };
+  var cow = function (x) {
+    return Curry._1(Y$1.foo, Curry._1(X$1.foo, x));
+  };
+  var sheep = function (x) {
+    return 1 + cow(x) | 0;
+  };
+  return {
+          sheep: sheep
+        };
+}
+
+var M = {
+  F: (function (funarg, funarg$1) {
+      var Y = {
+        foo: funarg$1.foo
+      };
+      var X = {
+        foo: funarg.foo
+      };
+      var cow = function (x) {
+        return Curry._1(Y.foo, Curry._1(X.foo, x));
+      };
+      var sheep = function (x) {
+        return 1 + cow(x) | 0;
+      };
+      return {
+              sheep: sheep
+            };
+    })
+};
+
+exports.O = O;
+exports.F = F;
+exports.F1 = F1;
+exports.F2 = F2;
+exports.M = M;
+/* No side effect */
