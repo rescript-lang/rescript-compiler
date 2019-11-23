c2b41400cee55e6d1174232065be11adfffb11c5
Patrick Stapfer <ryyppy@users.noreply.github.com>
2019-11-23 21:54:46 +0100

    Create jsoo_common.ml module for common logic for playground entrypoints

---
+++ jscomp/main/jsoo_common.ml
@@ -0,0 +1,39 @@
+module Js = struct
+  module Unsafe = struct
+    type any
+    external inject : 'a -> any = "%identity"
+    external get : 'a -> 'b -> 'c = "caml_js_get"
+    external set : 'a -> 'b -> 'c -> unit = "caml_js_set"
+    external pure_js_expr : string -> 'a = "caml_pure_js_expr"
+    let global = pure_js_expr "joo_global_object"
+    type obj
+    external obj : (string * any) array -> obj = "caml_js_object"
+  end
+  type (-'a, +'b) meth_callback
+  type 'a callback = (unit, 'a) meth_callback
+  external wrap_callback : ('a -> 'b) -> ('c, 'a -> 'b) meth_callback = "caml_js_wrap_callback"
+  external wrap_meth_callback : ('a -> 'b) -> ('a, 'b) meth_callback = "caml_js_wrap_meth_callback"
+  type + 'a t
+  type js_string
+  external string : string -> js_string t = "caml_js_from_string"
+  external to_string : js_string t -> string = "caml_js_to_string"
+  external create_file : js_string t -> js_string t -> unit = "caml_create_file"
+  external to_bytestring : js_string t -> string = "caml_js_to_byte_string"
+end
+
+let mk_js_error (loc: Location.t) (msg: string) = 
+  let (file,line,startchar) = Location.get_pos_info loc.Location.loc_start in
+  let (file,endline,endchar) = Location.get_pos_info loc.Location.loc_end in
+  Js.Unsafe.(obj
+  [|
+  "js_error_msg",
+    inject @@ Js.string (Printf.sprintf "Line %d, %d:\n  %s"  line startchar msg);
+  "row"    , inject (line - 1);
+  "column" , inject startchar;
+  "endRow" , inject (endline - 1);
+  "endColumn" , inject endchar;
+  "text" , inject @@ Js.string msg;
+  "type" , inject @@ Js.string "error"
+  |]
+  )
+  
--- jscomp/main/jsoo_refmt_main.ml
+++ jscomp/main/jsoo_refmt_main.ml
@@ -26,54 +26,8 @@
 `jsoo_refmt_main` is the JSOO compilation entry point for building BuckleScript + Refmt as one bundle.
 This is usually the file you want to build for the full playground experience.
 *)
-module Js = struct
-  module Unsafe = struct
-    type any
-    external inject : 'a -> any = "%identity"
-    external get : 'a -> 'b -> 'c = "caml_js_get"
-    external set : 'a -> 'b -> 'c -> unit = "caml_js_set"
-    external pure_js_expr : string -> 'a = "caml_pure_js_expr"
-    let global = pure_js_expr "joo_global_object"
-    type obj
-    external obj : (string * any) array -> obj = "caml_js_object"
-  end
-  type (-'a, +'b) meth_callback
-  type 'a callback = (unit, 'a) meth_callback
-  external wrap_callback : ('a -> 'b) -> ('c, 'a -> 'b) meth_callback = "caml_js_wrap_callback"
-  external wrap_meth_callback : ('a -> 'b) -> ('a, 'b) meth_callback = "caml_js_wrap_meth_callback"
-  type + 'a t
-  type js_string
-  external string : string -> js_string t = "caml_js_from_string"
-  external to_string : js_string t -> string = "caml_js_to_string"
-  external create_file : js_string t -> js_string t -> unit = "caml_create_file"
-  external to_bytestring : js_string t -> string = "caml_js_to_byte_string"
-end
 
-
-(*
- Error:
-     *  {
-     *    row: 12,
-     *    column: 2, //can be undefined
-     *    text: "Missing argument",
-     *    type: "error" // or "warning" or "info"
-     *  }
-*)
-let mk_js_error loc msg = 
-  let (file,line,startchar) = Location.get_pos_info loc.Location.loc_start in
-  let (file,endline,endchar) = Location.get_pos_info loc.Location.loc_end in
-  Js.Unsafe.(obj
-      [|
-        "js_error_msg",
-          inject @@ Js.string (Printf.sprintf "Line %d, %d:\n  %s"  line startchar msg);
-            "row"    , inject (line - 1);
-            "column" , inject startchar;
-            "endRow" , inject (endline - 1);
-            "endColumn" , inject endchar;
-            "text" , inject @@ Js.string msg;
-            "type" , inject @@ Js.string "error"
-      |]
-    )
+module Js = Jsoo_common.Js
 
 let () =  
   Bs_conditional_initial.setup_env ();
@@ -147,13 +101,13 @@ let implementation ~use_super_errors ?(react_ppx_version=V3) ?prefix impl str  :
       begin match error_of_exn e with
       | Some error ->
         Location.report_error Format.err_formatter  error;
-        mk_js_error error.loc error.msg
+        Jsoo_common.mk_js_error error.loc error.msg
       | None ->
         let msg = Printexc.to_string e in
         match e with
         | Refmt_api.Migrate_parsetree.Def.Migration_error (_,loc)
         | Refmt_api.Reason_errors.Reason_error (_,loc) ->
-          mk_js_error loc msg
+          Jsoo_common.mk_js_error loc msg
         | _ -> 
           Js.Unsafe.(obj [|
             "js_error_msg" , inject @@ Js.string msg;
@@ -168,15 +122,11 @@ let compile impl ~use_super_errors ?react_ppx_version =
 let shake_compile impl ~use_super_errors ?react_ppx_version prefix =
    implementation ~use_super_errors ?react_ppx_version ~prefix impl
 
-
-
 let load_module cmi_path cmi_content cmj_name cmj_content =
   Js.create_file cmi_path cmi_content;
   Js_cmj_datasets.data_sets :=
     String_map.add !Js_cmj_datasets.data_sets
       cmj_name (lazy (Js_cmj_format.from_string cmj_content))
-      
-
 
 let export (field : string) v =
   Js.Unsafe.set (Js.Unsafe.global) field v
@@ -186,7 +136,6 @@ let export (field : string) v =
 
 let dir_directory d =
   Config.load_path := d :: !Config.load_path
-
 
 let () =
   dir_directory "/static/cmis"
@@ -242,8 +191,6 @@ let make_compiler name impl prefix =
 
 let () = make_compiler "ocaml" Parse.implementation "[@@@bs.config{no_export}]\n#1 \"repl.ml\"\n"
 let () = make_compiler "reason" reason_parse "[@bs.config {no_export: no_export}];\n#1 \"repl.re\";\n"
-
-let _ = 1
 
 (* local variables: *)
 (* compile-command: "ocamlbuild -use-ocamlfind -pkg compiler-libs -no-hygiene driver.cmo" *)
