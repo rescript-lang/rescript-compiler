bf74e36357444306a0bdd19b3316bbc935756715
Hongbo Zhang <bob.hongbo.zhang@foxmail.com>
2019-11-24 12:10:18 -0800

    prepare for singleton variant -> undefined
    1. is_type_number has to be adjusted in several ways
    for singleton variant -- its type is now undefined
    for non-singleton variant -- it has to still be number
    2. Change Lam_compile_const.Const_pointer compilation
    3. Check Pisint

---
+++ jscomp/test/singular_unit_test.ml
@@ -0,0 +1,14 @@
+type t = A
+
+let f0 ( x : t) = 
+  match x with 
+  | A as y -> y
+
+let f1 (x : t) = 
+  match x with 
+  | A -> 2     
+
+let f3 x = 
+  match x with    
+  | Some (A as y) -> y
+  | None -> A
