70bbeb858c9716dc69a3d99a42a3f0fc20d67dd1
Hongbo Zhang <bob.hongbo.zhang@foxmail.com>
2019-12-09 10:55:02 +0800

    rm lib/4.02.3

--- jscomp/cleanlib.js
+++ jscomp/cleanlib.js
@@ -3,11 +3,7 @@ var cp = require('child_process')
 var fs = require('fs')
 function run() {
     cp.execSync(`make clean`)
-    if (fs.existsSync(`stdlib-406`)) {
-        cp.execSync(`git clean -dfx stdlib-406`)
-    }
-
-    cp.execSync(`git clean -dfx stdlib-402 test others runtime`)
+    cp.execSync(`git clean -dfx stdlib-406 test others runtime`)
 }
 
 if(require.main === module){
