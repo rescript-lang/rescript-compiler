1ea5ed55fc17524c980e6c6a9fb119093b64cb59
Alexander Ryzhikov <coobaha@gmail.com>
2019-12-14 20:06:34 +0200

    setup test

---
+++ jscomp/build_tests/duplicated_symlinked_packages/README.md
@@ -0,0 +1,4 @@
+Special tests for duplicated symlink warnings
+
+Run `node ./jscomp/build_tests/duplicated_symlinked_packages/input.js` to check the tests against previous snapshots.
+Run `node ./jscomp/build_tests/duplicated_symlinked_packages/input.js update` to update the snapshots (assuming you've made some changes to duplicated)
---
+++ jscomp/build_tests/duplicated_symlinked_packages/input.js
@@ -0,0 +1,32 @@
+const fs = require('fs')
+const path = require('path')
+const child_process = require('child_process')
+
+const expectedFilePath = path.join(__dirname, 'out.expected')
+
+const updateTests = process.argv[2] === 'update'
+
+function postProcessErrorOutput (output) {
+  output = output.trimRight()
+  output = output.replace(new RegExp(__dirname, 'gi'), '.')
+  return output
+}
+
+child_process.exec('bsb -make-world', {cwd: __dirname}, (err, stdout, stderr) => {
+  const actualErrorOutput = postProcessErrorOutput(stderr.toString())
+  if (updateTests) {
+    fs.writeFileSync(expectedFilePath, actualErrorOutput)
+  } else {
+    const expectedErrorOutput = postProcessErrorOutput(fs.readFileSync(expectedFilePath, {encoding: 'utf-8'}))
+    if (expectedErrorOutput !== actualErrorOutput) {
+      console.error(`The old and new error output aren't the same`)
+      console.error('\n=== Old:')
+      console.error(expectedErrorOutput)
+      console.error('\n=== New:')
+      console.error(actualErrorOutput)
+      process.exit(1)
+    }
+  }
+})
+
+
