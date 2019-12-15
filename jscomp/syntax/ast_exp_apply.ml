c2eef107b61b80819d0dc16767b057e840df1f02
Alexander Ryzhikov <coobaha@gmail.com>
2019-12-15 18:54:13 +0200

    Add variant to matching structs comment

--- jscomp/syntax/ast_exp_apply.ml
+++ jscomp/syntax/ast_exp_apply.ml
@@ -102,6 +102,7 @@ let app_exp_mapper
         a |. M.(f b c) --> M.f a M.b M.c
         a |. (g |. b)
         a |. M.Some
+        a |. M.`Variant
       *)
        let new_obj_arg = self.expr self obj_arg in
        let fn = self.expr self fn in 
