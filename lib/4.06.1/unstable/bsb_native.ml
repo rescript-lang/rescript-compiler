415e04adccbc1db10dfcb15e33b2170ceab64e54
Hongbo Zhang <bob.hongbo.zhang@foxmail.com>
2019-12-19 16:21:02 +0800

    snapshot

--- lib/4.06.1/unstable/bsb_native.ml
+++ lib/4.06.1/unstable/bsb_native.ml
@@ -9597,6 +9597,14 @@ module Bsb_build_util : sig
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *)
 
 (**
+  Use:
+  {[
+  flag_concat "-ppx" [ppxs]
+  ]}
+  *)
+  val flag_concat : string -> string list -> string
+
+(**
 Build quoted commandline arguments for bsc.exe for the given ppx flags
 
 Use:
