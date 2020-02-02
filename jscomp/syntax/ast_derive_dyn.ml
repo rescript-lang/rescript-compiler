ab9ed6c3d28a48c9a6d38c48e1bd749f8e3daca3
Hongbo Zhang <bob.hongbo.zhang@foxmail.com>
2020-02-02 17:25:59 +0800

    comment Ast_derive_dyn which is unused

--- jscomp/syntax/ast_derive_dyn.ml
+++ jscomp/syntax/ast_derive_dyn.ml
@@ -21,7 +21,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *)
-
+(* 
 open Ast_helper
 
 let not_supported loc =
@@ -355,4 +355,4 @@ let init ()  =
              end
 
          }
-     end
+     end *)
