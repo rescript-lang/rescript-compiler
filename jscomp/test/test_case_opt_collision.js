f9f2d46185a270f33482451fc3830070c9898e56
Hongbo Zhang <bob.hongbo.zhang@foxmail.com>
2020-02-04 11:01:52 +0800

    add a test case

---
+++ jscomp/test/test_case_opt_collision.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var Mt = require("./mt.js");
+
+var suites = {
+  contents: /* [] */0
+};
+
+var test_id = {
+  contents: 0
+};
+
+function eq(loc, x, y) {
+  return Mt.eq_suites(test_id, suites, loc, x, y);
+}
+
+function f(xOpt, y) {
+  var x = xOpt !== undefined ? xOpt : 3;
+  var xOpt$1 = x + 2 | 0;
+  console.log(xOpt$1);
+  return xOpt$1 + y | 0;
+}
+
+console.log(f(undefined, 2));
+
+eq("File \"test_case_opt_collision.ml\", line 15, characters 6-13", f(undefined, 2), 7);
+
+eq("File \"test_case_opt_collision.ml\", line 17, characters 6-13", f(4, 2), 8);
+
+Mt.from_pair_suites("test_case_opt_collision.ml", suites.contents);
+
+exports.suites = suites;
+exports.test_id = test_id;
+exports.eq = eq;
+exports.f = f;
+/*  Not a pure module */
