59efd20f5b719e54985e29c03b2b74c980be033f
Hongbo Zhang <bob.hongbo.zhang@foxmail.com>
2020-03-05 11:47:29 +0800

    add bench

--- jscomp/test/int64_string_bench.js
+++ jscomp/test/int64_string_bench.js
@@ -21,8 +21,21 @@ console.time("Int64.to_string");
 for(var i$1 = 0; i$1 <= 100000; ++i$1){
   Caml_int64.to_string(/* int64 */[
         /* hi */0,
-        /* lo */30000
+        /* lo */30000000
       ]);
+}
+
+console.timeEnd("Int64.to_string");
+
+console.time("Int64.to_string");
+
+var u$1 = Caml_int64.add(Int64.min_int, /* int64 */[
+      /* hi */0,
+      /* lo */100
+    ]);
+
+for(var i$2 = 0; i$2 <= 100000; ++i$2){
+  Caml_int64.to_string(u$1);
 }
 
 console.timeEnd("Int64.to_string");
--- jscomp/test/int64_string_bench.ml
+++ jscomp/test/int64_string_bench.ml
@@ -2,18 +2,26 @@
 
 let () = 
   Js.Console.timeStart "Int64.to_string";
-  let u = Int64.(sub max_int 200_000L) in 
+  let u = Int64.( max_int |. sub 200_000L) in 
   (* let u = 3L in  *)
   for i = 0 to 1_00_000 do 
     Int64.to_string u |> ignore
   done   ;
   Js.Console.timeEnd "Int64.to_string"
 
-  let () =
-    Js.Console.timeStart "Int64.to_string";
-    let u = 30_000L in 
-    for i = 0 to 1_00_000 do 
-      Int64.to_string u |> ignore
-    done   ;
-    Js.Console.timeEnd "Int64.to_string"
-  
+let () =
+  Js.Console.timeStart "Int64.to_string";
+  let u = 30_000_000L in 
+  for i = 0 to 1_00_000 do 
+    Int64.to_string u |> ignore
+  done   ;
+  Js.Console.timeEnd "Int64.to_string"
+
+
+let () =  
+  Js.Console.timeStart "Int64.to_string";
+  let u = Int64.(min_int |. add 100L) in 
+  for i = 0 to 1_00_000 do 
+    Int64.to_string u |> ignore
+  done   ;
+  Js.Console.timeEnd "Int64.to_string"
