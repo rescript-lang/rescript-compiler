5b9da0236144e506cc09021d248c23ed9713538c
Hongbo Zhang <bob.hongbo.zhang@foxmail.com>
2020-03-05 17:00:40 +0800

    snapshot js files

---
+++ jscomp/test/int64_string_bench.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var Int64 = require("../../lib/js/int64.js");
+var Caml_int64 = require("../../lib/js/caml_int64.js");
+var Caml_format = require("../../lib/js/caml_format.js");
+
+console.time("Int64.to_string");
+
+var u = Caml_int64.sub(Int64.max_int, /* int64 */[
+      /* hi */0,
+      /* lo */200000
+    ]);
+
+for(var i = 0; i <= 100000; ++i){
+  Caml_format.caml_int64_format("%d", u);
+}
+
+console.timeEnd("Int64.to_string");
+
+console.time("Int64.to_string");
+
+for(var i$1 = 0; i$1 <= 100000; ++i$1){
+  Caml_format.caml_int64_format("%d", /* int64 */[
+        /* hi */0,
+        /* lo */30000000
+      ]);
+}
+
+console.timeEnd("Int64.to_string");
+
+console.time("Int64.to_string");
+
+var u$1 = Caml_int64.add(Int64.min_int, /* int64 */[
+      /* hi */0,
+      /* lo */100
+    ]);
+
+for(var i$2 = 0; i$2 <= 100000; ++i$2){
+  Caml_format.caml_int64_format("%d", u$1);
+}
+
+console.timeEnd("Int64.to_string");
+
+/*  Not a pure module */
