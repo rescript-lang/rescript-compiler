b49b9cc0d833f787f28255ddfd68d6ca3e92b216
Hongbo Zhang <bob.hongbo.zhang@foxmail.com>
2020-03-18 11:48:23 +0800

    Note we keep Super_typecore logic intact since we don't translate all stuff, notably bs.meth and bs.this

---
+++ jscomp/test/arity.js
@@ -0,0 +1,29 @@
+'use strict';
+
+var Curry = require("../../lib/js/curry.js");
+
+function u(f, a, b) {
+  console.log(f(a, b));
+  console.log(f(a, b));
+  return /* () */0;
+}
+
+function u2(f, a, b) {
+  console.log(Curry._2(f, a, b));
+  console.log(Curry._2(f, a, b));
+  return /* () */0;
+}
+
+function f(x, y) {
+  return x + y | 0;
+}
+
+function add(prim, prim$1) {
+  return prim + prim$1 | 0;
+}
+
+exports.u = u;
+exports.u2 = u2;
+exports.f = f;
+exports.add = add;
+/* No side effect */
