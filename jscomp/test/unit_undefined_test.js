54d4709cbd33b753779587247af86db7b7843b53
Hongbo Zhang <bob.hongbo.zhang@foxmail.com>
2020-03-26 14:46:01 +0800

    upgrade compiler, fix some edge cases

--- jscomp/test/unit_undefined_test.js
+++ jscomp/test/unit_undefined_test.js
@@ -1,5 +1,19 @@
 'use strict';
 
+var Mt = require("./mt.js");
+var Caml_option = require("../../lib/js/caml_option.js");
+
+var suites = {
+  contents: /* [] */0
+};
+
+var test_id = {
+  contents: 0
+};
+
+function eq(loc, x, y) {
+  return Mt.eq_suites(test_id, suites, loc, x, y);
+}
 
 function f_01(param) {
   return hi((function () {
@@ -20,6 +34,45 @@ function u(x) {
   }
 }
 
+function fx(param) {
+  
+}
+
+function u0(x) {
+  return Caml_option.some(x);
+}
+
+var u1 = Caml_option.some(void 0);
+
+function u2(x) {
+  return Caml_option.some(x);
+}
+
+var u3 = Caml_option.some(void 0);
+
+eq("File \"unit_undefined_test.ml\", line 39, characters 6-13", Caml_option.some(void 0), Caml_option.some(void 0));
+
+eq("File \"unit_undefined_test.ml\", line 40, characters 6-13", u1, Caml_option.some(void 0));
+
+eq("File \"unit_undefined_test.ml\", line 41, characters 6-13", Caml_option.some(void 0), Caml_option.some(void 0));
+
+eq("File \"unit_undefined_test.ml\", line 42, characters 6-13", u3, Caml_option.some(void 0));
+
+eq("File \"unit_undefined_test.ml\", line 43, characters 6-13", void 0, void 0);
+
+Mt.from_pair_suites("unit_undefined_test.ml", suites.contents);
+
+var u4;
+
+exports.suites = suites;
+exports.test_id = test_id;
+exports.eq = eq;
 exports.f_01 = f_01;
 exports.u = u;
-/* No side effect */
+exports.fx = fx;
+exports.u0 = u0;
+exports.u1 = u1;
+exports.u2 = u2;
+exports.u3 = u3;
+exports.u4 = u4;
+/*  Not a pure module */
--- jscomp/test/unit_undefined_test.ml
+++ jscomp/test/unit_undefined_test.ml
@@ -2,7 +2,13 @@
 [@@@bs.config{flags = 
 [|
   "-bs-diagnose"
+  ; "-drawlambda"
+  ; "-dtypedtree"
 |]}]
+
+let suites :  Mt.pair_suites ref  = ref []
+let test_id = ref 0
+let eq loc x y = Mt.eq_suites ~test_id ~suites loc x y 
 
 
 external
@@ -18,3 +24,22 @@ let u x =
   | () when x < 2 -> 2
   | () when x > 4 -> 0
   | () -> 3 
+
+let fx () = ()  
+
+let u0 (x : unit) = Some x 
+
+let u1 = Some ()
+type t = ()
+
+let u2 (x : t) = Some x 
+let u3 : t option = Some ()
+let u4 : t = ()
+
+;; eq __LOC__ (u0 ()) (Obj.magic (Some None))
+;; eq __LOC__ (u1) (Obj.magic (Some None))
+;; eq __LOC__ (u2 ()) (Obj.magic (Some None))
+;; eq __LOC__ (u3) (Obj.magic (Some None))
+;; eq __LOC__ u4 (Obj.magic None)
+
+;; Mt.from_pair_suites __FILE__ !suites
