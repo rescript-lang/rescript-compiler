5c307ddc34dd1ea297f94af7ad12031bf948f5dd
ELLIOTTCABLE <git@ell.io>
2020-04-13 12:31:05 -0500

    (- style re) Reformat relevant JS/JSON files with Prettier

--- jscomp/build_tests/in_source/input.js
+++ jscomp/build_tests/in_source/input.js
@@ -2,25 +2,20 @@ var child_process = require('child_process')
 
 var assert = require('assert')
 
-
-
-
 assert.throws(
-    () => {
-        var output = child_process.execSync(`bsb -regen`,
-            { cwd: __dirname, encoding: 'utf8' }
-        )
+  () => {
+    var output = child_process.execSync(`bsb -regen`, {
+      cwd: __dirname,
+      encoding: 'utf8',
+    })
+  },
+  function (err) {
+    if (err.message.match(/detected two module formats/)) {
+      return true
     }
-    ,
-    function (err){
-        if (err.message.match(/detected two module formats/)){
-            return true
-        }
-        return false
-    }
-    
+    return false
+  }
 )
-
 
 // assert.throws(()=>{
 //     throw new Error('Wrong value')
