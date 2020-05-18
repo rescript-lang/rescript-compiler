321d7191c1ff7b04a32e805fc723547d6c498940
Hongbo Zhang <bob.hongbo.zhang@foxmail.com>
2020-05-18 19:10:35 +0800

    snapshot

--- jscomp/core/lam_convert.ml
+++ jscomp/core/lam_convert.ml
@@ -795,9 +795,9 @@ let convert (exports : Set_ident.t) (lam : Lambda.lambda) : Lam.t * Lam_module_i
         | {
           sw_failaction = None ;
           sw_blocks = [];
-          sw_blocks_full = 0;
+          sw_numblocks = 0;
           sw_consts ;
-          sw_consts_full ;
+          sw_numconsts ;
         } ->
           begin match happens_to_be_diff sw_consts with
             | Some 0 -> e
@@ -814,15 +814,15 @@ let convert (exports : Set_ident.t) (lam : Lambda.lambda) : Lam.t * Lam_module_i
                  sw_consts =
                    Ext_list.map_snd  sw_consts convert_aux;
                  sw_consts_full = 
-                   Ext_list.length_ge sw_consts sw_consts_full;
+                   Ext_list.length_ge sw_consts sw_numconsts;
                  sw_names = s.sw_names;
                 }
           end
         | _ -> 
           Lam.switch  e   
-            { sw_consts_full =  Ext_list.length_ge s.sw_consts s.sw_consts_full ;
+            { sw_consts_full =  Ext_list.length_ge s.sw_consts s.sw_numconsts ;
               sw_consts = Ext_list.map_snd  s.sw_consts convert_aux;
-              sw_blocks_full = Ext_list.length_ge s.sw_blocks s.sw_blocks_full;
+              sw_blocks_full = Ext_list.length_ge s.sw_blocks s.sw_numblocks;
               sw_blocks = Ext_list.map_snd s.sw_blocks convert_aux;
               sw_failaction =Ext_option.map s.sw_failaction convert_aux;
               sw_names = s.sw_names } in
