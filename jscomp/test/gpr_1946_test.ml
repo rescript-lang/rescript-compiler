1142c37028bda4df762c892f2007a824ef3971b4
Hongbo Zhang <bob.hongbo.zhang@foxmail.com>
2020-05-29 09:47:08 +0800

    add a test case

--- jscomp/test/gpr_1946_test.ml
+++ jscomp/test/gpr_1946_test.ml
@@ -1,4 +1,7 @@
 
+let suites :  Mt.pair_suites ref  = ref []
+let test_id = ref 0
+let eq loc x y = Mt.eq_suites ~test_id ~suites loc x y 
 
 
 
@@ -10,6 +13,8 @@ let x =  [%obj{x = 3 ; y = 4}]##x
 let zz =  [%obj{_5 = 3 }]##_5
 
 
-;; [%obj{_5 = 3 }]##_5
 
+;; eq __LOC__ [%obj{_5 = 3 }]##_5 3 
 ;; Js.log @@ Obj.tag (Obj.repr [%obj{_5 = 3 }])
+
+;; Mt.from_pair_suites __LOC__ !suites
