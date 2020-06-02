6fd94c1bbdcf051a6991de56dd894043d276341b
Hongbo Zhang <bob.hongbo.zhang@foxmail.com>
2020-06-02 18:01:20 +0800

    prototype #4434 compile recursive module in a better way

---
+++ jscomp/test/rec_module_opt.ml
@@ -0,0 +1,30 @@
+
+[@@@bs.config {
+  flags = [|
+  "-drawlambda";
+  (* "-dlambda";  *)
+  (* "-dtypedtree"; *)
+  (* "-bs-diagnose" *)
+
+  "-bs-no-cross-module-opt"
+  |]
+}]
+
+module rec A : sig
+  type t = Leaf of string | Node of ASet.t
+  val compare: t -> t -> int
+end = struct
+  type t = Leaf of string | Node of ASet.t
+  let compare t1 t2 =
+    match (t1, t2) with
+    | (Leaf s1, Leaf s2) -> Pervasives.compare s1 s2
+    | (Leaf _, Node _) -> 1
+    | (Node _, Leaf _) -> -1
+    | (Node n1, Node n2) -> ASet.compare n1 n2
+  let hello x = x  
+end
+and ASet
+  : Set.S with type elt = A.t
+  = Set.Make(A)
+
+module rec X : sig end = X  
