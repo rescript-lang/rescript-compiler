dd0ed9b9500cc68b178d6a6d0f31616fcd6539c7
Hongbo Zhang <bob.hongbo.zhang@foxmail.com>
2020-07-08 16:32:02 +0800

    snapshot

--- lib/4.06.1/unstable/all_ounit_tests.ml
+++ lib/4.06.1/unstable/all_ounit_tests.ml
@@ -9705,6 +9705,7 @@ val record_as_js_object : bool ref
 val as_ppx : bool ref 
 
 val mono_empty_array : bool ref
+val napkin : bool ref 
 end = struct
 #1 "js_config.ml"
 (* Copyright (C) 2015-2016 Bloomberg Finance L.P.
@@ -9818,6 +9819,8 @@ let record_as_js_object = ref false (* otherwise has an attribute *)
 let as_ppx = ref false
 
 let mono_empty_array = ref true
+
+let napkin = ref false
 end
 module Ml_binary : sig 
 #1 "ml_binary.mli"
