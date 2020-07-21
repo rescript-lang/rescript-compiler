0e309c6a5e568199b9942456dfbe5535a746ec6c
Patrick Stapfer <ryyppy@users.noreply.github.com>
2020-07-21 14:39:35 +0200

    Playground: First major refactor of the BS JS bundle API
    This commit introduces a breaking change to reorganize the API as
    discussed in issue #4450.
    
    The change will revolve around following features:
    - Introduce a `bs_platform` namespace for all the functionality
    - Reduce the global state of the compiler to allow different settings per instance
    - Add configuration support to be able to manipulate the compiler setup
    - Make super_errors reporting the default
    
    **Introduce a bs_platform namespace**
    
    Before we were independently adding language support via `window.reason`, `window.ocaml` and
    `window.napkin`, etc. This behavior pollutes the global namespace, is harder to discover and
    could potentially cause override conflicts.
    
    With one unified `bs_platform` object, we can now group together all necessary functionality.
    
    **Reduce global state / allow different settings**
    
    We wanted to introduce different settings for different instances of the
    bs_platform bundle API. The way the bundle was designed, the settings state
    was fully dependend on the global compiler state, meaning that in case we
    are using the bundle for things like multiple independent codesnippets within
    a documentation page, everytime we are setting the compilation setting for one
    snippet, it would automatically change it for all the snippets as well.
    
    Therefore we needed to introduce a factory function that looks like this:
    
    ```
    // load bs bundle globally via <script/> etc...
    let bsc = bs_platform.make();
    
    bsc.reason.setModuleType("es6");
    bsc.reason.compile("let a = 1");
    ```
    
    Internally, the API is still relying on one global compiler state, but we are now
    able to locally differentiate between different setting profiles by injecting a
    config record to each compile call.
    
    **Make super_errors reporting the default**
    
    Previously we offered functions like `ocaml.compile` and `ocaml.compile_super_errors` to
    switch between different reporting engines. Problem was, that once you called `compile_super_errors`,
    it would register the super_error reporters, and any further call with `use_super_errors=false` would
    not take any effect, since the super_error reporters would not be removed (in fact, there are no apis to
    clean up the changes of super_main.ml's setup function).
    
    The goal is to make super_errors really solid, and it's probably the best default experience
    for our users anyways. So to circumvent this global state issue by managing error reporters, we
    just drop support for non super_errors behavior.

--- jscomp/main/jsoo_common.mli
+++ jscomp/main/jsoo_common.mli
@@ -10,26 +10,39 @@ module Js :
         external get : 'a -> 'b -> 'c = "caml_js_get"
         external set : 'a -> 'b -> 'c -> unit = "caml_js_set"
         external pure_js_expr : string -> 'a = "caml_pure_js_expr"
+        external fun_call : 'a -> any array -> 'b = "caml_js_fun_call"
         val global : 'a
         type obj
         external obj : (string * any) array -> obj = "caml_js_object"
       end
     type (-'a, +'b) meth_callback
     type 'a callback = (unit, 'a) meth_callback
+
     external wrap_callback : ('a -> 'b) -> ('c, 'a -> 'b) meth_callback
       = "caml_js_wrap_callback"
     external wrap_meth_callback : ('a -> 'b) -> ('a, 'b) meth_callback
       = "caml_js_wrap_meth_callback"
+
     type +'a t
+
     type js_string
     external string : string -> js_string t = "caml_js_from_string"
     external to_string : js_string t -> string = "caml_js_to_string"
+
+    type 'a js_array
+    external array : 'a array -> 'a js_array = "caml_js_from_array"
+
+    external bool : bool -> bool t = "caml_js_from_bool"
+    external to_bool : bool t -> bool = "caml_js_to_bool"
+
+    type number
+    external number_of_float : float -> number t = "caml_js_from_float"
+    external float_of_number : number t -> float = "caml_js_to_float"
+
     external create_file : js_string t -> js_string t -> unit
       = "caml_create_file"
     external to_bytestring : js_string t -> string = "caml_js_to_byte_string"
 
-    type 'a js_array
-    external array : 'a array -> 'a js_array = "caml_js_from_array"
   end
 
 (*
--- jscomp/refmt/jsoo_refmt_main.ml
+++ jscomp/refmt/jsoo_refmt_main.ml
@@ -23,12 +23,15 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *)
 
 (**
-`jsoo_refmt_main` is the JSOO compilation entry point for building BuckleScript + Refmt as one bundle.
+`jsoo_refmt_main` is the JSOO compilation entry point for building BuckleScript + Refmt + Res syntax as one bundle.
 This is usually the file you want to build for the full playground experience.
 *)
 
 module Js = Jsoo_common.Js
 
+let export (field : string) v =
+  Js.Unsafe.set (Js.Unsafe.global) field v
+;;
 
 module Lang = struct
   type t = OCaml | Reason | Res
@@ -45,6 +48,26 @@ module Lang = struct
     | Res -> "res"
 end
 
+module BundleConfig = struct
+  type t = {
+    mutable module_system: Js_packages_info.module_system;
+    mutable filename: string option;
+  }
+
+  let make () = {
+    module_system=Js_packages_info.NodeJS;
+    filename=None;
+  }
+
+
+  let default_filename (lang: Lang.t) = "playground." ^ (Lang.toString lang)
+
+  let string_of_module_system m = (match m with
+    | Js_packages_info.NodeJS -> "nodejs"
+    | Es6 -> "es6"
+    | Es6_global -> "es6_global")
+end
+
 type napkinError = {
   fullMsg: string; (* Full report string with all context *)
   text: string; (* simple explain message without any extra context *)
@@ -52,7 +75,6 @@ type napkinError = {
 }
 
 exception NapkinParsingErrors of napkinError list
-
 
 module ErrorRet = struct
   let makeJsError ~(js_error_msg: string) ~(text: string) (loc: Location.t) =
@@ -93,13 +115,15 @@ end
 
 let () =
   Bs_conditional_initial.setup_env ();
-  Clflags.binary_annotations := false
+  Clflags.binary_annotations := false;
+  Misc.Color.setup (Some Always);
+  Lazy.force Super_main.setup
 
 let error_of_exn e =
-  match Location.error_of_exn e with
+  (match Location.error_of_exn e with
   | Some (`Ok e) -> Some e
   | Some `Already_displayed
-  | None -> None
+  | None -> None)
 
 (* Handles parse / type check errors / unexpected errors and converts them to Js.object results *)
 let handle_err e =
@@ -120,69 +144,25 @@ let handle_err e =
          ErrorRet.fromLocErrors [|error|]
        | _ -> ErrorRet.makeUnexpectedError msg)
 
-let implementation ~use_super_errors impl str  : Js.Unsafe.obj =
-  let modulename = "Test" in
-  (* let env = !Toploop.toplevel_env in *)
-  (* Compmisc.init_path false; *)
-  (* let modulename = module_of_filename ppf sourcefile outputprefix in *)
-  (* Env.set_unit_name modulename; *)
-  Lam_compile_env.reset () ;
-  let env = Compmisc.initial_env() in (* Question ?? *)
-  (* let finalenv = ref Env.empty in *)
-  let types_signature = ref [] in
-  if use_super_errors then begin
-    Misc.Color.setup (Some Always);
-    Lazy.force Super_main.setup ;
-  end;
-
-  try
-    Js_config.jsx_version :=  3 ; (* default *)
-    let ast = impl (str) in
-    let ast = Ppx_entry.rewrite_implementation ast in
-    let typed_tree =
-      let (a,b,_,signature) = Typemod.type_implementation_more modulename modulename modulename env ast in
-      (* finalenv := c ; *)
-      types_signature := signature;
-      (a,b) in
-  typed_tree
-  |>  Translmod.transl_implementation modulename
-  |> (* Printlambda.lambda ppf *) (fun {Lambda.code = lam} ->
-      let buffer = Buffer.create 1000 in
-      let () = Js_dump_program.pp_deps_program
-                          ~output_prefix:"" (* does not matter here *)
-                          NodeJS
-                          (Lam_compile_main.compile ""
-                              lam)
-                          (Ext_pp.from_buffer buffer) in
-      let v = Buffer.contents buffer in
-      Js.Unsafe.(obj [|
-          "js_code", inject @@ Js.string v;
-          "type" , inject @@ Js.string "success"
-        |]))
-  with
-  | e -> handle_err e;;
-
-let compile ~use_super_errors impl =
-    implementation ~use_super_errors impl
-
-let export (field : string) v =
-  Js.Unsafe.set (Js.Unsafe.global) field v
-;;
-
-(* To add a directory to the load path *)
-let dir_directory d =
-  Config.load_path := d :: !Config.load_path
-let () =
-  dir_directory "/static"
-
 module Converter = Refmt_api.Migrate_parsetree.Convert(Refmt_api.Migrate_parsetree.OCaml_404)(Refmt_api.Migrate_parsetree.OCaml_406)
 module Converter404 = Refmt_api.Migrate_parsetree.Convert(Refmt_api.Migrate_parsetree.OCaml_406)(Refmt_api.Migrate_parsetree.OCaml_404)
 
-let reason_parse str =
-  str |> Lexing.from_string |> Refmt_api.Reason_toolchain.RE.implementation |> Converter.copy_structure;;
+let get_filename ~(lang: Lang.t) opt =
+  match opt with
+  | Some fname -> fname
+  | None -> BundleConfig.default_filename lang
 
-let ocaml_parse str =
-  Lexing.from_string str |> Parse.implementation;;
+let lexbuf_from_string ~filename str =
+  let lexbuf = Lexing.from_string str in
+  lexbuf.lex_start_p <- { lexbuf.lex_start_p with pos_fname = filename };
+  lexbuf.lex_curr_p <- { lexbuf.lex_curr_p with pos_fname = filename };
+  lexbuf
+
+let reason_parse ~filename str =
+  lexbuf_from_string ~filename str |> Refmt_api.Reason_toolchain.RE.implementation |> Converter.copy_structure;;
+
+let ocaml_parse ~filename str =
+  lexbuf_from_string ~filename str |> Parse.implementation
 
 module NapkinDriver = struct
   (* For now we are basically overriding functionality from Napkin_driver *)
@@ -237,14 +217,65 @@ module NapkinDriver = struct
     [@@raises Location.Error]
 end
 
-let napkin_parse src =
-  let (structure, _ ) = NapkinDriver.parse_implementation ~forPrinter:true ~sourcefile:"playground.res" ~src
+let napkin_parse ~filename src =
+  let (structure, _ ) = NapkinDriver.parse_implementation ~forPrinter:true ~sourcefile:filename ~src
   in
   structure
 
-let parse_and_print ~(from:Lang.t) ~(to_:Lang.t) (src: string) =
+let implementation ~(config: BundleConfig.t) ~lang str  : Js.Unsafe.obj =
+  let {BundleConfig.module_system} = config in
+  let filename = get_filename ~lang config.filename in
+  let modulename = "Playground" in
+  let impl = match lang with
+    | Lang.OCaml -> ocaml_parse ~filename
+    | Reason -> reason_parse ~filename
+    | Res -> napkin_parse ~filename
+  in
+  (* let env = !Toploop.toplevel_env in *)
+  (* Compmisc.init_path false; *)
+  (* let modulename = module_of_filename ppf sourcefile outputprefix in *)
+  (* Env.set_unit_name modulename; *)
+  Lam_compile_env.reset () ;
+  let env = Compmisc.initial_env() in (* Question ?? *)
+  (* let finalenv = ref Env.empty in *)
+  let types_signature = ref [] in
+  try
+    Js_config.jsx_version := 3; (* default *)
+    let ast = impl (str) in
+    let ast = Ppx_entry.rewrite_implementation ast in
+    let typed_tree =
+      let (a,b,_,signature) = Typemod.type_implementation_more modulename modulename modulename env ast in
+      (* finalenv := c ; *)
+      types_signature := signature;
+      (a,b) in
+  typed_tree
+  |>  Translmod.transl_implementation modulename
+  |> (* Printlambda.lambda ppf *) (fun {Lambda.code = lam} ->
+      let buffer = Buffer.create 1000 in
+      let () = Js_dump_program.pp_deps_program
+                          ~output_prefix:"" (* does not matter here *)
+                          module_system
+                          (Lam_compile_main.compile ""
+                              lam)
+                          (Ext_pp.from_buffer buffer) in
+      let v = Buffer.contents buffer in
+      Js.Unsafe.(obj [|
+          "js_code", inject @@ Js.string v;
+          "type" , inject @@ Js.string "success"
+        |]))
+  with
+  | e -> handle_err e;;
+
+
+(* To add a directory to the load path *)
+let dir_directory d =
+  Config.load_path := d :: !Config.load_path
+let () =
+  dir_directory "/static"
+
+let parse_and_print ?(filename: string option) ~(from:Lang.t) ~(to_:Lang.t) (src: string) =
   let open Lang in
-  let sourcefile = "playground.res" in
+  let filename = get_filename ~lang:from filename in
   let handle_ret ~lang str =
     Js.Unsafe.(obj [|
         "code", inject @@ Js.string str;
@@ -255,22 +286,23 @@ let parse_and_print ~(from:Lang.t) ~(to_:Lang.t) (src: string) =
   try
     (match (from, to_) with
      | (Reason, OCaml) ->
-       src |>
-       Lexing.from_string
+       src
+       |> lexbuf_from_string ~filename
        |> Refmt_api.Reason_toolchain.RE.implementation_with_comments
        |> Refmt_api.Reason_toolchain.ML.print_implementation_with_comments Format.str_formatter;
        handle_ret ~lang:OCaml (Format.flush_str_formatter ())
      | (OCaml, Reason) ->
        src
-       |> Lexing.from_string
+       |> lexbuf_from_string ~filename
        |> Refmt_api.Reason_toolchain.ML.implementation_with_comments
        |> Refmt_api.Reason_toolchain.RE.print_implementation_with_comments Format.str_formatter;
        handle_ret ~lang:Reason (Format.flush_str_formatter ())
      | (Reason, Res) ->
-       let ast = src
-                 |> Lexing.from_string
-                 |> Refmt_api.Reason_toolchain.RE.implementation
-                 |> Converter.copy_structure
+       let ast = 
+         src
+         |> lexbuf_from_string ~filename
+         |> Refmt_api.Reason_toolchain.RE.implementation
+         |> Converter.copy_structure
        in
        let structure = ast
                        |> Napkin_ast_conversion.normalizeReasonArityStructure ~forPrinter:true
@@ -279,7 +311,7 @@ let parse_and_print ~(from:Lang.t) ~(to_:Lang.t) (src: string) =
        handle_ret ~lang:Res (Napkin_printer.printImplementation ~width:80 structure ~comments:[])
      | (Res, Reason) ->
        let (structure, _) =
-         NapkinDriver.parse_implementation ~forPrinter:true ~sourcefile ~src
+         NapkinDriver.parse_implementation ~forPrinter:true ~sourcefile:filename ~src
        in
        let sanitized = structure
                        |> Napkin_ast_conversion.normalizeReasonArityStructure ~forPrinter:true
@@ -288,25 +320,29 @@ let parse_and_print ~(from:Lang.t) ~(to_:Lang.t) (src: string) =
        Refmt_api.Reason_toolchain.RE.print_implementation_with_comments Format.str_formatter (Converter404.copy_structure sanitized, []);
        handle_ret ~lang:Reason (Format.flush_str_formatter ())
      | (OCaml, Res) ->
-       let structure = Lexing.from_string src |> Parse.implementation in
+       let structure = 
+         src
+         |> lexbuf_from_string ~filename
+         |> Parse.implementation
+       in
        handle_ret ~lang:Res (Napkin_printer.printImplementation ~width:80 structure ~comments:[])
      | (Res, OCaml) ->
        let (structure, _) =
-         NapkinDriver.parse_implementation ~forPrinter:true ~sourcefile ~src
+         NapkinDriver.parse_implementation ~forPrinter:true ~sourcefile:filename ~src
        in
        Pprintast.structure Format.str_formatter structure;
        handle_ret ~lang:OCaml (Format.flush_str_formatter ())
      | (Res, Res) ->
        (* Basically pretty printing *)
        let (structure, comments) =
-         NapkinDriver.parse_implementation ~forPrinter:true ~sourcefile ~src
+         NapkinDriver.parse_implementation ~forPrinter:true ~sourcefile:filename ~src
        in
        handle_ret ~lang:Res (Napkin_printer.printImplementation ~width:80 structure ~comments)
      | (OCaml, OCaml) ->
        handle_ret ~lang:OCaml src
      | (Reason, Reason) ->
        let astAndComments = src
-                            |> Lexing.from_string
+                            |> lexbuf_from_string ~filename
                             |> Refmt_api.Reason_toolchain.RE.implementation_with_comments
        in
        Refmt_api.Reason_toolchain.RE.print_implementation_with_comments Format.str_formatter astAndComments;
@@ -316,68 +352,148 @@ let parse_and_print ~(from:Lang.t) ~(to_:Lang.t) (src: string) =
   | e -> handle_err e
 
 
-let make_compiler ~name impl =
-  export name
+module Export = struct
+  let make_compiler ~config ~lang =
+    let open Lang in
+    let open Js.Unsafe in
+    let baseAttrs = 
+      [|"compile",
+        inject @@
+        Js.wrap_meth_callback
+          (fun _ code ->
+             (implementation ~config ~lang (Js.to_string code)));
+        "version",
+        inject @@
+        Js.string 
+          (match lang with
+           | Reason -> Refmt_api.version
+           | Res -> Bs_version.version
+           | OCaml -> Sys.ocaml_version);
+      |] in
+    let attrs =
+      if lang != OCaml then
+        Array.append baseAttrs [|
+          ("pretty_print",
+           inject @@
+           Js.wrap_meth_callback
+             (fun _ code ->
+                (match lang with
+                 | OCaml -> ErrorRet.makeUnexpectedError ("OCaml pretty printing not supported")
+                 | _ -> parse_and_print ?filename:config.filename ~from:lang ~to_:lang (Js.to_string code))))
+        |]
+      else
+        baseAttrs
+    in
+    obj attrs
+
+  let make_settings ~(config: BundleConfig.t) = 
+    let open Lang in
+    let set_module_system value =
+      match value with
+      | "es6" ->
+        config.module_system <- Js_packages_info.Es6; true
+      | "nodejs" ->
+        config.module_system <- NodeJS; true
+      | _ -> false in
+    let set_filename value =
+      config.filename <- Some value; true
+    in
     (Js.Unsafe.(obj
-                  [|"compile",
+                  [|
+                    "setModuleSystem",
                     inject @@
                     Js.wrap_meth_callback
-                      (fun _ code ->
-                         (compile impl ~use_super_errors:false (Js.to_string code)));
-                    "compile_super_errors",
+                      (fun _ value ->
+                         (Js.bool (set_module_system (Js.to_string value)))
+                      );
+                    "setFilename",
                     inject @@
                     Js.wrap_meth_callback
-                      (fun _ code ->
-                         (compile impl ~use_super_errors:true (Js.to_string code)));
-                    "pretty_print",
+                      (fun _ value ->
+                         (Js.bool (set_filename (Js.to_string value)))
+                      );
+                    "list",
                     inject @@
                     Js.wrap_meth_callback
-                      (fun _ code ->
-                         (match name with
-                          | "reason" -> parse_and_print ~from:Lang.Reason ~to_:Lang.Reason (Js.to_string code)
-                          | "napkin" -> parse_and_print ~from:Lang.Res ~to_:Lang.Res (Js.to_string code)
-                          | n -> ErrorRet.makeUnexpectedError (n ^ " pretty printing not supported")));
-                    "version", Js.Unsafe.inject (Js.string (match name with | "reason" -> Refmt_api.version | _ -> Bs_version.version));
+                      (fun _ ->
+                         (Js.Unsafe.(obj
+                                       [|
+                                         "module_system",
+                                         inject @@ (
+                                           config.module_system
+                                           |> BundleConfig.string_of_module_system
+                                           |> Js.string
+                                         );
+                                       |]))
+                      );
+
                   |]))
 
-let () = make_compiler ~name:"ocaml" ocaml_parse
-let () = make_compiler ~name:"reason" reason_parse
-let () = make_compiler ~name:"napkin" napkin_parse
-
-let () =
-  export "convert"
+  let convert ~(config: BundleConfig.t) = 
+    let open Lang in 
     (Js.Unsafe.(obj
                   [|"reason_to_ocaml",
                     inject @@
                     Js.wrap_meth_callback
                       (fun _ code ->
-                         (parse_and_print ~from:Lang.Reason ~to_:Lang.OCaml (Js.to_string code)));
+                         (parse_and_print ?filename:config.filename ~from:Reason ~to_:OCaml (Js.to_string code)));
                     "ocaml_to_reason",
                     inject @@
                     Js.wrap_meth_callback
                       (fun _ code ->
-                         (parse_and_print ~from:Lang.OCaml ~to_:Lang.Reason (Js.to_string code)));
+                         (parse_and_print ?filename:config.filename ~from:OCaml ~to_:Reason (Js.to_string code)));
                     "reason_to_res",
                     inject @@
                     Js.wrap_meth_callback
                       (fun _ code ->
-                         (parse_and_print ~from:Lang.Reason ~to_:Lang.Res (Js.to_string code)));
+                         (parse_and_print ?filename:config.filename ~from:Reason ~to_:Res (Js.to_string code)));
                     "res_to_reason",
                     inject @@
                     Js.wrap_meth_callback
                       (fun _ code ->
-                         (parse_and_print ~from:Lang.Res ~to_:Lang.Reason (Js.to_string code)));
+                         (parse_and_print ?filename:config.filename ~from:Res ~to_:Reason (Js.to_string code)));
                     "res_to_ocaml",
                     inject @@
                     Js.wrap_meth_callback
                       (fun _ code ->
-                         (parse_and_print ~from:Lang.Res ~to_:Lang.OCaml (Js.to_string code)));
+                         (parse_and_print ?filename:config.filename ~from:Res ~to_:OCaml (Js.to_string code)));
                     "ocaml_to_res",
                     inject @@
                     Js.wrap_meth_callback
                       (fun _ code ->
-                         (parse_and_print ~from:Lang.OCaml ~to_:Lang.Res (Js.to_string code)));
+                         (parse_and_print ?filename:config.filename ~from:OCaml ~to_:Res (Js.to_string code)));
                   |]))
+
+  let make () =
+    let config = BundleConfig.make () in
+    (Js.Unsafe.(obj
+                  [|
+                    "version",
+                    inject @@ Js.string Bs_version.version;
+                    "ocaml",
+                    inject @@ make_compiler ~config ~lang:OCaml;
+                    "reason",
+                    inject @@ make_compiler ~config ~lang:Reason;
+                    "napkin",
+                    inject @@ make_compiler ~config ~lang:Res;
+                    "convert",
+                    inject @@ convert ~config;
+                    "settings",
+                    inject @@ make_settings ~config
+                  |]))
+
+end
+
+let () =
+  let open Lang in
+  export "bs_platform"
+    (Js.Unsafe.(obj
+                  [|
+                    "make",
+                    inject @@ Export.make
+                  |]))
+
+ 
 
 (* local variables: *)
 (* compile-command: "ocamlbuild -use-ocamlfind -pkg compiler-libs -no-hygiene driver.cmo" *)
