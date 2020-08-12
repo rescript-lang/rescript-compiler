3d18dcdf63ee68fe0a67d724a3acc1323decfe3f
Hongbo Zhang <bob.hongbo.zhang@foxmail.com>
2020-08-12 17:30:24 +0800

    tests

--- jscomp/test/external_ppx.js
+++ jscomp/test/external_ppx.js
@@ -2,9 +2,16 @@
 
 var External_ppxGen = require("./external_ppx.gen");
 
+var u = {
+  hi: 2,
+  lo: 3,
+  lo2: {hi:3 }
+};
+
 function f(prim) {
   return External_ppxGen.f(prim);
 }
 
+exports.u = u;
 exports.f = f;
 /* ./external_ppx.gen Not a pure module */
