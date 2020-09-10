458808d7604e1fa34e1b6229e2c10bde8a8d2f82
Hongbo Zhang <bob.hongbo.zhang@foxmail.com>
2020-09-10 18:31:42 +0800

    snapshot

--- lib/4.06.1/bsb_helper.ml
+++ lib/4.06.1/bsb_helper.ml
@@ -2115,7 +2115,7 @@ let rec valid_module_name_aux name off len =
   else 
     let c = String.unsafe_get name off in 
     match c with 
-    | 'A'..'Z' | 'a'..'z' | '0'..'9' | '_' | '\'' -> 
+    | 'A'..'Z' | 'a'..'z' | '0'..'9' | '_' | '\'' | '.' -> 
       valid_module_name_aux name (off + 1) len 
     | _ -> false
 
