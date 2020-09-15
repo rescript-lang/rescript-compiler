0e75aefc4544c6971b30edb0022b1f32708a4093
Hongbo Zhang <bob.hongbo.zhang@foxmail.com>
2020-09-15 14:56:49 +0800

    add test case and fix old test case

---
+++ jscomp/build_tests/weird_names/input.js
@@ -0,0 +1,24 @@
+var cp = require("child_process");
+var assert = require("assert");
+var path = require("path");
+
+var out = cp.spawnSync(`bsb`, { encoding: "utf8" });
+
+console.log(out.stdout);
+assert.ok(out.stderr === "");
+
+let files = [
+  "_app.res",
+  "[...params_max_3].res",
+  "[...params].res",
+  "[[...params]].res",
+  "[slug_or_ID].res",
+  "404.res",
+];
+
+for (let f of files) {
+  let { name } = path.parse(f);
+  let m = `./lib/js/src/${name}.js`;
+  //   console.log(m);
+  assert.deepEqual(require(m), { a: 1 });
+}
