77296ccb8f99b69bc6aadbda370d9be09d3ddd5a
Hongbo Zhang <bob.hongbo.zhang@foxmail.com>
2020-11-07 13:34:33 +0800

    Fix CI

--- jscomp/build_tests/jsxv/input.js
+++ jscomp/build_tests/jsxv/input.js
@@ -6,4 +6,4 @@ var p = require('child_process')
 
 var output = p.spawnSync(`bsb`, {cwd:__dirname, encoding:'utf8',shell:true})
 
-assert.ok(output.stderr.match(/File "bsconfig.json", line 10/))
+assert.ok(output.stderr.match(/File ".*bsconfig.json", line 10/))
