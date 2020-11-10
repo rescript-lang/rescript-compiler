c781b0bcd0999ab997bcf675a6528917ff4a2908
Iwan <mvalcke@hubspot.com>
2020-11-10 07:28:56 +0100

    Add extra tests for printing and parsing of constrained patterns

--- syntax/tests/printer/expr/callback.js
+++ syntax/tests/printer/expr/callback.js
@@ -220,3 +220,14 @@ let f = () => {
     }}
   </div>
 }
+
+myPromise->Js.Promise.then_(value => {
+  Js.log(value)
+  Js.Promise.resolve(value + 2)
+}, _)->Js.Promise.then_(value => {
+  Js.log(value)
+  Js.Promise.resolve(value + 3)
+}, _)->Js.Promise.catch(err => {
+  Js.log2("Failure!!", err)
+  Js.Promise.resolve(-2)
+}, _)
