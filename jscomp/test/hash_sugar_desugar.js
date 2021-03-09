92ba998bac2609b746d63821aef0601eb335151c
Hongbo Zhang <bob.hongbo.zhang@foxmail.com>
2021-03-09 10:55:42 +0800

    simplify x##property#= 3 into x#property#= 3

--- jscomp/test/hash_sugar_desugar.js
+++ jscomp/test/hash_sugar_desugar.js
@@ -19,6 +19,11 @@ function h4(u) {
   return u.hi(1, 2);
 }
 
+function g5(u) {
+  u.hi = 3;
+  
+}
+
 function h5(u) {
   u.hi = 3;
   
@@ -45,6 +50,7 @@ exports.h1 = h1;
 exports.h2 = h2;
 exports.h3 = h3;
 exports.h4 = h4;
+exports.g5 = g5;
 exports.h5 = h5;
 exports.h6 = h6;
 exports.h7 = h7;
--- jscomp/test/hash_sugar_desugar.mli
+++ jscomp/test/hash_sugar_desugar.mli
@@ -4,7 +4,7 @@ val h1 : < p : 'a; .. > -> 'a
 val h2 : < m : (int -> int -> 'a [@bs]); .. > -> 'a
 val h3 : < hi : int -> int -> 'a; .. > -> 'a
 val h4 : < hi : (int -> int -> 'a [@bs.meth]); .. > -> 'a
-
+val g5 : < hi : int [@bs.set]; .. > -> unit
 val h5 : < hi : int [@bs.set]; .. > -> unit
 (* The inferred type is 
 val h5 : < hi#= : (int -> unit [@bs.meth]); .. > -> unit
