906f138bb8e784a8757ed81ef7715c5790a48f9d
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2021-03-18 12:40:20 -0700

    Attribution in `.mli` files
    This change builds on #4998 by adding the same attribution text to the
    `.mli` files.

--- jscomp/core/lam_arity.ml
+++ jscomp/core/lam_arity.ml
@@ -1,5 +1,5 @@
 (* Copyright (C) Hongbo Zhang, Authors of ReScript
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
--- jscomp/depends/bspack_ast_extract.mli
+++ jscomp/depends/bspack_ast_extract.mli
@@ -1,5 +1,5 @@
-(* Copyright (C) 2020 - Authors of ReScript
- * 
+(* Copyright (C) 2020 - Hongbo Zhang, Authors of ReScript
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
