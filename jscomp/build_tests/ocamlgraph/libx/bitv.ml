c7c6778e21cdbe7c4654f4ceea82b49f8a90a8d2
Hongbo Zhang <bob.hongbo.zhang@foxmail.com>
2021-04-01 18:30:59 +0800

    fix CI

--- jscomp/build_tests/ocamlgraph/libx/bitv.ml
+++ jscomp/build_tests/ocamlgraph/libx/bitv.ml
@@ -451,7 +451,7 @@ let to_string v =
   let n = v.length in
   let s = Bytes.make n '0' in
   for i = 0 to n - 1 do
-    if unsafe_get v i then s.[i] <- '1'
+    if unsafe_get v i then Bytes.set s i  '1'
   done;
   Bytes.to_string s
 
