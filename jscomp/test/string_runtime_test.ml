6a1190905894872ea4aa1ef4c74da13785158deb
Hongbo Zhang <bob.hongbo.zhang@foxmail.com>
2021-05-09 10:05:46 +0800

    the unsafe removal fix an out of bounds bug

--- jscomp/test/string_runtime_test.ml
+++ jscomp/test/string_runtime_test.ml
@@ -14,9 +14,9 @@ let suites = Mt.[
   "caml_string_of_bytes", (fun  _ ->
 
     let f len = 
-      let b = Bytes.create 1000 in
-      Bytes.unsafe_fill b 0 len 'c';
-      (B.bytes_to_string b,  (String.init len (fun _ -> 'c'))) in
+      let b = Bytes.create len in
+      Bytes.fill b 0 len 'c';
+      (Bytes.to_string b,  (String.init len (fun _ -> 'c'))) in
     let (a,b) =
       List.split @@ List.map (fun x -> f x ) [1000;1024;1025;4095;4096;5000;10000] in
     Eq(a,b)
