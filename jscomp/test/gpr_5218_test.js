6e8bb55e44e9db4cda1b3cf4ca789728af5b8eec
ZhangHongbo <bob.hongbo.zhang@foxmail.com>
2021-07-26 14:27:44 +0800

    add test case

---
+++ jscomp/test/gpr_5218_test.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var Mt = require("./mt.js");
+
+var test_id = {
+  contents: 0
+};
+
+var suites = {
+  contents: /* [] */0
+};
+
+function test(x) {
+  if (x.NAME === 2) {
+    return {
+            NAME: "2",
+            VAL: x.VAL
+          };
+  } else {
+    return {
+            NAME: "1",
+            VAL: x.VAL
+          };
+  }
+}
+
+Mt.eq_suites(test_id, suites, "File \"gpr_5218_test.res\", line 10, characters 27-34", test({
+          NAME: "1",
+          VAL: 3
+        }), {
+      NAME: "1",
+      VAL: 3
+    });
+
+Mt.eq_suites(test_id, suites, "File \"gpr_5218_test.res\", line 12, characters 27-34", test({
+          NAME: "2",
+          VAL: 3
+        }), {
+      NAME: "2",
+      VAL: 3
+    });
+
+Mt.from_pair_suites("gpr_5218_test.res", suites.contents);
+
+var eq_suites = Mt.eq_suites;
+
+exports.eq_suites = eq_suites;
+exports.test_id = test_id;
+exports.suites = suites;
+exports.test = test;
+/*  Not a pure module */
