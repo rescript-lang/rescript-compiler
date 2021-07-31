54ab76c37e698b3beb7f6e16031317bafd7dd717
ZhangHongbo <bob.hongbo.zhang@foxmail.com>
2021-07-31 10:58:00 +0800

    branching Pexp_record

--- jscomp/ml/typecore.ml
+++ jscomp/ml/typecore.ml
@@ -2709,6 +2709,89 @@ and type_expect_ ?in_function ?(recarg=Rejected) env sexp ty_expected =
           exp_attributes = sexp.pexp_attributes;
           exp_env = env }
       end
+    | Pexp_record(lid_sexp_list, None) ->
+        assert (lid_sexp_list <> []);
+        let ty_record, opath =
+          let get_path ty =
+            try
+              let (p0, p,_) = extract_concrete_record env ty in
+              (* XXX level may be wrong *)
+              Some (p0, p, true) (*ty.level = generic_level || not !Clflags.principal*)
+            with Not_found -> None
+          in
+          match get_path ty_expected with
+            None ->
+               newvar (), None
+          | op -> ty_expected, op
+        in
+        let closed = true in
+        let lbl_exp_list =
+          wrap_disambiguate "This record expression is expected to have" ty_record
+            (type_label_a_list loc closed env
+               (fun e k -> k (type_label_exp true env loc ty_record e))
+               opath lid_sexp_list)
+            (fun x -> x)
+        in
+        unify_exp_types loc env ty_record (instance env ty_expected);
+  
+        (* type_label_a_list returns a list of labels sorted by lbl_pos *)
+        (* note: check_duplicates would better be implemented in
+           type_label_a_list directly *)
+        let rec check_duplicates = function
+          | (_, lbl1, _) :: (_, lbl2, _) :: _ when lbl1.lbl_pos = lbl2.lbl_pos ->
+            raise(Error(loc, env, Label_multiply_defined lbl1.lbl_name))
+          | _ :: rem ->
+              check_duplicates rem
+          | [] -> ()
+        in
+        check_duplicates lbl_exp_list;
+        let  label_definitions =
+          let (_lid, lbl, _lbl_exp) = List.hd lbl_exp_list in
+          let matching_label lbl =
+            List.find
+              (fun (_, lbl',_) -> lbl'.lbl_pos = lbl.lbl_pos)
+              lbl_exp_list
+          in
+          let label_definitions =
+                Array.map (fun lbl ->
+                    match matching_label lbl with
+                    | (lid, _lbl, lbl_exp) ->
+                        Overridden (lid, lbl_exp)
+                    | exception Not_found ->
+                        let present_indices =
+                          List.map (fun (_, lbl, _) -> lbl.lbl_pos) lbl_exp_list
+                        in
+                        let label_names = extract_label_names env ty_expected in
+                        let rec missing_labels n = function
+                            [] -> []
+                          | lbl :: rem ->
+                              if List.mem n present_indices
+                              then missing_labels (n + 1) rem
+                              else lbl :: missing_labels (n + 1) rem
+                        in
+                        let missing = missing_labels 0 label_names in
+                        raise(Error(loc, env, Label_missing missing)))
+                  lbl.lbl_all
+          in
+          label_definitions
+        in
+        let label_descriptions, representation =
+          let (_, { lbl_all; lbl_repres }, _) = List.hd lbl_exp_list in
+          lbl_all, lbl_repres
+        in
+        let fields =
+          Array.map2 (fun descr def -> descr, def)
+            label_descriptions label_definitions
+        in
+        re {
+          exp_desc = Texp_record {
+              fields; representation;
+              extended_expression = None
+            };
+          exp_loc = loc; exp_extra = [];
+          exp_type = instance env ty_expected;
+          exp_attributes = sexp.pexp_attributes;
+          exp_env = env }      
   | Pexp_record(lid_sexp_list, opt_sexp) ->
       assert (lid_sexp_list <> []);
       let opt_exp =
