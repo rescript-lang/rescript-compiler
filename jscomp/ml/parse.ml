3a230f1310a8fc2d2bf344bd9974017e6c02574c
ZhangHongbo <bob.hongbo.zhang@foxmail.com>
2021-09-03 18:38:13 +0800

    remove toplevel handling code

--- jscomp/ml/parse.ml
+++ jscomp/ml/parse.ml
@@ -15,25 +15,6 @@
 
 (* Entry points in the parser *)
 
-(* Skip tokens to the end of the phrase *)
-
-let rec skip_phrase lexbuf =
-  try
-    match Lexer.token lexbuf with
-      Parser.SEMISEMI | Parser.EOF -> ()
-    | _ -> skip_phrase lexbuf
-  with
-    | Lexer.Error (Lexer.Unterminated_comment _, _)
-    | Lexer.Error (Lexer.Unterminated_string, _)
-    | Lexer.Error (Lexer.Unterminated_string_in_comment _, _)
-    | Lexer.Error (Lexer.Illegal_character _, _) -> skip_phrase lexbuf
-;;
-
-let maybe_skip_phrase lexbuf =
-  if Parsing.is_current_lookahead Parser.SEMISEMI
-  || Parsing.is_current_lookahead Parser.EOF
-  then ()
-  else skip_phrase lexbuf
 
 let wrap parsing_fun lexbuf =
   try
@@ -44,18 +25,8 @@ let wrap parsing_fun lexbuf =
     Docstrings.warn_bad_docstrings ();
     ast
   with
-  | Lexer.Error(Lexer.Illegal_character _, _) as err
-    when !Location.input_name = "//toplevel//"->
-      skip_phrase lexbuf;
-      raise err
-  | Syntaxerr.Error _ as err
-    when !Location.input_name = "//toplevel//" ->
-      maybe_skip_phrase lexbuf;
-      raise err
   | Parsing.Parse_error | Syntaxerr.Escape_error ->
       let loc = Location.curr lexbuf in
-      if !Location.input_name = "//toplevel//"
-      then maybe_skip_phrase lexbuf;
       raise(Syntaxerr.Error(Syntaxerr.Other loc))
 
 let implementation = wrap Parser.implementation
