b3a896aedcbf627612fabdf659a35f6bf8ccf51d
ZhangHongbo <bob.hongbo.zhang@foxmail.com>
2021-09-15 21:14:36 +0800

    add exn suffix to indicate that it is gonna raise

--- jscomp/core/lam_beta_reduce_util.ml
+++ jscomp/core/lam_beta_reduce_util.ml
@@ -51,18 +51,18 @@ let param_hash : _ Hash_ident.t = Hash_ident.create 20
 *)
 let simple_beta_reduce params body args =
   let exception Not_simple_apply in
-  let find_param v opt =
+  let find_param_exn v opt =
     match Hash_ident.find_opt param_hash v with
     | Some exp ->
         if exp.used then raise_notrace Not_simple_apply else exp.used <- true;
         exp.lambda
     | None -> opt
   in
-  let rec aux acc (us : Lam.t list) =
+  let rec aux_exn acc (us : Lam.t list) =
     match us with
     | [] -> List.rev acc
-    | (Lvar x as a) :: rest -> aux (find_param x a :: acc) rest
-    | (Lconst _ as u) :: rest -> aux (u :: acc) rest
+    | (Lvar x as a) :: rest -> aux_exn (find_param_exn x a :: acc) rest
+    | (Lconst _ as u) :: rest -> aux_exn (u :: acc) rest
     | _ :: _ -> raise_notrace Not_simple_apply
   in
   match (body : Lam.t) with
@@ -74,7 +74,7 @@ let simple_beta_reduce params body args =
           (fun p a -> Hash_ident.add param_hash p { lambda = a; used = false })
           params args
       in
-      match aux [] ap_args with
+      match aux_exn [] ap_args with
       | new_args ->
           let result =
             Hash_ident.fold param_hash
@@ -105,10 +105,10 @@ let simple_beta_reduce params body args =
         if it is removed twice there will be exception.
         if it is never removed, we have it as rest keys
       *)
-      match aux [] ap_args with
+      match aux_exn [] ap_args with
       | new_args ->
           let f =
-            match f with Lvar fn_name -> find_param fn_name f | _ -> f
+            match f with Lvar fn_name -> find_param_exn fn_name f | _ -> f
           in
           let result =
             Hash_ident.fold param_hash (Lam.apply f new_args ap_info)
