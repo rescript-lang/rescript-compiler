10f95a4ee4f716b82cf1c0c3f325a73000a8058b
ZhangHongbo <bob.hongbo.zhang@foxmail.com>
2021-09-15 14:13:49 +0800

    add a missed test code
    # Conflicts:
    #	jscomp/test/build.ninja

---
+++ jscomp/test/gpr_5280_optimize_test.js
@@ -0,0 +1,19 @@
+'use strict';
+
+
+var a = /* Color */{
+  _0: "#ffff"
+};
+
+var c;
+
+if (a) {
+  var s = "#ffff";
+  c = s === "#ffff" ? "white" : s;
+} else {
+  c = "orange";
+}
+
+exports.a = a;
+exports.c = c;
+/* c Not a pure module */
