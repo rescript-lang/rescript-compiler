b3861573bd5ae69277e72925ea87695bf1eeabb2
ZhangHongbo <bob.hongbo.zhang@foxmail.com>
2021-09-22 11:46:06 +0800

    add a WIP script for respack separation

---
+++ scripts/deps.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+//@ts-check
+
+var fs = require("fs");
+var path = require("path");
+var content = fs.readFileSync(
+  path.join(__dirname, "..", "lib", "4.06.1", "unstable", "bspack.ml.d"),
+  "utf8"
+);
+
+var files = content
+  .split("\n")
+  .filter(Boolean)
+  .map((x) =>
+    x
+      .split(":")
+      .map((x) => x.trim())
+      .filter(Boolean)
+      .at(-1)
+  );
+
+for (let file of files) {
+  let base = path.parse(file).base;
+  let src = path.join(__dirname, "..", "jscomp", file);
+  let dest = path.join(__dirname, "..", "..", "rescript-pack", base);
+  console.log(`copy ${src} -> ${dest}`);
+  fs.copyFileSync(src, dest);
+}
