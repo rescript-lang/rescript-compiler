8e6d82df9c88c4f3553a2c3605f2bc715961094a
ZhangHongbo <bob.hongbo.zhang@foxmail.com>
2021-10-08 14:17:26 +0800

    tweak

--- jscomp/common/js_config.ml
+++ jscomp/common/js_config.ml
@@ -89,8 +89,8 @@ let no_export = ref false
 let as_ppx = ref false
 
 
-
-let customize_runtime = ref None 
+(* option to config `@rescript/std`*)
+let customize_runtime : string option ref = ref None 
 
 let as_pp = ref false
 
