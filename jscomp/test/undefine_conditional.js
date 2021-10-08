7ab50ae345d48b18983a527a2a807e8109b652d4
ZhangHongbo <bob.hongbo.zhang@foxmail.com>
2021-10-08 11:09:01 +0800

    adapt test case

---
+++ jscomp/test/undefine_conditional.js
@@ -0,0 +1,10 @@
+'use strict';
+
+
+var x = 3;
+
+var y = 6;
+
+exports.x = x;
+exports.y = y;
+/* No side effect */
---
+++ jscomp/test/undefine_conditional.ml
@@ -0,0 +1,15 @@
+
+
+#ifndef RELEASE
+let x = 3
+#else 
+let x = 4   
+#endif 
+
+#ifndef OCAML_VERSION
+let y = 5
+
+#else 
+let y = 6
+
+#endif 
