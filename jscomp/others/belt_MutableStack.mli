d43bf159193ca77d39dfcea259085901333f7570
Patrick Ecker <ryyppy@users.noreply.github.com>
2022-06-14 20:37:50 +0200

    Sync docs for belt_MutableStack.mli

--- jscomp/others/belt_MutableStack.mli
+++ jscomp/others/belt_MutableStack.mli
@@ -22,46 +22,42 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *)
 
-(** First in last out stack.
-
-    This module implements stacks, with in-place modification.
+(**
+First in last out stack. This module implements stacks, with in-place
+modification.
 *)
 
 type 'a t
 
-
-val make: unit -> 'a t
-(** **return** a new stack, initially empty. *)
-
-val clear: 'a t -> unit
-(** Discard all elements from the stack. *)
-
-val copy : 'a t -> 'a t
-(** `copy x` O(1) operation, return a new stack  *)
-
-val push : 'a t -> 'a -> unit
-
-val popUndefined : 'a t -> 'a Js.undefined
-
-val pop : 'a t -> 'a option 
-
-val topUndefined : 'a t -> 'a Js.undefined
-
-val top : 'a t -> 'a option 
-
-val isEmpty : 'a t -> bool
-
-val size : 'a t -> int
-
-val forEachU : 'a t -> ('a -> unit [@bs] ) -> unit
-val forEach : 'a t -> ('a -> unit ) -> unit
-
-
-val dynamicPopIterU : 'a t -> ('a ->  unit [@bs]) -> unit
-val dynamicPopIter : 'a t -> ('a ->  unit ) -> unit   
-(** `dynamicPopIter s f `
-    apply `f` to each element of `s`. The item is poped 
-    before applying `f`, `s` will be empty  after this opeartion.
-    This function is useful for worklist algorithm
+val make : unit -> 'a t
+(**
+  Returns a new stack, initially empty.
 *)
 
+val clear : 'a t -> unit
+(**
+  Discard all elements from the stack.
+*)
+
+val copy : 'a t -> 'a t
+(**
+  `copy(x)` O(1) operation, return a new stack.
+*)
+
+val push : 'a t -> 'a -> unit
+val popUndefined : 'a t -> 'a Js.undefined
+val pop : 'a t -> 'a option
+val topUndefined : 'a t -> 'a Js.undefined
+val top : 'a t -> 'a option
+val isEmpty : 'a t -> bool
+val size : 'a t -> int
+val forEachU : 'a t -> (('a -> unit)[@bs]) -> unit
+val forEach : 'a t -> ('a -> unit) -> unit
+val dynamicPopIterU : 'a t -> (('a -> unit)[@bs]) -> unit
+
+val dynamicPopIter : 'a t -> ('a -> unit) -> unit
+(**
+  `dynamicPopIter(s, f)` apply `f` to each element of `s`. The item is poped
+  before applying `f`, `s` will be empty after this opeartion. This function is
+  useful for worklist algorithm.
+ *)
