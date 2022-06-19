eea6b5a15d9d659c69f04764e8f619a5eff010a5
Cristiano Calcagno <cristianoc@users.noreply.github.com>
2022-06-20 01:54:05 +0200

    An alternative solution that requires fewer changes.

--- jscomp/core/lam_compile_const.ml
+++ jscomp/core/lam_compile_const.ml
@@ -101,5 +101,7 @@ and translate (x : Lam_constant.t) : J.expression =
 let translate_arg_cst (cst : External_arg_spec.cst) =
   match cst with
   | Arg_int_lit i -> E.int (Int32.of_int i)
-  | Arg_string_lit i -> E.str ~escape:false i
+  | Arg_string_lit i ->
+      let s = "\"" ^ i ^ "\"" in
+      E.raw_js_code (Exp (Js_literal { comment = None })) s
   | Arg_js_literal s -> E.raw_js_code (Exp (Js_literal { comment = None })) s
