1ffb89e2359976564e2e504f8b224aaf3aef9039
Cristiano Calcagno <cristianoc@users.noreply.github.com>
2022-06-20 06:36:35 +0200

    Clean test case.
    So no git changes are left.

--- jscomp/build_tests/bs_dependencies_node_path_override/testcase.js
+++ jscomp/build_tests/bs_dependencies_node_path_override/testcase.js
@@ -2,6 +2,9 @@
 var assert = require('assert')
 var path = require('path')
 var p = require('child_process')
-p.execSync(`../node_modules/.bin/rescript`, {cwd:__dirname,shell:true,encoding:'utf8',stdio:"inherit"})
+var fs = require('fs')
+p.execSync(`../node_modules/.bin/rescript`, { cwd: __dirname, shell: true, encoding: 'utf8', stdio: "inherit" })
+fs.rmSync('overridden_node_modules/liba/lib', {recursive:true, force:true})
+
 var u = require("./examples/test.js")
-assert.equal(path.basename(u.v),'demo.mldemo.ml')
+assert.equal(path.basename(u.v), 'demo.mldemo.ml')
