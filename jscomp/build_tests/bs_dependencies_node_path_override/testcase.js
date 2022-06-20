924720292113e49a35384d55a353d5c36822e93d
Cristiano Calcagno <cristianoc@users.noreply.github.com>
2022-06-20 07:01:09 +0200

    Tweak test.

--- jscomp/build_tests/bs_dependencies_node_path_override/testcase.js
+++ jscomp/build_tests/bs_dependencies_node_path_override/testcase.js
@@ -3,8 +3,15 @@ var assert = require('assert')
 var path = require('path')
 var p = require('child_process')
 var fs = require('fs')
-p.execSync(`../node_modules/.bin/rescript`, { cwd: __dirname, shell: true, encoding: 'utf8', stdio: "inherit" })
-fs.rmSync('overridden_node_modules/liba/lib', {recursive:true, force:true})
+var output = p.spawnSync(
+    `../node_modules/.bin/rescript clean -with-deps && ../node_modules/.bin/rescript build`,
+    {
+        cwd: __dirname,
+        encoding: "utf8",
+        stdio: ["pipe", "pipe", "pipe"],
+        shell: true,
+    })
+fs.rmSync('overridden_node_modules/liba/lib', { recursive: true, force: true })
 
 var u = require("./examples/test.js")
 assert.equal(path.basename(u.v), 'demo.mldemo.ml')
