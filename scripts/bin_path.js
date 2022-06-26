0ef1981f2895d587236feb5e45c7c2b143466a2d
Javier Chávarri <javier.chavarri@gmail.com>
2022-06-26 21:13:11 +0200

    Fix win build (#5473)
    * fix win build
    
    * naming

--- scripts/bin_path.js
+++ scripts/bin_path.js
@@ -3,33 +3,36 @@
 var path = require("path");
 
 /**
+ * @type{string}
+ */
+var binDirName =
+  process.platform === "darwin" && process.arch === "arm64"
+    ? process.platform + process.arch
+    : process.platform;
+
+/**
  *
  * @type{string}
  */
-var bin_path = path.join(
-  __dirname,
-  "..",
-  process.platform === "darwin" && process.arch === "arm64"
-    ? process.platform + process.arch
-    : process.platform
-);
+var binAbsolutePath = path.join(__dirname, "..", binDirName);
 
 /**
  * @type{string}
  */
-var bsc_exe = path.join(bin_path, "bsc.exe");
+var bsc_exe = path.join(binAbsolutePath, "bsc.exe");
 
 /**
  * @type{string}
  */
-var ninja_exe = path.join(bin_path, "ninja.exe");
+var ninja_exe = path.join(binAbsolutePath, "ninja.exe");
 
 /**
  * @type{string}
  */
-var rescript_exe = path.join(bin_path, "rescript.exe");
+var rescript_exe = path.join(binAbsolutePath, "rescript.exe");
 
-exports.folder = bin_path;
+exports.dirName = binDirName;
+exports.absolutePath = binAbsolutePath;
 exports.bsc_exe = bsc_exe;
 exports.ninja_exe = ninja_exe;
 exports.rescript_exe = rescript_exe;
--- scripts/ninjaFactory.js
+++ scripts/ninjaFactory.js
@@ -6,7 +6,7 @@
 // # build bsdep.exe: cc bsdep.mli bsdep.ml
 // ../native/4.06.1/bin/ocamlopt.opt -c -O2 -nodynlink -I 4.06.1 -g -w a+32  4.06.1/whole_compiler.mli 4.06.1/whole_compiler.ml  &> warning.log
 
-var targetDir = require("./bin_path").folder;
+var targetDir = require("./bin_path").dirName;
 
 /**
  *
@@ -24,11 +24,11 @@ rule cc
       config.isWin ? "" : "&& strip $out"
     }
     description = Making $out
-build ${targetDir}/rescript$ext:  cc $INCL/rescript.mli $INCL/rescript.ml
+build ../${targetDir}/rescript$ext:  cc $INCL/rescript.mli $INCL/rescript.ml
     flags = $flags -unboxed-types unix.cmxa str.cmxa    
-build ${targetDir}/bsb_helper$ext:  cc $INCL/bsb_helper.mli $INCL/bsb_helper.ml
+build ../${targetDir}/bsb_helper$ext:  cc $INCL/bsb_helper.mli $INCL/bsb_helper.ml
     flags = $flags  -unboxed-types -w -a
-build ${targetDir}/bsc$ext: cc $INCL/whole_compiler.mli $INCL/whole_compiler.ml
+build ../${targetDir}/bsc$ext: cc $INCL/whole_compiler.mli $INCL/whole_compiler.ml
     flags = $flags -w A-4-9-48-40-45-41-44-50-21-30-32-34-37-27-60-42 
 `;
 }
