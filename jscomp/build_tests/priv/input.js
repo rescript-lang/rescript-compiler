08c03847d5484be878c5b9de98c583d88f01f695
Cristiano Calcagno <cristianoc@users.noreply.github.com>
2022-06-26 22:14:32 +0200

    clean up

--- jscomp/build_tests/priv/input.js
+++ jscomp/build_tests/priv/input.js
@@ -2,7 +2,7 @@ var child_process = require("child_process");
 var rescript_exe = require("../../../scripts/bin_path").rescript_exe;
 
 var output = child_process.spawnSync(
-  `${rescript_exe} -with-deps && ${rescript_exe} build`,
+  `${rescript_exe} clean -with-deps && ${rescript_exe} build`,
   { cwd: __dirname, shell: true, encoding: "utf8" }
 );
 
