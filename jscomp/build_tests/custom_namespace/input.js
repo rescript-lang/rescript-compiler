8a2185eaae90dbd652e51c115e43e0e48251517e
Javier Chávarri <javier.chavarri@gmail.com>
2022-06-26 22:35:49 +0200

    tests: use rescript_exe in custom_namespace test

--- jscomp/build_tests/custom_namespace/input.js
+++ jscomp/build_tests/custom_namespace/input.js
@@ -2,8 +2,9 @@ var child_process = require("child_process");
 var fs = require("fs");
 var path = require("path");
 var assert = require("assert");
+var rescript_exe = require("../../../scripts/bin_path").rescript_exe;
 
-child_process.execSync(`../node_modules/.bin/rescript clean -with-deps && ../node_modules/.bin/rescript build`, {
+child_process.execSync(`${rescript_exe} clean -with-deps && ${rescript_exe} build`, {
   cwd: __dirname,
   stdio: [0, 1, 2],
 });
