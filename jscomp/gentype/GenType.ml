5b4f3226c4ef2c6d7bc8c5e16f6e894d328f68a8
Cristiano Calcagno <cristianoc@users.noreply.github.com>
2022-07-17 03:52:42 +0200

    Remove unused CLI commands.

--- jscomp/gentype/GenType.ml
+++ jscomp/gentype/GenType.ml
@@ -1,6 +1,6 @@
 let version = Version.version
 
-type cliCommand = Add of string | Clean | NoOp | Rm of string list
+type cliCommand = Add of string | NoOp
 
 let executeCliCommand ~printUsageAndExit cliCommand =
   match cliCommand with
@@ -18,44 +18,7 @@ let executeCliCommand ~printUsageAndExit cliCommand =
       if !Debug.basic then Log_.item "Add %s  %s\n" cmt mlast;
       cmt |> GenTypeMain.processCmtFile ~config;
       exit 0
-  | Clean ->
-      let config = Paths.readConfig ~namespace:None in
-      let sourceDirs = ModuleResolver.readSourceDirs ~config in
-      if !Debug.basic then
-        Log_.item "Clean %d dirs\n" (sourceDirs.dirs |> List.length);
-      let count = ref 0 in
-      sourceDirs.dirs
-      |> List.iter (fun dir ->
-             let files = Sys.readdir dir in
-             files
-             |> Array.iter (fun file ->
-                    if
-                      Filename.check_suffix file ".re"
-                      || Filename.check_suffix file ".res"
-                    then
-                      let extension = EmitType.outputFileSuffix ~config in
-                      let generated =
-                        Filename.concat dir
-                          ((file |> Filename.chop_extension) ^ extension)
-                      in
-                      if Sys.file_exists generated then (
-                        Unix.unlink generated;
-                        incr count)));
-      if !Debug.basic then Log_.item "Cleaned %d files\n" !count;
-      exit 0
   | NoOp -> printUsageAndExit ()
-  | Rm l ->
-      let removeOne s =
-        let cmtAbsolutePath = s in
-        (* somehow the CMT hook is passing an absolute path here *)
-        let cmt = cmtAbsolutePath |> Paths.relativePathFromBsLib in
-        let config = Paths.readConfig ~namespace:(cmt |> Paths.findNameSpace) in
-        let outputFile = cmt |> Paths.getOutputFile ~config in
-        if !Debug.basic then Log_.item "Remove %s\n" cmt;
-        if Sys.file_exists outputFile then Unix.unlink outputFile
-      in
-      l |> List.rev |> List.iter removeOne;
-      exit 0
 
 let cli () =
   let cliCommand = ref NoOp in
@@ -71,23 +34,14 @@ let cli () =
     if !cliCommand <> NoOp then printUsageAndExit ();
     cliCommand := command
   and setAdd s = Add s |> setCliCommand
-  and setRm s = Rm s |> setCliCommand
-  and setClean () = Clean |> setCliCommand
   and speclist =
     [
-      ("-clean", Arg.Unit setClean, "clean all the generated files");
       ("-cmt-add", Arg.String setAdd, "compile a .cmt[i] file");
-      ( "-cmt-rm",
-        Arg.String (fun s -> setRm [ s ]),
-        "remove one or more .cmt[i] files" );
       ("-version", Arg.Unit versionAndExit, "show version information and exit");
       ("--version", Arg.Unit versionAndExit, "show version information and exit");
     ]
   in
-  let anonArg s =
-    match !cliCommand with
-    | Rm l -> cliCommand := Rm (s :: l)
-    | _ -> print_endline s
+  let anonArg s = print_endline s
   in
   Arg.parse speclist anonArg usage;
   executeCliCommand ~printUsageAndExit !cliCommand
