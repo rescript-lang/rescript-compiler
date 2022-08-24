6de8e49f02185b5abd54803cfa781df20d3a85bf
Cristiano Calcagno <cristianoc@users.noreply.github.com>
2022-08-24 19:36:34 +0200

    Slim down genType test.
    Remove heavy dependencies.

--- jscomp/gentype_tests/typescript-react-example/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './App';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App name={"e"} />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
