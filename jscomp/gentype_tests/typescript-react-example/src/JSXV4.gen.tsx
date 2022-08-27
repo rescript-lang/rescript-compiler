a8ab0d6739d3634f1e93885e6ca2082bebbd3594
Cristiano Calcagno <cristianoc@users.noreply.github.com>
2022-08-27 07:32:43 +0200

    Add example of JSX V4 with genType.
    No special treatment so far, just a normal function as if it were written by hand.

---
+++ jscomp/gentype_tests/typescript-react-example/src/JSXV4.gen.tsx
@@ -0,0 +1,16 @@
+/* TypeScript file generated from JSXV4.res by genType. */
+/* eslint-disable import/first */
+
+
+// @ts-ignore: Implicit any on import
+import * as JSXV4BS__Es6Import from './JSXV4.bs';
+const JSXV4BS: any = JSXV4BS__Es6Import;
+
+// tslint:disable-next-line:interface-over-type-literal
+export type props<x,y> = {
+  readonly key?: string; 
+  readonly x: x; 
+  readonly y: y
+};
+
+export const make: (_1:props<string,string>) => JSX.Element = JSXV4BS.make;
---
+++ jscomp/gentype_tests/typescript-react-example/src/JSXV4.res
@@ -0,0 +1,4 @@
+@@jsxConfig({version: 4})
+
+@genType @react.component
+let make = (~x, ~y) => React.string(x ++ y)
