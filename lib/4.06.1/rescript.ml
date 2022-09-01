81c7caf3f81290fe905c56a602f1598527b4b7ff
Victor Nakoryakov <nail.xx@gmail.com>
2022-09-01 12:45:45 +0300

    Add extra variants for suffixes in bsconfig.json
    - `.bs.mjs`, `.bs.cjs` to fill the gap between `.bs.js` and `.mjs`
    - `.res.js`, `.res.mjs`, `.res.cjs` to assist BS → ReScript rebranding

--- lib/4.06.1/rescript.ml
+++ lib/4.06.1/rescript.ml
@@ -7098,22 +7098,42 @@ end
 module Ext_js_suffix
 = struct
 #1 "ext_js_suffix.ml"
-type t = Js | Bs_js | Mjs | Cjs | Unknown_extension
+type t =
+  | Js
+  | Bs_js
+  | Res_js
+  | Mjs
+  | Bs_mjs
+  | Res_mjs
+  | Cjs
+  | Bs_cjs
+  | Res_cjs
+  | Unknown_extension
 
 let to_string (x : t) =
   match x with
   | Js -> Literals.suffix_js
   | Bs_js -> Literals.suffix_bs_js
+  | Res_js -> Literals.suffix_res_js
   | Mjs -> Literals.suffix_mjs
+  | Bs_mjs -> Literals.suffix_bs_mjs
+  | Res_mjs -> Literals.suffix_res_mjs
   | Cjs -> Literals.suffix_cjs
+  | Bs_cjs -> Literals.suffix_bs_cjs
+  | Res_cjs -> Literals.suffix_res_cjs
   | Unknown_extension -> assert false
 
 let of_string (x : string) : t =
   match () with
   | () when x = Literals.suffix_js -> Js
   | () when x = Literals.suffix_bs_js -> Bs_js
+  | () when x = Literals.suffix_res_js -> Res_js
   | () when x = Literals.suffix_mjs -> Mjs
+  | () when x = Literals.suffix_bs_mjs -> Bs_mjs
+  | () when x = Literals.suffix_res_mjs -> Res_mjs
   | () when x = Literals.suffix_cjs -> Cjs
+  | () when x = Literals.suffix_bs_cjs -> Bs_cjs
+  | () when x = Literals.suffix_res_cjs -> Res_cjs
   | _ -> Unknown_extension
 
 end
@@ -7853,7 +7873,8 @@ let extract_bs_suffix_exn (map : json_map) : Ext_js_suffix.t =
   | Some (Str { str; loc }) ->
       let s = Ext_js_suffix.of_string str in
       if s = Unknown_extension then
-        Bsb_exception.errorf ~loc "expect .bs.js, .js, .cjs, .mjs here"
+        Bsb_exception.errorf ~loc
+          "expect .js, .bs.js, .res.js, .mjs, .bs.mjs, .res.mjs, .cjs, .bs.cjs, .res.cjs here"
       else s
   | Some config ->
       Bsb_exception.config_error config
