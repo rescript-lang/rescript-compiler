fde32bdb9ab521548e90758fee409fddb7877745
Cristiano Calcagno <cristianoc@users.noreply.github.com>
2022-09-16 14:16:25 +0200

    tweak example

--- jscomp/test/TestOption.js
+++ jscomp/test/TestOption.js
@@ -2,11 +2,22 @@
 
 
 function test1(r) {
-  var x = r.f;
-  if (x !== undefined) {
-    return x.f;
+  var x = r.fo;
+  var x$1 = (
+    x !== undefined ? x.f : undefined
+  ).f.f;
+  var match = x$1.fo;
+  var x$2 = r.fo;
+  var x$3 = (
+    x$2 !== undefined ? x$2.f : undefined
+  ).f.f;
+  var x$4 = x$3.fo;
+  var match$1 = x$4 !== undefined ? x$4.n : undefined;
+  if (match !== undefined) {
+    return match.n + match$1 | 0;
+  } else {
+    return 0;
   }
-  
 }
 
 exports.test1 = test1;
--- jscomp/test/TestOption.res
+++ jscomp/test/TestOption.res
@@ -1,3 +1,7 @@
-type rec r = {n: int, f: option<r>}
+type rec r = {n: int, f: r, fo: option<r>}
 
-let test1 = (r:r) => r.f.f
+let test1 = (r: r) =>
+  switch (r.fo.f.f.f.fo, r.fo.f.f.f.fo.n) {
+  | (Some({n}), Some(n1)) => n + n1
+  | _ => 0
+  }
