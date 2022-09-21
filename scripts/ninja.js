d87d78b99e49397b739dda26c957850c37f884b5
Christoph Knittel <ck@cca.io>
2022-09-21 16:47:32 +0200

    Remove refmt.exe

--- scripts/ninja.js
+++ scripts/ninja.js
@@ -1452,7 +1452,7 @@ function setSortedToStringAsNativeDeps(xs) {
 }
 
 /**
- * Built cppo.exe refmt.exe etc for dev purpose
+ * Built cppo.exe for dev purpose
  */
 function preprocessorNinjaSync() {
   var napkinFiles = fs
