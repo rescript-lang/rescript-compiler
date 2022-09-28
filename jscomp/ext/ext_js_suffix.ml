6cacc02c66fd0fecc8575626c0e6920e709e039b
Victor Nakoryakov <nail.xx@gmail.com>
2022-09-28 20:06:03 +0300

    Step back and revoke `.res.*` suffix options

--- jscomp/ext/ext_js_suffix.ml
+++ jscomp/ext/ext_js_suffix.ml
@@ -1,37 +1,28 @@
 type t =
   | Js
   | Bs_js
-  | Res_js
   | Mjs
   | Bs_mjs
-  | Res_mjs
   | Cjs
   | Bs_cjs
-  | Res_cjs
   | Unknown_extension
 
 let to_string (x : t) =
   match x with
   | Js -> Literals.suffix_js
   | Bs_js -> Literals.suffix_bs_js
-  | Res_js -> Literals.suffix_res_js
   | Mjs -> Literals.suffix_mjs
   | Bs_mjs -> Literals.suffix_bs_mjs
-  | Res_mjs -> Literals.suffix_res_mjs
   | Cjs -> Literals.suffix_cjs
   | Bs_cjs -> Literals.suffix_bs_cjs
-  | Res_cjs -> Literals.suffix_res_cjs
   | Unknown_extension -> assert false
 
 let of_string (x : string) : t =
   match () with
   | () when x = Literals.suffix_js -> Js
   | () when x = Literals.suffix_bs_js -> Bs_js
-  | () when x = Literals.suffix_res_js -> Res_js
   | () when x = Literals.suffix_mjs -> Mjs
   | () when x = Literals.suffix_bs_mjs -> Bs_mjs
-  | () when x = Literals.suffix_res_mjs -> Res_mjs
   | () when x = Literals.suffix_cjs -> Cjs
   | () when x = Literals.suffix_bs_cjs -> Bs_cjs
-  | () when x = Literals.suffix_res_cjs -> Res_cjs
   | _ -> Unknown_extension
--- lib/4.06.1/unstable/all_ounit_tests.ml
+++ lib/4.06.1/unstable/all_ounit_tests.ml
@@ -5802,19 +5802,13 @@ let suffix_js = ".js"
 
 let suffix_bs_js = ".bs.js"
 
-let suffix_res_js = ".res.js"
-
 let suffix_mjs = ".mjs"
 
 let suffix_bs_mjs = ".bs.mjs"
 
-let suffix_res_mjs = ".res.mjs"
-
 let suffix_cjs = ".cjs"
 
 let suffix_bs_cjs = ".bs.cjs"
-
-let suffix_res_cjs = ".res.cjs"
 
 let suffix_gen_js = ".gen.js"
 
@@ -38357,39 +38351,30 @@ module Ext_js_suffix
 type t =
   | Js
   | Bs_js
-  | Res_js
   | Mjs
   | Bs_mjs
-  | Res_mjs
   | Cjs
   | Bs_cjs
-  | Res_cjs
   | Unknown_extension
 
 let to_string (x : t) =
   match x with
   | Js -> Literals.suffix_js
   | Bs_js -> Literals.suffix_bs_js
-  | Res_js -> Literals.suffix_res_js
   | Mjs -> Literals.suffix_mjs
   | Bs_mjs -> Literals.suffix_bs_mjs
-  | Res_mjs -> Literals.suffix_res_mjs
   | Cjs -> Literals.suffix_cjs
   | Bs_cjs -> Literals.suffix_bs_cjs
-  | Res_cjs -> Literals.suffix_res_cjs
   | Unknown_extension -> assert false
 
 let of_string (x : string) : t =
   match () with
   | () when x = Literals.suffix_js -> Js
   | () when x = Literals.suffix_bs_js -> Bs_js
-  | () when x = Literals.suffix_res_js -> Res_js
   | () when x = Literals.suffix_mjs -> Mjs
   | () when x = Literals.suffix_bs_mjs -> Bs_mjs
-  | () when x = Literals.suffix_res_mjs -> Res_mjs
   | () when x = Literals.suffix_cjs -> Cjs
   | () when x = Literals.suffix_bs_cjs -> Bs_cjs
-  | () when x = Literals.suffix_res_cjs -> Res_cjs
   | _ -> Unknown_extension
 
 end
