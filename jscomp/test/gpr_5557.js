2a543a9725fd20fbffdb2aeedd05dd66b8816e1a
HongboZhang <bob.hongbo.zhang@foxmail.com>
2022-10-24 10:31:42 +0800

    attempt to fix #5557 #5743

---
+++ jscomp/test/gpr_5557.js
@@ -0,0 +1,12 @@
+'use strict';
+
+
+function isA(c) {
+  return true;
+}
+
+var h = /* 'a' */97;
+
+exports.isA = isA;
+exports.h = h;
+/* No side effect */
---
+++ jscomp/test/gpr_5557.res
@@ -0,0 +1,6 @@
+let isA = c =>
+  switch c {
+  | 'a' => true
+  }
+
+let h : int = ('a' :> int)  
