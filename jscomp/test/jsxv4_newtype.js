bf8f1a33bf79269788cbb78aaa65ea23137ae4ff
Cristiano Calcagno <cristianoc@users.noreply.github.com>
2023-03-03 15:11:30 +0100

    Fix issue with JSX V4 and newtype
    Make the following changes:
    1) Don't replace type variable `a` with `"type-a`",  but leave it alone.
    2) If the original `make` uses `(type a b, ...` wrap the first generated `make` with the same new type definitions
    3) In the constraint `:props<'a, 'b>` where `'a` and `'b` were not constrained, use `:props<_, _>` instead. This avoids issues when e.g. `'a` is inferred to be a newtype bound in the make function, which would need to be mentioned explicitly and would give a type error.
    
    Fixes https://github.com/rescript-lang/rescript-compiler/issues/5978
    Replaces https://github.com/rescript-lang/rescript-compiler/pull/6000

---
+++ jscomp/test/jsxv4_newtype.js
@@ -0,0 +1,40 @@
+'use strict';
+
+
+function Jsxv4_newtype$V4A(props) {
+  return null;
+}
+
+var V4A = {
+  make: Jsxv4_newtype$V4A
+};
+
+function Jsxv4_newtype$V4A1(props) {
+  return null;
+}
+
+var V4A1 = {
+  make: Jsxv4_newtype$V4A1
+};
+
+function Jsxv4_newtype$V4A2(props) {
+  return null;
+}
+
+var V4A2 = {
+  make: Jsxv4_newtype$V4A2
+};
+
+function Jsxv4_newtype$V4A3(props) {
+  return props.foo;
+}
+
+var V4A3 = {
+  make: Jsxv4_newtype$V4A3
+};
+
+exports.V4A = V4A;
+exports.V4A1 = V4A1;
+exports.V4A2 = V4A2;
+exports.V4A3 = V4A3;
+/* No side effect */
--- res_syntax/tests/ppx/react/newtype.res
+++ res_syntax/tests/ppx/react/newtype.res
@@ -18,3 +18,28 @@ module V4A = {
   @react.component
   let make = (type a, ~a: a, ~b: array<option<[#Foo(a)]>>, ~c: 'a, _) => <div />
 }
+
+module V4A1 = {
+  @react.component
+  let make = (type x y, ~a:x, ~b:array<y>, ~c:'a) => <div />
+}
+
+module type T = {
+  type t
+}
+
+module V4A2 = {
+  @react.component
+  let make = (type a, ~foo: module(T with type t = a)) => {
+    module T = unpack(foo)
+    <div />
+  }
+}
+
+module V4A3 = {
+  @react.component
+  let make = (type a, ~foo) => {
+    module T = unpack(foo: T with type t = a)
+    foo
+  }
+}
