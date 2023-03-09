a85adb15a5e29ceea912770400951fd081c8a7c0
Cristiano Calcagno <cristianoc@users.noreply.github.com>
2023-03-09 11:44:36 +0100

    Try to determine uncurried-always mode in the formatter.
    Project config is not used in the formatter normally.
    This attempts a best-effort way to find bsconfig.json and see if the mode is set.
    The editor currently calls this on a temp file, so it tries to find bsconfig from the path of `bsc.exe` used, by looking outside node_modules.
    A more robust method should be provided later on, but this should be OK to start experimenting.

--- res_syntax/src/res_multi_printer.ml
+++ res_syntax/src/res_multi_printer.ml
@@ -1,7 +1,55 @@
 let defaultPrintWidth = 100
 
+(* Determine if the file is in uncurried-always mode by looking for
+   the fist ancestor .bsconfig and see if it contains "uncurry": "default" *)
+let getUncurriedAlwaysFromBsconfig ~filename =
+  let rec findBsconfig ~dir =
+    let bsconfig = Filename.concat dir "bsconfig.json" in
+    if Sys.file_exists bsconfig then Some (Res_io.readFile ~filename:bsconfig)
+    else
+      let parent = Filename.dirname dir in
+      if parent = dir then None else findBsconfig ~dir:parent
+  in
+  let rec findFromNodeModules ~dir =
+    let parent = Filename.dirname dir in
+    if Filename.basename dir = "node_modules" then
+      let bsconfig = Filename.concat parent "bsconfig.json" in
+      if Sys.file_exists bsconfig then Some (Res_io.readFile ~filename:bsconfig)
+      else None
+    else if parent = dir then None
+    else findFromNodeModules ~dir:parent
+  in
+  let dir =
+    if Filename.is_relative filename then
+      Filename.dirname (Filename.concat (Sys.getcwd ()) filename)
+    else Filename.dirname filename
+  in
+  let bsconfig () =
+    match findBsconfig ~dir with
+    | None ->
+      (* The editor calls format on a temporary file. So bsconfig can't be found.
+         This looks outside the node_modules containing the bsc binary *)
+      let dir = Filename.dirname Sys.argv.(0) in
+      findFromNodeModules ~dir
+    | x -> x
+  in
+  match bsconfig () with
+  | exception _ -> ()
+  | None -> ()
+  | Some bsconfig ->
+    let lines = bsconfig |> String.split_on_char '\n' in
+    let uncurriedAlways =
+      lines
+      |> List.exists (fun line ->
+             let words = line |> String.split_on_char '\"' in
+             words |> List.exists (fun word -> word = "uncurried")
+             && words |> List.exists (fun word -> word = "always"))
+    in
+    if uncurriedAlways then Res_uncurried.init := Always
+
 (* print res files to res syntax *)
 let printRes ~ignoreParseErrors ~isInterface ~filename =
+  getUncurriedAlwaysFromBsconfig ~filename;
   if isInterface then (
     let parseResult =
       Res_driver.parsingEngine.parseInterface ~forPrinter:true ~filename
