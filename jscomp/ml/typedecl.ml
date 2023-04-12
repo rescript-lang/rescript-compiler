5a508d39db2f803d26e2e4caed3e7126ec36b988
Cristiano Calcagno <cristianoc@users.noreply.github.com>
2023-04-12 10:30:10 +0200

    Disable warning of repeated "dotdotdot".

--- jscomp/ml/typedecl.ml
+++ jscomp/ml/typedecl.ml
@@ -1214,6 +1214,7 @@ let check_duplicates sdecl_list =
           (fun {pld_name=cname;pld_loc=loc} ->
             try
               let name' = Hashtbl.find labels cname.txt in
+              if cname.txt <> "dotdotdot" then
               Location.prerr_warning loc
                 (Warnings.Duplicate_definitions
                    ("label", cname.txt, name', sdecl.ptype_name.txt))
