f69d6457bc4da2b6be1eb45da7451315ebeb0f37
Cristiano Calcagno <cristianoc@users.noreply.github.com>
2023-04-13 15:39:44 +0200

    Fix formatting of uncurried function with constraint on return type.
    See https://github.com/rescript-lang/rescript-compiler/issues/6141

--- res_syntax/src/res_parens.ml
+++ res_syntax/src/res_parens.ml
@@ -441,6 +441,7 @@ let includeModExpr modExpr =
 let arrowReturnTypExpr typExpr =
   match typExpr.Parsetree.ptyp_desc with
   | Parsetree.Ptyp_arrow _ -> true
+  | _ when Ast_uncurried.typeIsUncurriedFun typExpr -> true
   | _ -> false
 
 let patternRecordRowRhs (pattern : Parsetree.pattern) =
