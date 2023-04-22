3c40a3a74b384afdbe7f0ed6cd04041e116fba70
Patrick Ecker <ryyppy@users.noreply.github.com>
2023-04-22 10:27:28 +0200

    Add pending changes to playground infra for building 10.1.2

--- playground/playground_test.js
+++ playground/playground_test.js
@@ -4,13 +4,37 @@ require("./packages/@rescript/react/cmij.js")
 let compiler = rescript_compiler.make()
 
 let result = compiler.rescript.compile(`
-  let a = <div> {React.string("test")} </div>
+  @@jsxConfig({ version: 4, mode: "automatic" })
+
+  module A = {
+    @react.component
+    let make = (~a) => {
+      <div> {React.string(a)} </div>
+    }
+  }
+
+  module B = {
+    type props = { a: string }
+
+    let make = ({a}) => {
+      <A a/>
+    }
+  }
+
+  let a = <B a="hello" />
 `);
 
 if(result.js_code != "") {
   console.log('-- Playground test output --');
   console.log(`ReScript version: ${compiler.rescript.version}`);
   console.log('----');
+  if(result.errors && result.errors.length > 0) {
+    console.log("COMPILATION ERROR");
+    for(let error of result.errors) {
+      console.log(error.shortMsg);
+    }
+    process.exit(1);
+  }
   console.log(result.js_code);
   console.log('-- Playground test complete --');
 }
