f3ebaffb8568b1191d28c646d91ad05d5d48acec
Woonki Moon <woonki.moon@gmail.com>
2023-06-26 17:18:06 +0900

    avoid infinite loop

--- jscomp/frontend/bs_builtin_ppx.ml
+++ jscomp/frontend/bs_builtin_ppx.ml
@@ -228,16 +228,15 @@ let expr_mapper ~async_context ~in_function_def (self : mapper)
     let safe_module_type_lid : Ast_helper.lid =
       {txt = Lident (local_module_type_name txt); loc = me.pmod_loc}
     in
-    self.expr self
-      {
-        e with
-        pexp_desc =
-          Pexp_letmodule
-            ( lid,
-              Ast_await.create_await_module_expression
-                ~module_type_lid:safe_module_type_lid me,
-              expr );
-      }
+    {
+      e with
+      pexp_desc =
+        Pexp_letmodule
+          ( lid,
+            Ast_await.create_await_module_expression
+              ~module_type_lid:safe_module_type_lid me,
+            self.expr self expr );
+    }
   (* module M = await (Belt.List: BeltList) *)
   | Pexp_letmodule
       ( lid,
@@ -249,16 +248,15 @@ let expr_mapper ~async_context ~in_function_def (self : mapper)
          } as me),
         expr )
     when Res_parsetree_viewer.hasAwaitAttribute pmod_attributes ->
-    self.expr self
-      {
-        e with
-        pexp_desc =
-          Pexp_letmodule
-            ( lid,
-              Ast_await.create_await_module_expression ~module_type_lid:mtyp_lid
-                me,
-              expr );
-      }
+    {
+      e with
+      pexp_desc =
+        Pexp_letmodule
+          ( lid,
+            Ast_await.create_await_module_expression ~module_type_lid:mtyp_lid
+              me,
+            self.expr self expr );
+    }
   | _ -> default_expr_mapper self e
 
 let expr_mapper ~async_context ~in_function_def (self : mapper)
