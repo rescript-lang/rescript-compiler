7d6ccc9cd5dad9f9617989f060d97d69fb7c1324
Cristiano Calcagno <cristianoc@users.noreply.github.com>
2023-09-18 18:52:07 +0200

    Make optional args of option type.

--- jscomp/gentype/NamedArgs.ml
+++ jscomp/gentype/NamedArgs.ml
@@ -2,14 +2,11 @@ open GenTypeCommon
 
 let group labeledTypes =
   let types =
-    Ext_list.map labeledTypes (fun (lbl, type_) ->
-        let aName =
-          match lbl with
-          | Nolabel -> ""
-          | Label lbl | OptLabel lbl -> lbl
-        in
-
-        {aName; aType = type_})
+    Ext_list.map labeledTypes (fun (lbl, aType) ->
+        match lbl with
+        | Nolabel -> {aName = ""; aType}
+        | Label lbl -> {aName = lbl; aType}
+        | OptLabel lbl -> {aName = lbl; aType = Option aType})
   in
   match types with
   | [{aType}] when aType = unitT ->
--- jscomp/gentype_tests/typescript-react-example/src/LabeledFun.gen.tsx
+++ jscomp/gentype_tests/typescript-react-example/src/LabeledFun.gen.tsx
@@ -6,4 +6,4 @@
 import * as LabeledFunBS__Es6Import from './LabeledFun.bs';
 const LabeledFunBS: any = LabeledFunBS__Es6Import;
 
-export const labelled: (a:number, b:number, c:number, _4:number, e:number, f:number) => number = LabeledFunBS.labelled;
+export const labelled: (a:number, b:(undefined | number), c:number, _4:number, e:number, f:number) => number = LabeledFunBS.labelled;
