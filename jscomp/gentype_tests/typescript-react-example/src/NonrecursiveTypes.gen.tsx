8031c1329942778e3bb9d03d9d001d0d1ce21d29
Jono Prest <65739024+JonoPrest@users.noreply.github.com>
2023-12-14 14:40:39 +0200

    Fix Gentype: Support mutual recursion in types (#6528)
    * In GenType Add mutual recursive test
    
    Signed-Off-By:  Jono Prest <jjprest@gmail.com>
    
    * In GenType add support for mutual recursion
    
    Signed-Off-By: Jono Prest <jjprest@gmail.com>
    
    * Refactor GenType support mutual type recursion
    Signed-Off-By: Jono Prest <jjprest@gmail.com>
    
    ---------
    
    Signed-off-by: Jono Prest <jjprest@gmail.com>

--- jscomp/gentype_tests/typescript-react-example/src/NonrecursiveTypes.gen.tsx
+++ jscomp/gentype_tests/typescript-react-example/src/NonrecursiveTypes.gen.tsx
@@ -8,3 +8,7 @@ export type notRecursive = number;
 export type M_notRecursive = notRecursive[];
 
 export type M_recursive = { readonly self: M_recursive };
+
+export type M_mutualRecursive = { readonly a: M_a };
+
+export type M_a = { readonly self: M_mutualRecursive };
