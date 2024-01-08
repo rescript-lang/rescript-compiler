d32a1292dc42e4513dcea66396aaaa120bc60cdb
mununki <woonki.moon@gmail.com>
2024-01-09 02:03:36 +0900

    fix dead value

--- jscomp/gentype/ImportPath.mli
+++ jscomp/gentype/ImportPath.mli
@@ -3,7 +3,7 @@ open GenTypeCommon
 type t
 
 val bsCurryPath : config:Config.t -> t
-val chopExtensionSafe : t -> t
+val chopExtensionSafe : t -> t [@@live]
 val dump : t -> string
 val emit : t -> string
 val fromModule : dir:string -> importExtension:string -> ModuleName.t -> t
