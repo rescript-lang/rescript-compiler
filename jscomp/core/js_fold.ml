dc2aef6e58d57e25be6012ea4f66a744097df2e6
Paul Tsnobiladzé <paul.tsnobiladze@gmail.com>
2024-01-24 15:27:09 +0100

    implement expression_desc for Tagged_template

--- jscomp/core/js_fold.ml
+++ jscomp/core/js_fold.ml
@@ -124,7 +124,10 @@ class fold =
           let _self = _self#expression _x0 in
           let _self = list (fun _self -> _self#expression) _self _x1 in
           _self
-      | Tagged_template (_, _, _) ->
+      | Tagged_template (_x0, _x1, _x2) ->
+          let _self = _self#expression _x0 in
+          let _self = list (fun _self -> _self#expression) _self _x1 in
+          let _self = list (fun _self -> _self#expression) _self _x2 in
           _self
       | String_index (_x0, _x1) ->
           let _self = _self#expression _x0 in
