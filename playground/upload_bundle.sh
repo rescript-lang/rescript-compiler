8cd1f69954d2a54a67133f8ed381ff31fade050a
Christoph Knittel <ck@cca.io>
2024-02-08 06:50:16 +0100

    Debug playground upload

--- playground/upload_bundle.sh
+++ playground/upload_bundle.sh
@@ -1,4 +1,4 @@
-#/usr/bin/sh
+#!/bin/bash
 
 # This script will publish the compiler.js bundle / packages cmij.js files to our KeyCDN server.
 # The target folder on KeyCDN will be the compiler.js' version number.
@@ -7,17 +7,18 @@
 SCRIPT_DIR=$(cd "$(dirname "$0")"; pwd -P)
 
 # Get the actual version from the compiled playground bundle
-VERSION=$(cd $SCRIPT_DIR; node -e 'require("./compiler.js"); console.log(rescript_compiler.make().rescript.version)')
+# VERSION=$(cd $SCRIPT_DIR; node -e 'require("./compiler.js"); console.log(rescript_compiler.make().rescript.version)')
+VERSION=test
 
-if [ -z "${KEYCDN_USER}" ]; then
-  echo "KEYCDN_USER environment variable not set. Make sure to set the environment accordingly."
-  exit 1
-fi
+# if [ -z "${KEYCDN_USER}" ]; then
+#   echo "KEYCDN_USER environment variable not set. Make sure to set the environment accordingly."
+#   exit 1
+# fi
 
-if [ -z "${KEYCDN_PASSWORD}" ]; then
-  echo "KEYCDN_PASSWORD environment variable not set. Make sure to set the environment accordingly."
-  exit 1
-fi
+# if [ -z "${KEYCDN_PASSWORD}" ]; then
+#   echo "KEYCDN_PASSWORD environment variable not set. Make sure to set the environment accordingly."
+#   exit 1
+# fi
 
 KEYCDN_SRV="ftp.keycdn.com"
 NETRC_FILE="${SCRIPT_DIR}/.netrc"
@@ -29,7 +30,7 @@ if [ ! -f "${NETRC_FILE}" ]; then
   echo "machine ${KEYCDN_SRV} login $KEYCDN_USER password $KEYCDN_PASSWORD" > "${NETRC_FILE}"
 fi
 
-PACKAGES=( "compiler-builtins" "@rescript/react" "@rescript/core")
+PACKAGES=("compiler-builtins" "@rescript/react" "@rescript/core")
 
 echo "Uploading compiler.js file..."
 curl --ftp-create-dirs -T "${SCRIPT_DIR}/compiler.js" --ssl --netrc-file $NETRC_FILE ftp://${KEYCDN_SRV}/v${VERSION}/compiler.js
@@ -45,7 +46,3 @@ do
 
   curl --ftp-create-dirs -T "${SOURCE}/cmij.js" --ssl --netrc-file $NETRC_FILE "${TARGET}/cmij.js"
 done
-   
-
-
-
