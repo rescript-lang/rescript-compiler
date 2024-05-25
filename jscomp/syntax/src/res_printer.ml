067fe15d2c11ee98a7caa48ae556a6e4f7a45982
Shulhi Sapli <shulhi@gmail.com>
2024-05-25 20:00:05 +0800

    Different approach

--- jscomp/syntax/src/res_printer.ml
+++ jscomp/syntax/src/res_printer.ml
@@ -2646,21 +2646,24 @@ and printExpression ~state (e : Parsetree.expression) cmtTbl =
         ( Nolabel,
           None,
           {ppat_desc = Ppat_var {txt = "__x"}},
-          {pexp_desc = Pexp_apply _} )
+          {pexp_desc = Pexp_apply _} ) ->
+      printExpressionWithComments ~state
+        (ParsetreeViewer.rewriteUnderscoreApply e)
+        cmtTbl
     | Pexp_construct
         ( {txt = Lident "Function$"},
           Some
-            {
-              pexp_desc =
-                Pexp_fun
-                  ( Nolabel,
-                    None,
-                    {ppat_desc = Ppat_var {txt = "__x"}},
-                    {pexp_desc = Pexp_apply _} );
-            } ) ->
+            ({
+               pexp_desc =
+                 Pexp_fun
+                   ( Nolabel,
+                     None,
+                     {ppat_desc = Ppat_var {txt = "__x"}},
+                     {pexp_desc = Pexp_apply _} );
+             } as e_fun) ) ->
       (* (__x) => f(a, __x, c) -----> f(a, _, c)  *)
       printExpressionWithComments ~state
-        (ParsetreeViewer.rewriteUnderscoreApply e)
+        (ParsetreeViewer.rewriteUnderscoreApply e_fun)
         cmtTbl
     | Pexp_construct ({txt = Lident "Function$"}, Some expr) -> (
       let nonGhostAttrs =
