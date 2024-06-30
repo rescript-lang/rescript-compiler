73130b954ee38ff0a521a9c5e1d042845b3fa5b2
Hyeseong Kim <hey@hyeseong.kim>
2024-06-30 09:59:01 +0900

    revert some

--- .github/workflows/get_artifact_dir_name.js
+++ .github/workflows/get_artifact_dir_name.js
@@ -5,6 +5,6 @@ const { dirName: artifactDirName } = require("../../cli/bin_path.js");
 
 // Pass artifactDirName to subsequent GitHub actions
 fs.appendFileSync(
-  process.env.GITHUB_OUTPUT,
-  `dirname=${artifactDirName}${os.EOL}`,
+  process.env.GITHUB_ENV,
+  `artifact_dir_name=${artifactDirName}${os.EOL}`,
 );
