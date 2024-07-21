42df72029985dc0f5f49508a223b60475a27f74b
Pedro Castro <aspeddro@gmail.com>
2024-07-21 03:58:25 -0300

    Force regenerate `build.ninja` (#6877)
    * force regenerate build.ninja
    
    * fix bin path
    
    * Update CHANGELOG.md
    
    * force regenerate when `warn-error` argument change
    
    * restore `-regen` argument
    
    * regenerate if previous warn error != 0
    # Conflicts:
    #	CHANGELOG.md

--- jscomp/build_tests/build_warn_as_error/input.js
+++ jscomp/build_tests/build_warn_as_error/input.js
@@ -2,16 +2,47 @@ var p = require("child_process");
 var assert = require("assert");
 var rescript_exe = require("../../../scripts/bin_path").rescript_exe;
 
-var o = p.spawnSync(rescript_exe, ["build", "-warn-error", "+110"], {
+var o1 = p.spawnSync(rescript_exe, ["build"], {
   encoding: "utf8",
   cwd: __dirname,
 });
 
-var error_message = o.stdout
+var first_message = o1.stdout
   .split("\n")
   .map(s => s.trim())
-  .includes("Warning number 110 (configured as error)");
+  .find(s => s == "Warning number 110");
 
-if (!error_message) {
-  assert.fail(o.stdout);
+if (!first_message) {
+  assert.fail(o1.stdout);
+}
+
+// Second build using -warn-error +110
+var o2 = p.spawnSync(rescript_exe, ["build", "-warn-error", "+110"], {
+  encoding: "utf8",
+  cwd: __dirname,
+});
+
+var second_message = o2.stdout
+  .split("\n")
+  .map(s => s.trim())
+  .find(s => s == "Warning number 110 (configured as error)");
+
+if (!second_message) {
+  assert.fail(o2.stdout);
+}
+
+// Third build, without -warn-error +110
+// The result should not be a warning as error
+var o3 = p.spawnSync(rescript_exe, ["build"], {
+  encoding: "utf8",
+  cwd: __dirname,
+});
+
+var third_message = o3.stdout
+  .split("\n")
+  .map(s => s.trim())
+  .find(s => s == "Dependency Finished");
+
+if (!third_message) {
+  assert.fail(o3.stdout);
 }
