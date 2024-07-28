6cfcf3c3402144f83ba3a5c8eb77d0996cd84869
Christoph Knittel <ck@cca.io>
2024-07-28 12:30:29 +0200

    CI: build Linux x64 binaries directly on runner (no docker) (#6917)
    * CI: build Linux x64 binaries directly on runner (no docker)
    
    * Check that all Linux executables are statically linked

--- scripts/moveArtifacts.sh
+++ scripts/moveArtifacts.sh
@@ -1,5 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 set -e
+
+check_statically_linked() {
+    local dir=$1
+    local all_statically_linked=true
+
+    for file in "$dir"/*; do
+        if [ -f "$file" ]; then
+            if file "$file" | grep -Eq "statically linked|static-pie linked"; then
+                echo "$file is statically linked."
+            else
+                echo "$file is NOT statically linked."
+                all_statically_linked=false
+            fi
+        fi
+    done
+
+    if $all_statically_linked; then
+        echo "All files in $dir are statically linked executables."
+    else
+        echo "Error: Not all files in $dir are statically linked executables."
+        exit 1
+    fi
+}
 
 chmod +x binaries-*/*.exe
 
@@ -11,3 +34,7 @@ mv binaries-win32 win32
 
 mv lib-ocaml lib/ocaml
 mv ninja/COPYING ninja.COPYING
+
+check_statically_linked "linux"
+check_statically_linked "linuxarm64"
+
