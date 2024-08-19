e78c73ee442a8136df58d6d19b27717ce5ac3e6b
nojaf <florian.verdonck@outlook.com>
2024-08-19 09:44:20 +0000

    Simplify Dockerfile and devcontainer

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,4 +1,9 @@
-#!/bin/bash
+#!/bin/sh
+
+# Opam stuff post install
+opam init -y --bare --disable-sandboxing git+https://github.com/rescript-lang/opam-repository
+opam switch create 5.2.0 --packages ocaml-option-static
+opam install -y dune cppo=1.6.9 js_of_ocaml-compiler=5.8.1 ocamlformat=0.26.2 ounit2=2.2.7 reanalyze=2.25.1
 
 # Install dev dependencies from OPAM
 opam install . --deps-only -y
@@ -7,6 +12,6 @@ opam install . --deps-only -y
 opam install ocaml-lsp-server -y
 
 # Add OPAM environment setup to shell startup script
-echo 'eval $(opam env)' >> ~/.bashrc
+echo 'eval $(opam env)' >> ~/.zshrc
 
 npm install --ignore-scripts
