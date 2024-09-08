4b8b0e0ab9082d49f1ec1183417c4d08ae22d961
Hyeseong Kim <hey@hyeseong.kim>
2024-09-09 08:38:39 +0900

    remove unused code

--- jscomp/ml/printlambda.mli
+++ jscomp/ml/printlambda.mli
@@ -19,7 +19,3 @@ open Format
 
 val structured_constant: formatter -> structured_constant -> unit
 val lambda: formatter -> lambda -> unit
-
-val primitive: formatter -> primitive -> unit
-val name_of_primitive : primitive -> string
-val value_kind : value_kind -> string
