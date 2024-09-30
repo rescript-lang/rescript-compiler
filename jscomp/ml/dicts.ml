b1ebde4587aa17f53de1920ad5970578802104c6
Gabriel Nordeborn <gabbe.nord@gmail.com>
2024-09-30 12:10:19 +0200

    format

--- jscomp/ml/dicts.ml
+++ jscomp/ml/dicts.ml
@@ -1,11 +1,10 @@
 let has_dict_pattern_attribute attrs =
   attrs
   |> List.find_opt (fun (({txt}, _) : Parsetree.attribute) ->
-          txt = "res.dictPattern")
+         txt = "res.dictPattern")
   |> Option.is_some
 
 let has_dict_attribute attrs =
-        attrs
-        |> List.find_opt (fun (({txt}, _) : Parsetree.attribute) ->
-                txt = "res.dict")
-        |> Option.is_some
+  attrs
+  |> List.find_opt (fun (({txt}, _) : Parsetree.attribute) -> txt = "res.dict")
+  |> Option.is_some
