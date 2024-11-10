6a077e283599b6fff187c768aa697c8aa125e8c5
Hyeseong Kim <hey@hyeseong.kim>
2024-11-10 09:58:42 +0900

    add a test

--- tests/tests/src/exponentiation_test.mjs
+++ tests/tests/src/exponentiation_test.mjs
@@ -16,23 +16,27 @@ function eq(loc, x, y) {
 
 let intPow = ((a, b) => Math.pow(a, b) | 0);
 
-eq("File \"exponentiation_test.res\", line 10, characters 5-12", 2 ** 3 ** 2, Math.pow(2, Math.pow(3, 2)));
+let four = 4;
 
-eq("File \"exponentiation_test.res\", line 11, characters 5-12", 2 ** (-3) ** 2, Math.pow(2, Math.pow(-3, 2)));
+eq("File \"exponentiation_test.res\", line 11, characters 5-12", 2 ** 3 ** 2, Math.pow(2, Math.pow(3, 2)));
 
-eq("File \"exponentiation_test.res\", line 12, characters 5-12", (2 ** 3) ** 2, Math.pow(Math.pow(2, 3), 2));
+eq("File \"exponentiation_test.res\", line 12, characters 5-12", 2 ** (-3) ** 2, Math.pow(2, Math.pow(-3, 2)));
 
-eq("File \"exponentiation_test.res\", line 13, characters 5-12", (-2) ** 2, Math.pow(-2, 2));
+eq("File \"exponentiation_test.res\", line 13, characters 5-12", (2 ** 3) ** 2, Math.pow(Math.pow(2, 3), 2));
 
-eq("File \"exponentiation_test.res\", line 15, characters 5-12", 512, intPow(2, intPow(3, 2)));
+eq("File \"exponentiation_test.res\", line 14, characters 5-12", (-2) ** 2, Math.pow(-2, 2));
 
-eq("File \"exponentiation_test.res\", line 16, characters 5-12", 512, intPow(2, intPow(-3, 2)));
+eq("File \"exponentiation_test.res\", line 16, characters 5-12", 512, intPow(2, intPow(3, 2)));
 
-eq("File \"exponentiation_test.res\", line 17, characters 5-12", 64, intPow(intPow(2, 3), 2));
+eq("File \"exponentiation_test.res\", line 17, characters 5-12", 512, intPow(2, intPow(-3, 2)));
 
-eq("File \"exponentiation_test.res\", line 18, characters 5-12", -2147483648, intPow(-2, 31));
+eq("File \"exponentiation_test.res\", line 18, characters 5-12", 64, intPow(intPow(2, 3), 2));
 
-eq("File \"exponentiation_test.res\", line 19, characters 5-12", 0, intPow(2, 32));
+eq("File \"exponentiation_test.res\", line 19, characters 5-12", -2147483648, intPow(-2, 31));
+
+eq("File \"exponentiation_test.res\", line 20, characters 5-12", 0, intPow(2, 32));
+
+eq("File \"exponentiation_test.res\", line 22, characters 5-12", 256, four ** four | 0);
 
 Mt.from_pair_suites("Exponentiation_test", suites.contents);
 
@@ -41,5 +45,6 @@ export {
   test_id,
   eq,
   intPow,
+  four,
 }
 /*  Not a pure module */
--- tests/tests/src/exponentiation_test.res
+++ tests/tests/src/exponentiation_test.res
@@ -5,6 +5,7 @@ let eq = (loc, x, y) => Mt.eq_suites(~test_id, ~suites, loc, x, y)
 external jsPow: (float, float) => float = "Math.pow"
 
 let intPow: (int, int) => int = %raw(`(a, b) => Math.pow(a, b) | 0`)
+let four: int = %raw(`4`)
 
 let () = {
   eq(__LOC__, 2. ** 3. ** 2., jsPow(2., jsPow(3., 2.)))
@@ -17,6 +18,8 @@ let () = {
   eq(__LOC__, (2 ** 3) ** 2, intPow(intPow(2, 3), 2))
   eq(__LOC__, -2 ** 31, intPow(-2, 31))
   eq(__LOC__, 2 ** 32, intPow(2, 32))
+
+  eq(__LOC__, 4 ** 4, four ** four)
 }
 
 let () = Mt.from_pair_suites(__MODULE__, suites.contents)
