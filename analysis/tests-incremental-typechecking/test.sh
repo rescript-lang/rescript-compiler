ef14388e8348baca3015c6769b6604579fcc4c74
Gabriel Nordeborn <gabbe.nord@gmail.com>
2024-11-10 14:46:57 +0100

    commit OK output

--- analysis/tests-incremental-typechecking/test.sh
+++ analysis/tests-incremental-typechecking/test.sh
@@ -1,6 +1,6 @@
 for file in src/*.res; do
   output="$(dirname $file)/expected/$(basename $file).txt"
-  ../../rescript-editor-analysis.exe test $file &> $output
+  ../../_build/install/default/bin/rescript-editor-analysis test $file &> $output
   # CI. We use LF, and the CI OCaml fork prints CRLF. Convert.
   if [ "$RUNNER_OS" == "Windows" ]; then
     perl -pi -e 's/\r\n/\n/g' -- $output
