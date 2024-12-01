0e9cb31b78b166176392c59b06eade1687684025
Pedro Castro <aspeddro@gmail.com>
2024-12-01 17:58:49 -0300

    rename attr

--- compiler/syntax/src/res_core.ml
+++ compiler/syntax/src/res_core.ml
@@ -2041,9 +2041,6 @@ and parse_bracket_access p expr start_pos =
     Parser.eat_breadcrumb p;
     let rbracket = p.prev_end_pos in
     let array_loc = mk_loc lbracket rbracket in
-    let attr =
-      (Location.mkloc "res.syntaxSugar" array_loc, Parsetree.PStr [])
-    in
     match p.token with
     | Equal ->
       Parser.leave_breadcrumb p ExprArrayMutation;
@@ -2060,6 +2057,8 @@ and parse_bracket_access p expr start_pos =
         Location.mkloc (Longident.Ldot (Lident "Array", "set")) array_loc
       in
       let end_pos = p.prev_end_pos in
+      (* Adding attribute because the internal representation of arr[idx] is Array.set and we don't have a way to distinguish both when issuing the token on the LSP *)
+      let attr = (Location.mkloc "res.arraySet" array_loc, Parsetree.PStr []) in
       let array_set =
         Ast_helper.Exp.apply ~loc:(mk_loc start_pos end_pos)
           (Ast_helper.Exp.ident ~loc:array_loc array_set ~attrs:[attr])
@@ -2069,6 +2068,7 @@ and parse_bracket_access p expr start_pos =
       array_set
     | _ ->
       let end_pos = p.prev_end_pos in
+      let attr = (Location.mkloc "res.arrayGet" array_loc, Parsetree.PStr []) in
       let e =
         Ast_helper.Exp.apply ~loc:(mk_loc start_pos end_pos)
           (Ast_helper.Exp.ident ~loc:array_loc ~attrs:[attr]
