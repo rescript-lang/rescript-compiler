81a5081d18d825878029f003243b95aa6db73beb
Christoph Knittel <ck@cca.io>
2024-12-14 15:14:12 +0100

    Remove unused code from Location and Rescript_cpp modules (#7150)
    * Remove unused stuff from Location and Rescript_cpp modules
    
    * Remove unused stuff from Syntaxerr module
    
    * CHANGELOG

--- compiler/ml/location.mli
+++ compiler/ml/location.mli
@@ -36,31 +36,13 @@ val none : t
 val in_file : string -> t
 (** Return an empty ghost range located in a given file. *)
 
-val init : Lexing.lexbuf -> string -> unit
-(** Set the file name and line number of the [lexbuf] to be the start
-    of the named file. *)
-
-val curr : Lexing.lexbuf -> t
-(** Get the location of the current token from the [lexbuf]. *)
-
-val symbol_rloc : unit -> t
-val symbol_gloc : unit -> t
-
-val rhs_loc : int -> t
-(** [rhs_loc n] returns the location of the symbol at position [n], starting
-  at 1, in the current parser rule. *)
-
 val input_name : string ref
 val set_input_name : string -> unit
-val input_lexbuf : Lexing.lexbuf option ref
 
 val get_pos_info : Lexing.position -> string * int * int (* file, line, char *)
 val print_loc : formatter -> t -> unit
-val print_error : tag -> formatter -> t -> unit
 
 val prerr_warning : t -> Warnings.t -> unit
-val echo_eof : unit -> unit
-val reset : unit -> unit
 
 val warning_printer : (t -> formatter -> Warnings.t -> unit) ref
 (** Hook for intercepting warnings. *)
@@ -75,23 +57,9 @@ type 'a loc = {txt: 'a; loc: t}
 val mknoloc : 'a -> 'a loc
 val mkloc : 'a -> t -> 'a loc
 
-val print :
-  ?src:string option ->
-  message_kind:[< `error | `warning | `warning_as_error > `warning] ->
-  string ->
-  formatter ->
-  t ->
-  unit
-val print_compact : formatter -> t -> unit
 val print_filename : formatter -> string -> unit
 
-val absolute_path : string -> string
-
 val show_filename : string -> string
-(** In -absname mode, return the absolute path for this filename.
-        Otherwise, returns the filename unchanged. *)
-
-val absname : bool ref
 
 (** Support for located errors *)
 
@@ -106,13 +74,6 @@ exception Already_displayed_error
 exception Error of error
 
 val error : ?loc:t -> ?sub:error list -> ?if_highlight:string -> string -> error
-
-val print_error_prefix : Format.formatter -> unit
-val pp_ksprintf :
-  ?before:(formatter -> unit) ->
-  (string -> 'a) ->
-  ('b, formatter, unit, 'a) format4 ->
-  'b
 
 val errorf :
   ?loc:t ->
